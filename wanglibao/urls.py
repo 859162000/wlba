d06e2202f824d4854d690b82c935b1d4f64f0691
shuo li <shuoli84@gmail.com>
2014-01-07 15:25:40 +0800

    Add trust and issuer

--- wanglibao/urls.py
+++ wanglibao/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib.auth.models import User, Group, Permission, ContentType
 from rest_framework import viewsets, routers
+from trust.models import Trust, Issuer
 
 from django.contrib import admin
 admin.autodiscover()
@@ -21,19 +22,27 @@ class PermissionViewSet(viewsets.ModelViewSet):
 class ContentTypeViewSet(viewsets.ModelViewSet):
     model = ContentType
 
+
+class TrustViewSet(viewsets.ModelViewSet):
+    model = Trust
+
+
+class IssuerViewSet(viewsets.ModelViewSet):
+    model = Issuer
+
+
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
 router.register(r'permissions', PermissionViewSet)
 router.register(r'contentTypes', ContentTypeViewSet)
+router.register(r'trusts', TrustViewSet)
+router.register(r'issuers', IssuerViewSet)
 
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'wanglibao.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+urlpatterns = patterns(
+    '',
     url(r'^', include(router.urls)),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
