560b22b3360fc32ff1790ca3b3b941095d232930
shuo li <shuoli84@gmail.com>
2014-01-15 13:12:46 +0800

    Add filtering by rate with min_rate, max_rate, scale by min_scale, max_scale

--- wanglibao_rest/views.py
+++ wanglibao_rest/views.py
@@ -2,7 +2,9 @@ from django.contrib.auth.models import User, Group, Permission
 
 # Create your views here.
 from django.contrib.contenttypes.models import ContentType
+import django_filters
 from rest_framework import viewsets
+from rest_framework.filters import FilterSet
 from trust.models import Trust, Issuer
 from wanglibao_rest.serializers import UserSerializer
 
@@ -12,9 +14,28 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
+class TrustFilterSet(FilterSet):
+    min_rate = django_filters.NumberFilter(name="expected_earning_rate", lookup_type='gte')
+    max_rate = django_filters.NumberFilter(name="expected_earning_rate", lookup_type='lte')
+    min_scale = django_filters.NumberFilter(name="scale", lookup_type='gte')
+    max_scale = django_filters.NumberFilter(name="scale", lookup_type='lte')
+
+    class Meta:
+        model = Trust
+        fields = ['name',
+                  'short_name',
+                  'expected_earning_rate',
+                  'issuer__name',
+                  'available_region',
+                  'investment_threshold',
+                  'min_rate', 'max_rate',
+                  'min_scale', 'max_scale',
+                  ]
+
+
 class TrustViewSet(viewsets.ModelViewSet):
     model = Trust
-    filter_fields = ('name', 'short_name', 'expected_earning_rate', 'issuer__name', 'available_region', 'investment_threshold')
+    filter_class = TrustFilterSet
 
 
 class IssuerViewSet(viewsets.ModelViewSet):
