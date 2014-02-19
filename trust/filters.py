fba15ab1c97948e8f7355760e3e51eab797f7fe6
shuo li <shuoli84@gmail.com>
2014-02-20 00:44:25 +0800

    Add numeric short hands for numberic fields, lt gt lte gte etc, with much less code

--- trust/filters.py
+++ trust/filters.py
@@ -1,27 +1,36 @@
 import django_filters
+from django_filters.filterset import get_declared_filters
 from rest_framework.filters import FilterSet
 from trust.models import Trust
 
 
 class TrustFilterSet(FilterSet):
-    min_rate = django_filters.NumberFilter(name="expected_earning_rate", lookup_type='gte')
-    max_rate = django_filters.NumberFilter(name="expected_earning_rate", lookup_type='lt')
-    min_scale = django_filters.NumberFilter(name="scale", lookup_type='gte')
-    max_scale = django_filters.NumberFilter(name="scale", lookup_type='lt')
-    min_period = django_filters.NumberFilter(name="period", lookup_type='gte')
-    max_period = django_filters.NumberFilter(name="period", lookup_type='lt')
-    min_threshold = django_filters.NumberFilter(name="investment_threshold", lookup_type='gte')
-    max_threshold = django_filters.NumberFilter(name="investment_threshold", lookup_type='lt')
-
     class Meta:
         model = Trust
-        fields = ['name',
-                  'short_name',
-                  'expected_earning_rate',
-                  'issuer__name',
-                  'available_region',
-                  'investment_threshold',
-                  'min_rate', 'max_rate',
-                  'min_scale', 'max_scale',
-                  'min_period', 'max_period',
-                  ]
+        number_fields = (
+            ('threshold', 'investment_threshold'),
+            ('period', 'period'),
+            ('rate', 'expected_earning_rate'),
+            ('scale', 'scale')
+        )
+        number_lookup_types = ('gt', 'gte', 'lt', 'lte', ('max', 'lt'), ('min', 'gte'))
+
+    def __init__(self, *args, **kwargs):
+        super(TrustFilterSet, self).__init__(*args, **kwargs)
+
+        number_fields = self.Meta.number_fields
+        number_lookup_types = self.Meta.number_lookup_types
+        attrs = {}
+
+        for field, source_field in number_fields:
+            attrs[field] = django_filters.NumberFilter(name=source_field)
+
+            for lookup_type in number_lookup_types:
+                if isinstance(lookup_type, tuple):
+                    name, operator = lookup_type
+                    attrs[name + '_' + field] = django_filters.NumberFilter(name=source_field, lookup_type=operator)
+                else:
+                    attrs[str(lookup_type) + '_' + field] = django_filters.NumberFilter(name=source_field, lookup_type=lookup_type)
+
+        for name, filter in attrs.items():
+            self.base_filters[name] = filter
