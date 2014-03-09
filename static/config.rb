578400a2469caa300ad11f1660168c1abcc0f666
shuo li <shuoli84@gmail.com>
2014-03-09 16:53:03 +0800

    Add iconfont support, in future, migrate all icon stuff to font icon to get better visual effect in higher resolution

--- static/config.rb
+++ static/config.rb
@@ -1,6 +1,7 @@
 # Require any additional compass plugins here.
 require 'susy'
 require 'toolkit'
+require 'font-icons'
 require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
