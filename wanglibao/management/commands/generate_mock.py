88606bebe236655cbac9a75d0b86595f85e5f935
Xihui He <xihuihe@gmail.com>
2014-03-28 16:58:54 +0800

    new style for home page, initial version

--- wanglibao/management/commands/generate_mock.py
+++ wanglibao/management/commands/generate_mock.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from wanglibao.mock_generator import MockGenerator
 
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
