b847368b32e3e1e7e60abc374d4b045cb91c04eb
Shuo Li <shuoli84@gmail.com>
2014-04-09 13:56:31 +0800

    Hot list item input instead of select

--- wanglibao_hotlist/admin.py
+++ wanglibao_hotlist/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 from wanglibao_hotlist.models import HotTrust, HotFinancing, HotFund
 
-admin.site.register(HotTrust)
-admin.site.register(HotFinancing)
-admin.site.register(HotFund)
+
+class HotTrustAdmin(admin.ModelAdmin):
+    raw_id_fields = ('trust',)
+
+
+class HotFinancingAdmin(admin.ModelAdmin):
+    raw_id_fields = ('bank_financing',)
+
+
+class HotFundAdmin(admin.ModelAdmin):
+    raw_id_fields = ('fund',)
+
+admin.site.register(HotTrust, HotTrustAdmin)
+admin.site.register(HotFinancing, HotFinancingAdmin)
+admin.site.register(HotFund, HotFundAdmin)
