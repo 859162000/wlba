5575bf3f1b4c23ade03b512ab2df992f64375495
Xihui He <xihuihe@gmail.com>
2014-05-05 16:51:07 +0800

    change link to char

--- wanglibao_banner/models.py
+++ wanglibao_banner/models.py
@@ -22,7 +22,7 @@ class Banner(models.Model):
     device = models.CharField(max_length=15, verbose_name=u'设备', choices=DEVICES)
     type = models.CharField(max_length=15, verbose_name=u'类型', choices=TYPES)
     name = models.CharField(max_length=30, verbose_name=u'名称')
-    link = models.URLField(max_length=1024, blank=True, verbose_name='link')
+    link = models.CharField(max_length=1024, blank=True, verbose_name=u'数据')
     image = models.ImageField(upload_to='banner', blank=True, verbose_name=u'图片')
     priority = models.IntegerField(verbose_name=u'优先级', help_text=u'越大越优先')
     alt = models.TextField(blank=True, verbose_name=u'图片说明')
