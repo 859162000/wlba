34f3594c51427feaabde5b7493ddf18776af48e7
LowID <soloradish@gmail.com>
2014-05-14 21:30:48 +0800

    add fetch available cash fund function in AppInfoFetcher Class

---
+++ wanglibao_buy/migrations/0005_auto__chg_field_availablefund_last_update__chg_field_tradehistory_crea.py
@@ -0,0 +1,184 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+
+        # Changing field 'AvailableFund.last_update'
+        db.alter_column(u'wanglibao_buy_availablefund', 'last_update', self.gf('django.db.models.fields.DateTimeField')())
+
+        # Changing field 'TradeHistory.create_date'
+        db.alter_column(u'wanglibao_buy_tradehistory', 'create_date', self.gf('django.db.models.fields.DateField')(auto_now=True))
+
+    def backwards(self, orm):
+
+        # Changing field 'AvailableFund.last_update'
+        db.alter_column(u'wanglibao_buy_availablefund', 'last_update', self.gf('django.db.models.fields.CharField')(max_length=20))
+
+        # Changing field 'TradeHistory.create_date'
+        db.alter_column(u'wanglibao_buy_tradehistory', 'create_date', self.gf('django.db.models.fields.DateField')())
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'wanglibao_buy.availablefund': {
+            'Meta': {'object_name': 'AvailableFund'},
+            'create_date': ('django.db.models.fields.DateField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'declare_status': ('django.db.models.fields.BooleanField', [], {}),
+            'fund_code': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'fund_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'fund_state': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'fund_type': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_update': ('django.db.models.fields.DateTimeField', [], {}),
+            'min_shares': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'purchase_limit_max': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'purchase_limit_min': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'purchase_second_limit_min': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'quick_cash_limit_max': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'quick_cash_limit_min': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'ration_limit_max': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'ration_limit_min': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'redeem_limit_max': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'redeem_limit_min': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'risk_level': ('django.db.models.fields.IntegerField', [], {}),
+            'share_type': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'subscribe_limit_max': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'subscribe_limit_min': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'subscribe_state': ('django.db.models.fields.BooleanField', [], {}),
+            'transform_limit_max': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'transform_limit_min': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'valuagr_state': ('django.db.models.fields.BooleanField', [], {}),
+            'withdraw_state': ('django.db.models.fields.BooleanField', [], {})
+        },
+        u'wanglibao_buy.bindbank': {
+            'Meta': {'object_name': 'BindBank'},
+            'balance': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'bank_name': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'bank_serial': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'bind_way': ('django.db.models.fields.IntegerField', [], {}),
+            'capital_mode': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'content_describe': ('django.db.models.fields.CharField', [], {'max_length': '500'}),
+            'create_date': ('django.db.models.fields.DateField', [], {'auto_now': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_freeze': ('django.db.models.fields.BooleanField', [], {}),
+            'is_vaild': ('django.db.models.fields.BooleanField', [], {}),
+            'limit_describe': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'no': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'priority': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'status_to_cn': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'sub_trade_account': ('django.db.models.fields.CharField', [], {'max_length': '1000'}),
+            'support_auto_pay': ('django.db.models.fields.BooleanField', [], {}),
+            'trade_account': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
+        },
+        u'wanglibao_buy.fundholdinfo': {
+            'Meta': {'object_name': 'FundHoldInfo'},
+            'bank_account': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'bank_name': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'bank_serial': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'capital_mode': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'create_date': ('django.db.models.fields.DateField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'current_remain_share': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'expire_shares': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'freeze_remain_share': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'fund_code': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'fund_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'fund_type': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'fund_type_to_cn': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'market_value': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '3'}),
+            'melon_method': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'nav_date': ('django.db.models.fields.DateField', [], {}),
+            'pernet_value': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'rapid_redeem': ('django.db.models.fields.BooleanField', [], {}),
+            'share_type': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            't_freeze_remain_share': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'trade_account': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'unpaid_income': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'usable_remain_share': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
+        },
+        u'wanglibao_buy.tradehistory': {
+            'Meta': {'ordering': "['-apply_date_time']", 'object_name': 'TradeHistory'},
+            'amount': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'apply_date_time': ('django.db.models.fields.DateTimeField', [], {}),
+            'apply_serial': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'bank_account': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'bank_name': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'bank_serial': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'business_type': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'business_type_to_cn': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'can_cancel': ('django.db.models.fields.BooleanField', [], {}),
+            'create_date': ('django.db.models.fields.DateField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'fund_code': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'fund_name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_cash_buy': ('django.db.models.fields.BooleanField', [], {}),
+            'pay_result': ('django.db.models.fields.IntegerField', [], {}),
+            'pay_status_to_cn': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'pound_age': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'share_type': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'share_type_to_cn': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'shares': ('django.db.models.fields.DecimalField', [], {'max_digits': '20', 'decimal_places': '2'}),
+            'status': ('django.db.models.fields.IntegerField', [], {}),
+            'status_to_cn': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'trade_account': ('django.db.models.fields.CharField', [], {'max_length': '20'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"})
+        },
+        u'wanglibao_buy.tradeinfo': {
+            'Meta': {'object_name': 'TradeInfo'},
+            'amount': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'created_at': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'item_id': ('django.db.models.fields.IntegerField', [], {}),
+            'item_name': ('django.db.models.fields.CharField', [], {'max_length': '64', 'blank': 'True'}),
+            'related_info': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'trade_type': ('django.db.models.fields.CharField', [], {'max_length': '16', 'blank': 'True'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['auth.User']"}),
+            'verify_info': ('django.db.models.fields.CharField', [], {'max_length': '128', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['wanglibao_buy']
