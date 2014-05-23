8ac02ab5552255e46eeca7437c05ee83da6b2959
LowID <soloradish@gmail.com>
2014-05-23 17:55:02 +0800

    rebuild fund robot

--- wanglibao_robot/fund.py
+++ wanglibao_robot/fund.py
@@ -1,259 +1,103 @@
-# -*- coding: utf-8 -*-
-import re
-
-import urllib2
-from datetime import timedelta
-import math
-from pyquery import PyQuery
-import time
-from wanglibao_fake.models import Formular
 from wanglibao_fund.models import Fund, FundIssuer
-from wanglibao_hotlist.models import HotFund, MobileHotFund
-from wanglibao_robot.models import ScrawlItem
-from wanglibao_robot.util import *
-from django.utils import timezone
-import sys
+from shumi_backend.fetch import AppLevel
+from shumi_backend.utility import mapping_fund_details, mapping_fund_issuer, mapping_fund_details_plus
+from shumi_backend.exception import FetchException
 
+class FundRobot(object):
 
-def get_keyvalues_table(pq):
-    trs = pq('tr')
-    pairs = [PyQuery(tr)('td') for tr in trs if len(tr.getchildren()) == 2]
-    results = {}
-    for k, v in pairs:
-        key = k.text_content()
-        value = v.text_content()
-        results[key] = value
-    return results
+    def __init__(self):
+        self.fetcher = AppLevel()
 
+    def run_robot(self):
+        # run sync_issuer before run_robot, in case guarantee related fund issuers existed.
+        info = self.fetcher.fund_details()
+        cash_info = [fund for fund in info if fund['fund_type'] == 7]
 
-def get_keyvalues_div_t_c(pq):
-    pairs = [(PyQuery(div)('div.t'), PyQuery(div)('div.c')) for div in pq('div.t').parent()]
+        funds = Fund.objects.all()
+        funds_dict = {fund.product_code: fund for fund in funds}
 
-    results = {}
-    for k, v in pairs:
-        key = k.text().strip()
-        value = ''
-        if v.html():
-            linebreaked = v.html().replace('<br />', '\n').strip()
-            if len(linebreaked) > 0:
-                value = PyQuery(linebreaked).text().strip()
-        else:
-            value = ''
-        results[key] = value
+        issuer_info = self.fetcher.fund_issuers()
+        issuer_dict = {issuer_record['guid']: issuer_record for issuer_record in issuer_info}
 
-    return results
+        for cash in cash_info:
+            print cash
+            issuer = FundIssuer.objects.filter(uuid=cash['invest_advisor_guid']).first()
 
-
-def get_today_rate(pq):
-    text = pq('.LatestNet p').text()
-
-    if text.find(u'日增长率') != -1:
-        matches = re.compile('(?P<earning_10k>[\-\d\.]+)\D*(?P<rate_today>[\-\d\.%]+)').match(text)
-        rate_today = '0'
-        earning_10k = '0'
-        if matches:
-            match_result = matches.groupdict()
-            rate_today = str(match_result['rate_today'])
-            earning_10k = str(match_result['earning_10k'])
-        return {
-            u'日涨幅': rate_today,
-            u'万份收益': earning_10k
-        }
-    else:
-        face_value, value_delta, rate = text.strip(')').replace(' ', '(').split('(')
-        return {
-            u'日涨幅': rate,
-            u'净值': face_value,
-            u'日净值涨幅': value_delta
-        }
-
-
-def get_7days_accumulated_value(pq):
-    key, value = pq('.LatestNet dl dt').text().split(':')
-    key = key.strip()
-    value = value.strip()
-
-    return {
-        key: value
-    }
-
-
-mappings = {
-    u'基金名称': 'name',
-    u'基金代码': 'product_code',
-    u'基金状态': 'status',
-    u'基金公司': 'issuer_name',
-    u'基金管理费': ('management_fee', 'percent'),
-    u'成立日期': ('found_date', 'date'),
-    u'首募规模': 'init_scale',
-    u'托管银行': 'hosting_bank',
-    u'基金全称': 'full_name',
-    u'基金类型': 'type',
-    u'交易状态': 'trade_status',
-    u'基金经理': 'manager',
-    u'基金托管费': ('hosting_fee', 'percent'),
-    u'最新份额': 'latest_shares',
-    u'最新规模': 'latest_scale',
-    u'投资目标': 'investment_target',
-    u'投资范围': 'investment_scope',
-    u'投资策略': 'investment_strategy',
-    u'收益分配原则': 'profit_allocation',
-    u'风险收益特征': 'risk_character',
-    u'日涨幅': ('rate_today', 'percent'),
-    u'净值': ('face_value', 'float'),
-}
-
-
-def get_info(pq):
-    fund = Fund()
-
-    # Get key values from table
-    key_values = get_keyvalues_table(pq)
-    key_values.update(get_keyvalues_div_t_c(pq))
-    key_values.update(get_today_rate(pq))
-    key_values.update(get_7days_accumulated_value(pq))
-
-    for key, value in key_values.items():
-        value = unicode(value.strip())
-        key = key.strip(':').strip(u'：')
-        if key in mappings:
-            type = 'string'
-            if isinstance(mappings[key], tuple):
-                attr_name, type = mappings[key]
-            else:
-                attr_name = mappings[key]
-
-            if hasattr(fund, attr_name):
-                if type == 'string':
-                    setattr(fund, attr_name, value)
-                elif type == 'float':
-                    setattr(fund, attr_name, parse_float(value))
-                elif type == 'date':
-                    setattr(fund, attr_name, parse_time(value))
-                elif type == 'percent':
-                    setattr(fund, attr_name, parse_percent(value))
-
-    issuer_name = key_values[u'基金公司']
-
-    if not FundIssuer.objects.filter(name=issuer_name).exists():
-        issuer = FundIssuer()
-        issuer.name = issuer_name
-        issuer.save()
-        fund.issuer = issuer
-    else:
-        fund.issuer = FundIssuer.objects.get(name=issuer_name)
-
-    qs = Fund.objects.filter(product_code=fund.product_code)
-    if qs.exists():
-        fund.id = qs.last().id
-        fund.save()
-        sys.stdout.write('u')
-    else:
-        fund.brief = ''
-        fund.save()
-        sys.stdout.write('.')
-
-    scrawl_item = ScrawlItem.objects.filter(type='fund', item_id=fund.id).first()
-    if scrawl_item is not None:
-        scrawl_item.last_updated = timezone.now()
-        scrawl_item.save()
-        sys.stdout.write('u')
-    else:
-        item = ScrawlItem()
-        item.type = 'fund'
-        item.item_id = fund.id
-        item.name = fund.name
-        item.issuer_name = fund.issuer.name
-        item.last_updated = timezone.now()
-        item.save()
-        sys.stdout.write('+')
-
-    sys.stdout.write(' ')
-    sys.stdout.flush()
-    return fund
-
-
-def handle_link(link):
-    uri = PyQuery(link).attr('href')
-    pq = PyQuery(urllib2.urlopen('http://www.howbuy.com' + uri + 'jjgk/').read())
-    get_info(pq)
-
-
-def run_robot(clean=False, offset=0):
-    if clean:
-        for fund in Fund.objects.all():
-            fund.delete()
-
-    try:
-        tree = PyQuery(urllib2.urlopen("http://www.howbuy.com/fundtool/filter.htm?action=filter&categories=all&companies=all&asserts=all&establish=all&risks=all").read())
-        links = tree.find('table tbody tr td:nth-child(3) a') + tree('.result_list textarea tr td:nth-child(3) a')
-        links = links[offset:]
-
-        print 'Get %d fund links, now get detail information on each of them' % len(links)
-
-        count = 0
-        for link in links:
-            error_count = 0
-            try:
-                count += 1
-                handle_link(link)
-                time.sleep(.5)
-                if error_count > 0:
-                    error_count -= 1
-
-                if count % 50 == 0:
-                    print '%d processed, %d to go' % (count, len(links) - count)
-            except Exception, e:
-                print 'Meet error %s at count: %d link: %s' % (e.message, count, PyQuery(link).attr('href'))
-                error_count += 1
-                if error_count >= 10:
-                    raise
-                time.sleep(10)
-
-                # Handle the link again
+            if not issuer:
+                # try retrieve fund issuer object by uuid.
                 try:
-                    handle_link(link)
-                except:
-                    print 'Meet error again, ignore it'
+                    issuer = FundIssuer(**mapping_fund_issuer(issuer_dict[cash['invest_advisor_guid']]))
+                    issuer.save()
+                # if no match uuid, log and continue.
+                except KeyError:
+                    continue
+                except Exception, e:
+                    print(e)
+                    continue
 
-        for si in ScrawlItem.objects.filter(last_updated__lt=timezone.now() - timedelta(weeks=2), type='fund'):
             try:
-                f = Fund.objects.get(id=si.item_id)
-                f.status = u'停售'
-                f.save()
-            except Exception:
-                si.delete()
+                target_fund = funds_dict[cash['fund_code']]
+                self.model_setter(target_fund, mapping_fund_details(cash, False))
+                target_fund.issuer = issuer
 
-        today = timezone.now().date()
-        if not Formular.objects.all().exists():
-            f = Formular()
-            f.save()
+            except KeyError:
+                target_fund = Fund(issuer=issuer, **mapping_fund_details(cash))
+                print('create new fund')
 
-        formular = Formular.objects.all().first()
+            # retrieve cash fund detail plus info
+            try:
+                detail_plus = self.fetcher.fund_details_plus(target_fund.product_code)
+                self.model_setter(target_fund, mapping_fund_details_plus(detail_plus[0]))
+            except FetchException:
+                pass
+            except KeyError:
+                pass
 
-        d = (today - timezone.datetime.strptime('2014-05-02', '%Y-%m-%d').date()).days
-        from math import log
+            try:
+                target_fund.save()
+            except Exception, e:
+                print(e)
 
-        for f in Fund.objects.all():
-            x = f.rate_7_days
-            ra = f.bought_count_random
-            rp = f.bought_amount_random
+    def sync_issuer(self):
+        try:
+            info = self.fetcher.fund_issuers()
+        except FetchException:
+            pass
 
-            if f.rate_7_days <= 5.21:
-                bought_people_count = eval(formular.bought_people_count_le_521)
-            else:
-                bought_people_count = eval(formular.bought_people_count_gt_521)
+        issuers = FundIssuer.objects.all()
+        issuers_dict = {issuer.name: issuer for issuer in issuers}
+        for issuer_info in info:
+            try:
+                target_issuer = issuers_dict[issuer_info['name']]
+                target_issuer.uuid = issuer_info['guid']
+            except KeyError:
+                target_issuer = FundIssuer(name=issuer_info['name'], description=issuer_info['background'] or '',
+                                           home_page=issuer_info['direct_sell_url'] or '', uuid=issuer_info['guid'])
 
-            count = bought_people_count
+            target_issuer.save()
 
-            bought_count = eval(formular.bought_count)
-            bought_amount_per_people = eval(formular.bought_amount_per_people)
-            bought_amount = bought_people_count * bought_amount_per_people
+    def update_issuer(self):
+        try:
+            info = self.fetcher.fund_issuers()
+        except FetchException:
+            pass
 
-            f.bought_amount = bought_amount
-            f.bought_people_count = bought_people_count
-            f.bought_count = bought_count
-            f.save()
+        issuers = FundIssuer.objects.all()
+        issuers_dict = {issuer.uuid: issuer for issuer in issuers}
+        for issuer_info in info:
+            try:
+                target_issuer = issuers_dict[issuer_info['guid']]
+                target_issuer.description= issuer_info['background'] or ''
+                target_issuer.name = issuer_info['name']
+                target_issuer.home_page = issuer_info['direct_sell_url'] or ''
+            except KeyError:
+                target_issuer = FundIssuer(name=issuer_info['name'], description=issuer_info['background'] or '',
+                                           home_page=issuer_info['direct_sell_url'] or '', uuid=issuer_info['guid'])
 
-    except urllib2.URLError, e:
-        print "Reason: ", e.reason
+            target_issuer.save()
+
+
+    def model_setter(self, model, value_dict):
+        for key in value_dict.keys():
+            setattr(model, key, value_dict[key])
+
