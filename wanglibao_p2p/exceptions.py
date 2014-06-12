a5061be33e9787aa80413f7ea67829b28246a6c0
LowID <soloradish@gmail.com>
2014-06-12 21:30:21 +0800

    init p2p model

---
+++ wanglibao_p2p/exceptions.py
@@ -0,0 +1,35 @@
+# encoding: utf-8
+
+
+class RestrictionException(Exception):
+    ErrorMessage = {}
+    __error__ = 'RestrictionException'
+
+    def __init__(self, error_code):
+        self.code = str(error_code)
+
+    def __str__(self):
+        return "<%s Error %s: %s" % (self.__error__, self.code, self.ErrorMessage[self.code])
+
+
+
+class UserRestriction(RestrictionException):
+    ErrorMessage = {
+        '100000': u'不是合法的用户对象',
+        '100001': u'用户余额不足',
+        '100002': u'用户身份证未验证',
+        '100003': u'获取用户资金失败'
+    }
+    __error__ = 'UserRestriction'
+
+
+
+
+class ProductRestriction(RestrictionException):
+    ErrorMessage = {
+        '200000': u'不是合法的产品对象',
+        '200001': u'产品售罄',
+        '200002': u'产品已停售',
+    }
+    __error__ = 'ProductRestriction'
+
--- wanglibao_p2p/models.py
+++ wanglibao_p2p/models.py
@@ -1,9 +1,13 @@
 # encoding: utf8
 import collections
+from decimal import Decimal
 from django.db import models
 from django.utils import timezone
 from jsonfield import JSONField
 from wanglibao.models import ProductBase
+from django.contrib.auth import get_user_model
+
+user_model = get_user_model()
 
 
 class P2PProduct(ProductBase):
@@ -12,11 +16,13 @@ class P2PProduct(ProductBase):
     period = models.IntegerField(default=0, verbose_name=u'产品期限(月)')
     brief = models.TextField(blank=True, verbose_name=u'产品点评')
     expected_earning_rate = models.FloatField(default=0, verbose_name=u'预期收益(%)')
+    status = models.CharField(verbose_name=u'产品状态', max_length=20)
+    closed = models.BooleanField(verbose_name=u'是否完结', default=False)
 
     pay_method = models.CharField(max_length=32, verbose_name=u'还款方式')
 
     total_amount = models.BigIntegerField(default=0, verbose_name=u'借款总额')
-    ordered_amount = models.BigIntegerField(default=0, verbose_name=u'以募集金额')
+    ordered_amount = models.BigIntegerField(default=0, verbose_name=u'已募集金额')
 
     extra_data = JSONField(blank=True, load_kwargs={'object_pairs_hook': collections.OrderedDict})
 
@@ -24,6 +30,7 @@ class P2PProduct(ProductBase):
     end_time = models.DateTimeField(default=timezone.now, verbose_name=u'终止时间')
 
     type = models.CharField(max_length=32, verbose_name=u'产品类型')
+    limit_per_user = models.FloatField(verbose_name=u'单用户购买限额', default=0.2)
 
     def __unicode__(self):
         return u'%s %f' % (self.name, self.expected_earning_rate)
@@ -38,3 +45,47 @@ class Warrant(models.Model):
 
     def __unicode__(self):
         return u'%s %s %s' % (self.product.name, self.name, str(self.warranted_at))
+
+
+class TradeRecordType(models.Model):
+    name = models.CharField(max_length=10, help_text=u'类型')
+    description = models.CharField(max_length=200, help_text=u'类型说明')
+
+
+class TradeRecord(models.Model):
+    type = models.ForeignKey(TradeRecordType, help_text=u'流水类型')
+    serial_number = models.IntegerField(verbose_name=u'序列号')
+    amount = models.DecimalField(verbose_name=u'发生数', max_digits=20, decimal_places=2)
+
+    product = models.ForeignKey(P2PProduct, help_text=u'标的产品')
+    product_balance_before = models.IntegerField(verbose_name=u'标的前余额', help_text=u'该笔流水发生前标的剩余量')
+    product_balance_after = models.IntegerField(verbose_name=u'标的后余额', help_text=u'该笔流水发生后标的剩余量')
+
+    user = models.ForeignKey(user_model)
+    user_balance_before = models.DecimalField(verbose_name=u'用户前余额', help_text=u'该笔流水发生前用户余额', max_digits=20,
+                                              decimal_places=3)
+    user_balance_after = models.DecimalField(verbose_name=u'用户后余额', help_text=u'该笔流水发生后用户余额', max_digits=20,
+                                             decimal_places=3)
+
+    cancelable = models.BooleanField(verbose_name=u'可撤单', default=False)
+
+    operation_ip = models.IPAddressField(verbose_name=u'流水ip')
+    operation_request_headers = models.TextField(verbose_name=u'流水请求信息', max_length=1000,
+                                                 help_text=u'存储流水产生时request headers信息')
+
+    create_time = models.DateTimeField(verbose_name=u'发生时间', auto_now_add=True)
+
+    def __unicode__(self):
+        return u'流水号%s %s 发生金额%s' %(self.serial_number, self.type.name, self.amount)
+
+
+class UserMargin(models.Model):
+    user = models.OneToOneField(get_user_model(), primary_key=True)
+    balance = models.DecimalField(verbose_name=u'用户余额', max_digits=20, decimal_places=2, default=Decimal('0.00'))
+    freeze = models.DecimalField(verbose_name=u'冻结金额', max_digits=20, decimal_places=2, default=Decimal('0.00'))
+
+    def has_margin(self, amount):
+        amount = Decimal(amount)
+        if amount >= self.balance:
+            return True
+        return False
