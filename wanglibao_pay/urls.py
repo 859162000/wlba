cf9bd6a3b6558a42bda7ddcc12be2e9713190bf1
Xihui He <xihuihe@gmail.com>
2014-06-17 14:39:11 +0800

    withdraw initial version

--- wanglibao_pay/urls.py
+++ wanglibao_pay/urls.py
@@ -1,8 +1,12 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
-from wanglibao_pay.views import BankListView, PayCallback
+from django.views.generic import TemplateView
+from wanglibao_pay.views import BankListView, PayCallback, WithdrawCompleteView, WithdrawCallback
 
 urlpatterns = patterns('',
     url(r'^banks/$', login_required(BankListView.as_view()), name='pay-banks'),
-    url(r'^callback/', PayCallback.as_view(), name='pay-callback'),
+    url(r'^withdraw/$', TemplateView.as_view(template_name="withdraw.jade"), name='withdraw'),
+    url(r'^withdraw/complete/$', WithdrawCompleteView.as_view(), name='withdraw-complete'),
+    url(r'^pay/callback/', PayCallback.as_view(), name='pay-callback'),
+    url(r'^withdraw/callback/', WithdrawCallback.as_view(), name='withdraw-callback'),
 )
--- wanglibao_pay/views.py
+++ wanglibao_pay/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
@@ -7,23 +8,15 @@ from rest_framework.views import APIView
 from wanglibao_buy.models import Bank
 from wanglibao_pay.huifu_pay import HuifuPay
 from wanglibao_pay.models import PayInfo
+import requests
+import xml.etree.ElementTree as ET
 
 
 class BankListView(TemplateView):
     template_name = 'pay_banks.jade'
 
     def get_context_data(self, **kwargs):
-        order_id = self.request.GET.get('order_id')
-        post = {'OrdId': order_id,
-                'OrdAmt': '500.00',
-                'GateId': '09'}
-        pay = HuifuPay()
-        form = pay.pay(post)
-
-        return {
-            'banks': Bank.objects.all(),
-            'form': form
-        }
+        return dict(banks=Bank.objects.all())
 
 
 class Pay(APIView):
@@ -49,11 +42,48 @@ class Pay(APIView):
         })
 
 
+class WithdrawCompleteView(TemplateView):
+    template_name = 'withdraw_complete.jade'
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        amount = request.POST.get('amount', '')
+        pay_info = PayInfo()
+        pay_info.amount = float(amount)
+        pay_info.type = 'W'
+        pay_info.user = request.user
+        pay_info.save()
+
+        post = dict()
+        post['OrdId'] = str(pay_info.pk)
+        post['OrdAmt'] = '500.00'
+        post['AcctName'] = u'用户'
+        post['BankId'] = 'CCB'
+        post['AcctId'] = '62225888'
+        post['PrPurpose'] = 'p2p'
+
+        pay = HuifuPay()
+        form = pay.withdraw(post)
+        r = requests.post(form['url'], form['post'])
+        root = ET.fromstring(r.text.encode('utf-8'))
+        result = root.find('result')
+        data = dict()
+        pay = HuifuPay()
+        for child in result:
+            data[child.tag] = child.text
+        if pay.verify_sign(data, HuifuPay.WITHDRAW_FIELDS) and data['RespCode'] == '000':
+            context['result'] = u'提款成功'
+        else:
+            context['result'] = u'提款失败'
+        return self.render_to_response(context)
+
+
 class PayCallback(View):
     def post(self, request, *args, **kwargs):
         pay = HuifuPay()
-        if pay.verify_sign(request.POST.dict()):
+        if pay.verify_sign(request.POST.dict(), HuifuPay.PAY_FIELDS):
             order_id = request.POST.get('OrdId', '')
+            print 'order ID:', order_id
             return HttpResponse('RECV_ORD_ID_' + order_id)
         else:
             return HttpResponseBadRequest()
@@ -61,4 +91,18 @@ class PayCallback(View):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return super(PayCallback, self).dispatch(request, *args, **kwargs)
+
+
+class WithdrawCallback(View):
+    def post(self, request, *args, **kwargs):
+        pay = HuifuPay()
+        if pay.verify_sign(request.POST.dict(), HuifuPay.WITHDRAW_FIELDS):
+            order_id = request.POST.get('OrdId', '')
+            return HttpResponse('RECV_ORD_ID_' + order_id)
+        else:
+            return HttpResponseBadRequest()
+
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(WithdrawCallback, self).dispatch(request, *args, **kwargs)
 
