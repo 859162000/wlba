d94775f5cae503f83b880549f6130ca7bc0af64b
Xihui He <xihuihe@gmail.com>
2014-06-17 17:40:15 +0800

    add pay exception handling

--- wanglibao_pay/urls.py
+++ wanglibao_pay/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, url
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
-from wanglibao_pay.views import BankListView, PayCallback, WithdrawCompleteView, WithdrawCallback
+from wanglibao_pay.views import BankListView, PayCallback, WithdrawCompleteView, WithdrawCallback, PayView
 
 urlpatterns = patterns('',
     url(r'^banks/$', login_required(BankListView.as_view()), name='pay-banks'),
     url(r'^withdraw/$', TemplateView.as_view(template_name="withdraw.jade"), name='withdraw'),
     url(r'^withdraw/complete/$', WithdrawCompleteView.as_view(), name='withdraw-complete'),
-    url(r'^pay/callback/', PayCallback.as_view(), name='pay-callback'),
     url(r'^withdraw/callback/', WithdrawCallback.as_view(), name='withdraw-callback'),
+    url(r'^deposit/', PayView.as_view(), name='deposit-view'),
+    url(r'^deposit/callback/', PayCallback.as_view(), name='deposit-callback'),
 )
--- wanglibao_pay/views.py
+++ wanglibao_pay/views.py
@@ -1,16 +1,18 @@
 # -*- coding: utf-8 -*-
+import logging
+import socket
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import TemplateView, View
-from rest_framework.response import Response
-from rest_framework.views import APIView
 from wanglibao_buy.models import Bank
-from wanglibao_pay.huifu_pay import HuifuPay
+from wanglibao_pay.huifu_pay import HuifuPay, SignException
 from wanglibao_pay.models import PayInfo
 import requests
 import xml.etree.ElementTree as ET
+import decimal
 
+logger = logging.getLogger(__name__)
 
 class BankListView(TemplateView):
     template_name = 'pay_banks.jade'
@@ -21,34 +23,55 @@ class BankListView(TemplateView):
         }
 
 
-class Pay(APIView):
-    permission_classes = ()
-    def post(self, request):
-        amount = request.DATA.get('amount', '')
-        gate_id = request.DATA.get('gate_id', '')
-        pay_info = PayInfo()
-        pay_info.amount = float(amount)
-        pay_info.type = 'D'
-        pay_info.user = request.user
-        pay_info.save()
-        post = {
-            'OrdId': pay_info.pk,
-            'GateId': gate_id,
-            'OrdAmt': amount
-        }
-        pay = HuifuPay()
-        form = pay.pay(post)
+class PayView(TemplateView):
+    template_name = ''
+    TWO_PLACES = decimal.Decimal(10) ** -2
 
-        return Response({
+    def post(self, request):
+        try:
+            amount_str = request.DATA.get('amount', '')
+            amount = decimal.Decimal(amount_str).\
+                quantize(PayView.TWO_PLACES, context=decimal.Context(traps=[decimal.Inexact]))
+            amount_str = str(amount)
+            #todo: validate gate id
+            gate_id = request.DATA.get('gate_id', '')
+
+            pay_info = PayInfo()
+            pay_info.amount = amount
+            pay_info.type = 'P'
+            pay_info.status = PayInfo.INITIAL
+            pay_info.user = request.user
+            pay_info.save()
+            post = {
+                'OrdId': pay_info.pk,
+                'GateId': gate_id,
+                'OrdAmt': amount_str
+            }
+            pay = HuifuPay()
+            form = pay.pay(post)
+            pay_info.request = str(form)
+            pay_info.status = PayInfo.PROCESSING
+            pay_info.save()
+        except decimal.DecimalException:
+            message = u'金额格式错误'
+        except (socket.error, SignException) as e:
+            message = u'系统内部错误,请重试'
+            pay_info.status = PayInfo.FAIL
+            pay_info.error_message = e.message
+            pay_info.save()
+            logger.fatal('sign error! order id: ' + str(pay_info.pk) + ' ' + e.message)
+
+        context = {
+            'message': message,
             'form': form
-        })
+        }
+        return self.render_to_response(context)
 
 
 class WithdrawCompleteView(TemplateView):
     template_name = 'withdraw_complete.jade'
 
     def post(self, request, *args, **kwargs):
-        context = self.get_context_data(**kwargs)
         amount = request.POST.get('amount', '')
         pay_info = PayInfo()
         pay_info.amount = float(amount)
@@ -66,13 +89,15 @@ class WithdrawCompleteView(TemplateView):
 
         pay = HuifuPay()
         form = pay.withdraw(post)
+
         r = requests.post(form['url'], form['post'])
         root = ET.fromstring(r.text.encode('utf-8'))
         result = root.find('result')
         data = dict()
-        pay = HuifuPay()
         for child in result:
             data[child.tag] = child.text
+        context = dict()
+        pay = HuifuPay()
         if pay.verify_sign(data, HuifuPay.WITHDRAW_FIELDS) and data['RespCode'] == '000':
             context['result'] = u'提款成功'
         else:
@@ -85,7 +110,10 @@ class PayCallback(View):
         pay = HuifuPay()
         if pay.verify_sign(request.POST.dict(), HuifuPay.PAY_FIELDS):
             order_id = request.POST.get('OrdId', '')
-            print 'order ID:', order_id
+            pay_info = PayInfo.objects.get(pk=order_id)
+            print str(request.POST.dict())
+            pay_info.status = PayInfo.SUCCESS
+            pay_info.save()
             return HttpResponse('RECV_ORD_ID_' + order_id)
         else:
             return HttpResponseBadRequest()
