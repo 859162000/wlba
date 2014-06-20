c3be94cd31e6f9113b234ceec579a85a42049079
LowID <soloradish@gmail.com>
2014-06-20 14:15:58 +0800

    draft wanglibao_margin

--- wanglibao_margin/admin.py
+++ wanglibao_margin/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
-
+from models import Margin, MarginRecord
 # Register your models here.
 class UserMarginAdmin(admin.ModelAdmin):
     list_display = ('user', 'margin', 'freeze', 'withdrawing', )
 
 
 class MarginRecordAdmin(admin.ModelAdmin):
-    list_display = ('catalog', 'user', 'amount', 'description', 'checksum', 'trust')
+    list_display = ('catalog', 'user', 'amount', 'description', )
+
+admin.site.register(Margin, UserMarginAdmin)
+admin.site.register(MarginRecord, MarginRecordAdmin)
---
+++ wanglibao_margin/exceptions.py
@@ -0,0 +1,10 @@
+class MarginException(Exception):
+    pass
+
+
+class MarginLack(MarginException):
+    pass
+
+
+class MarginNotExist(MarginException):
+    pass
