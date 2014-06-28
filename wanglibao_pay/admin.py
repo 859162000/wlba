c58766be819fd37419f243cdbe4d4705b5eedec6
Xihui He <xihuihe@gmail.com>
2014-06-28 20:37:47 +0800

    fix add bank permission issue

--- wanglibao_pay/admin.py
+++ wanglibao_pay/admin.py
@@ -1,8 +1,33 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
 # Register your models here.
 from wanglibao_pay.models import Bank, PayInfo, Card
 
-admin.site.register(Bank)
-admin.site.register(Card)
-admin.site.register(PayInfo)
+
+class PayInfoAdmin(admin.ModelAdmin):
+    list_display = ('get_phone', 'type', 'total_amount', 'fee', 'bank', 'card', 'status', 'create_time')
+    search_fields = ['user__wanglibaouserprofile__phone', 'card__no']
+
+    def get_phone(self, obj):
+        return obj.user.wanglibaouserprofile.phone
+
+    get_phone.short_description = u'手机'
+
+
+class BankAdmin(admin.ModelAdmin):
+    list_display = ('name', 'gate_id', 'code')
+
+
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('get_phone', 'no', 'bank')
+    search_fields = ['user__wanglibaouserprofile__phone', 'no']
+
+    def get_phone(self, obj):
+        return obj.user.wanglibaouserprofile.phone
+
+    get_phone.short_description = u'手机'
+
+admin.site.register(Bank, BankAdmin)
+admin.site.register(Card, CardAdmin)
+admin.site.register(PayInfo, PayInfoAdmin)
