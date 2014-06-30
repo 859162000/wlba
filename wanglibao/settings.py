5ec000a78782842be507527a87d0621bad3caa62
Xihui He <xihuihe@gmail.com>
2014-06-30 15:16:39 +0800

    use test pay env in staging

--- wanglibao/settings.py
+++ wanglibao/settings.py
@@ -381,10 +381,10 @@ CELERY_RESULT_SERIALIZER = 'json'
 ID_VERIFY_USERNAME = 'wljr_admin'
 ID_VERIFY_PASSWORD = 'wljr888'
 
-if not DEBUG:
-    PAY_BACK_RETURN_URL = 'http://111.206.165.46:8000/pay/deposit/callback/'
+if PRODUCTION:
+    PAY_BACK_RETURN_URL = 'http://111.206.165.46/pay/deposit/callback/'
     PAY_RET_URL = 'http://111.206.165.46:8000/pay/deposit/complete/'
-    WITHDRAW_BACK_RETURN_URL = 'http://111.206.165.46:8000/pay/withdraw/callback/'
+    WITHDRAW_BACK_RETURN_URL = 'http://111.206.165.46/pay/withdraw/callback/'
     MER_ID = '872724'
     CUSTOM_ID = '000007522683'
     SIGN_HOST = '192.168.0.12'
@@ -392,9 +392,9 @@ if not DEBUG:
     PAY_URL = 'https://mas.chinapnr.com'
     WITHDRAW_URL = 'https://lab.chinapnr.com/buser'
 else:
-    PAY_BACK_RETURN_URL = 'http://111.206.165.46:8000/pay/deposit/callback/'
+    PAY_BACK_RETURN_URL = 'http://111.206.165.46/pay/deposit/callback/'
     PAY_RET_URL = 'http://111.206.165.46:8000/pay/deposit/complete/'
-    WITHDRAW_BACK_RETURN_URL = 'http://111.206.165.46:8000/pay/withdraw/callback/'
+    WITHDRAW_BACK_RETURN_URL = 'http://111.206.165.46/pay/withdraw/callback/'
     MER_ID = '510672'
     CUSTOM_ID = '000010124821'
     SIGN_HOST = '192.168.0.12'
