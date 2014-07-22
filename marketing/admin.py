eb73e88f68be14c28041bf2e86dfd0b7da36f26d
Shuo Li <shuoli84@gmail.com>
2014-07-22 17:10:19 +0800

    Update admin display

--- marketing/admin.py
+++ marketing/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from marketing.models import NewsAndReport
+from marketing.models import NewsAndReport, SiteData
 
 
 class NewsAndReportAdmin(admin.ModelAdmin):
     list_display = ("name", "link", "score")
 
 
+class SiteDataAdmin(admin.ModelAdmin):
+    list_display = ("id", "invest_threshold", "p2p_total_earning", "p2p_total_trade", "earning_rate", "highest_earning_rate", "demand_deposit_interest_rate", "one_year_interest_rate")
+    list_editable = ("invest_threshold", "p2p_total_earning", "p2p_total_trade", "earning_rate", "highest_earning_rate", "demand_deposit_interest_rate", "one_year_interest_rate")
+    list_display_link = ('id',)
+
+
 admin.site.register(NewsAndReport, NewsAndReportAdmin)
+admin.site.register(SiteData, SiteDataAdmin)
