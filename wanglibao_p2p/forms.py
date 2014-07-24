3d7a2443389868c2dceac52cca2acdc7ee61e97d
leo <zdlm@mail.ustc.edu.cn>
2014-07-24 19:45:52 +0800

    disalbe captcha

--- wanglibao_p2p/forms.py
+++ wanglibao_p2p/forms.py
@@ -6,4 +6,4 @@ from wanglibao_p2p.models import P2PProduct
 class PurchaseForm(forms.Form):
     product = forms.ModelChoiceField(queryset=P2PProduct.objects.all(), widget=forms.HiddenInput())
     amount = forms.IntegerField()
-    captcha = CaptchaField()
+    #captcha = CaptchaField()
