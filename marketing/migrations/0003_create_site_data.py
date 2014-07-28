c0a8841562010426e4ef13f5521ea0736c0b2ff7
Shuo Li <shuoli84@gmail.com>
2014-07-28 23:13:22 +0800

    Fix the data migration, use orm version instead of current version

--- marketing/migrations/0003_create_site_data.py
+++ marketing/migrations/0003_create_site_data.py
@@ -13,7 +13,7 @@ class Migration(DataMigration):
         # Note: Don't use "from appname.models import ModelName". 
         # Use orm.ModelName to refer to models in this application,
         # and orm['appname.ModelName'] for models in other applications.
-        SiteData().save()
+        orm.SiteData().save()
 
     def backwards(self, orm):
         "Write your backwards methods here."
