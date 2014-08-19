54f1a77502bc375d705f38f77e7909f8fae8a803
Shuo Li <shuoli84@gmail.com>
2014-08-19 14:07:56 +0800

    Use 128 favicon, use svg for supported browser

--- wanglibao_account/admin.py
+++ wanglibao_account/admin.py
@@ -57,7 +57,10 @@ class UserProfileAdmin(UserAdmin, ImportExportModelAdmin):
 
 
 def user_unicode(self):
-    return u'[%s] %s %s ' % (str(self.id), self.wanglibaouserprofile.name, self.wanglibaouserprofile.phone)
+    if hasattr(self, 'wanglibaouserprofile'):
+        return u'[%s] %s %s ' % (str(self.id), self.wanglibaouserprofile.name, self.wanglibaouserprofile.phone)
+    else:
+        return u'%s [%s]' % (str(self.id), self.username)
 
 User.__unicode__ = user_unicode
 
