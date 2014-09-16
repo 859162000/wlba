88855fa1fa1c6db6940f4f43cddffc957df37788
rsj217 <rsj217@gmail.com>
2014-09-16 13:38:29 +0800

    change the home p2plist status

--- wanglibao_account/views.py
+++ wanglibao_account/views.py
@@ -257,7 +257,11 @@ class AccountHome(TemplateView):
 
 
         # Followings for p2p
-        p2p_equities = P2PEquity.objects.filter(user=user).filter(~Q(product__status=u"已完成")).select_related('product')
+        # p2p_equities = P2PEquity.objects.filter(user=user).filter(~Q(product__status=u"已完成")).select_related('product')
+        p2p_equities = P2PEquity.objects.filter(user=user).filter(product__status__in=[
+            u'已完成', u'满标待打款',u'满标已打款', u'满标待审核', u'满标已审核', u'还款中'
+        ]).select_related('product')
+
         amortizations = ProductAmortization.objects.filter(product__in=[e.product for e in p2p_equities], settled=False).prefetch_related("subs")
 
         unpayed_principle = 0
