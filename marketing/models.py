91d78bb68011c12ba3c86ee4e36c6487a409c65c
rsj217 <rsj217@gmail.com>
2014-09-23 19:33:26 +0800

    fix the invite

--- marketing/models.py
+++ marketing/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models.signals import post_save
 from wanglibao_pay.util import get_a_uuid
-
+from django.db import transaction
 
 class NewsAndReport(models.Model):
     name = models.CharField(u'名字', max_length=128)
@@ -41,20 +41,30 @@ class SiteData(models.Model):
     def one_year_times(self):
         return int(self.highest_earning_rate / self.one_year_interest_rate)
 
-
-class PromotionToken(models.Model):
-    user = models.OneToOneField(get_user_model(), primary_key=True)
-    token = models.CharField(u'推广代码', max_length=64, db_index=True, default=get_a_uuid)
-
-    def __unicode__(self):
-        return self.token
-
 class InviteCode(models.Model):
     code = models.CharField(u'邀请码', max_length=6, db_index=True, unique=True)
     is_used = models.BooleanField(u'是否使用', default=False)
 
     def __unicode__(self):
         return self.code
+
+
+def get_a_invitecode():
+    with transaction.atomic():
+        invite_code = InviteCode.objects.select_for_update().filter(is_used=False).first()
+        invite_code.is_used = True
+        invite_code.save()
+        return invite_code.code
+
+
+
+class PromotionToken(models.Model):
+    user = models.OneToOneField(get_user_model(), primary_key=True)
+    token = models.CharField(u'推广代码', max_length=64, db_index=True, default=get_a_invitecode)
+
+    def __unicode__(self):
+        return self.token
+
 
 class IntroducedBy(models.Model):
     user = models.ForeignKey(get_user_model())
@@ -80,3 +90,7 @@ class TimelySiteData(models.Model):
     freeze_amount = models.DecimalField(u'投资中冻结金额', max_digits=20, decimal_places=2, default=0)
     total_amount = models.DecimalField(u'总额', max_digits=20, decimal_places=2, default=0)
     user_count = models.IntegerField(u'用户总数', default=0)
+
+
+
+
