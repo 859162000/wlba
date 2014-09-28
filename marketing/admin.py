49d6a90d818b618d17f663f0238553ac7f717b27
rsj217 <rsj217@gmail.com>
2014-09-28 17:52:22 +0800

    add the export

--- marketing/admin.py
+++ marketing/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
+from import_export import fields
 from marketing.models import NewsAndReport, SiteData, PromotionToken, IntroducedBy, TimelySiteData
 
 class NewsAndReportAdmin(admin.ModelAdmin):
@@ -19,8 +20,17 @@ class PromotionTokenAdmin(admin.ModelAdmin):
 
 class IntroducedByResource(resources.ModelResource):
 
+    user_name = fields.Field(attribute="user__wanglibaouserprofile__name")
+    user_phone = fields.Field(attribute="user__wanglibaouserprofile__phone")
+    introduce_name = fields.Field(attribute="introduced_by__wanglibaouserprofile__name")
+    introduce_phone = fields.Field(attribute="introduced_by__wanglibaouserprofile__phone")
+
     class Meta:
         model = IntroducedBy
+        fields = ('user_name', 'user_phone','introduce_name', 'introduce_phone',
+                  'created_at', 'bought_at', 'gift_send_at' )
+
+
 
 
 class IntroducedByAdmin(ImportExportModelAdmin):
