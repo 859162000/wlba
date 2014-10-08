4d3faceea780512b27871c2da095f1961876b365
hetaov <v.tao.he@gmail.com>
2014-10-08 11:10:36 +0800

    modify

--- marketing/views.py
+++ marketing/views.py
@@ -2,7 +2,7 @@ from datetime import date, timedelta, datetime
 from collections import defaultdict
 
 from django.views.generic import  TemplateView
-from django.db.models import Count, F
+from django.db.models import Count, F, Sum
 from wanglibao_profile.models import WanglibaoUserProfile
 from django.contrib.auth.models import User
 from wanglibao_buy.models import TradeHistory
@@ -19,10 +19,14 @@ class MarketingView(TemplateView):
         d0 = date(2014, 3, 01)
         d1 = date.today()
 
-        users = User.objects.filter(date_joined__range=(d0, d1)).extra({'each_day': 'date(date_joined)'}).values('each_day').annotate(joined_num=Count('id'))
-        trades = TradeHistory.objects.extra({'each_day': 'date(create_date)'}).filter(business_type='022', create_date__range=(d0, d1), user__date_joined=F('create_date')).values('each_day').annotate(trade_num=Count('id'))
+        users = User.objects.filter(date_joined__range=(d0, d1)).order_by('each_day')\
+            .extra({'each_day': 'date(date_joined)'}).values('each_day')\
+            .annotate(joined_num=Count('id'))
 
-        days = [d0 + timedelta(days=x) for x in range((d1-d0).days + 1)]
+
+        trades = TradeHistory.objects.filter(business_type='022', create_date__range=(d0, d1)).order_by('each_day')\
+            .extra({'each_day': 'date(create_date)'}).values('each_day')\
+            .annotate(trade_num=Count('id'), amount=Sum('shares'))
 
         d = defaultdict(dict)
 
@@ -30,11 +34,16 @@ class MarketingView(TemplateView):
             for elem in l:
                 d[elem['each_day']].update(elem)
 
-        result = d.values()
+        dd = d.values()
+
+        result = sorted(dd, key=lambda x: x['each_day'])
+
+        print result
+
+        #result = d.values()
 
 
         return {
-            'all': result,
-            'trades': trades,
-            'days': days
+            'result': result,
+            'users': users
         }
