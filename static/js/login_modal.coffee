c8c0ae5a2abbb5411136c2227abf5d024a7743d1
leo <zdlm@mail.ustc.edu.cn>
2014-10-09 18:40:57 +0800

    change to status for 429

--- static/js/login_modal.coffee
+++ static/js/login_modal.coffee
@@ -189,7 +189,8 @@ require ['jquery', 'lib/modal', 'lib/backend', 'jquery.validate', "tools", 'jque
           $(element).addClass 'button-red'
           $(element).removeClass 'button-gray'
           result = JSON.parse xhr.responseText
-          if xhr.errorCode == 429
+          if xhr.status == 429
+
             tool.modalAlert({title: '温馨提示', msg: "访问过于频繁，请稍候重试", callback_ok: _showModal})
           else
             tool.modalAlert({title: '温馨提示', msg: result.message, callback_ok: _showModal})
--- static/js/login_modal.js
+++ static/js/login_modal.js
@@ -215,11 +215,19 @@
           $(element).addClass('button-red');
           $(element).removeClass('button-gray');
           result = JSON.parse(xhr.responseText);
-          return tool.modalAlert({
-            title: '温馨提示',
-            msg: result.message,
-            callback_ok: _showModal
-          });
+          if (xhr.status === 429) {
+            return tool.modalAlert({
+              title: '温馨提示',
+              msg: "访问过于频繁，请稍候重试",
+              callback_ok: _showModal
+            });
+          } else {
+            return tool.modalAlert({
+              title: '温馨提示',
+              msg: result.message,
+              callback_ok: _showModal
+            });
+          }
         });
         intervalId;
         count = 60;
