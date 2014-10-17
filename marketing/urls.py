2d4e5e6dabb589b327e7ebd31361549e4b3106f5
LiLi <249143979@qq.com>
2014-10-17 17:20:53 +0800

    add  activity_invite page

--- marketing/urls.py
+++ marketing/urls.py
@@ -4,6 +4,6 @@ from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = patterns(
     '',
-    url(r'^invite/$', TemplateView.as_view(template_name="activity.jade")),
+    url(r'^invite/$', TemplateView.as_view(template_name="activity_invite.jade")),
     url(r'^xunlei/$', TemplateView.as_view(template_name="xunlei.jade")),
-    )
+)
