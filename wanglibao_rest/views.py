5376639b1c90afa8597d8bedbd356d5965e4541c
leo <zdlm@mail.ustc.edu.cn>
2014-10-18 22:12:32 +0800

    change register api

--- wanglibao_rest/views.py
+++ wanglibao_rest/views.py
@@ -103,11 +103,11 @@ class RegisterAPIView(APIView):
                 try:
                     PromotionToken.objects.get(token=invite_code)
                 except:
-                    return Response(u"邀请码错误", status=status.HTTP_200_OK)
+                    return Response({"non_field_errors": u"邀请码错误"}, status=status.HTTP_200_OK)
             user = create_user(serializer.object['identifier'], serializer.object['password'], "")
             if invite_code:
                 set_promo_user(request, user, invitecode=invite_code)
-            return Response({'message': '注册成功'})
+            return Response(u'注册成功', status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_200_OK)
 
 
