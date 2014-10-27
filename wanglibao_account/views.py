8ec40e0466f94cb7cbd42a359ea86e37bf309eba
zhenjing <zhenjinglee@gmail.com>
2014-10-27 18:59:09 +0800

    fix xunlei bind bug

--- wanglibao_account/views.py
+++ wanglibao_account/views.py
@@ -720,6 +720,7 @@ class Third_login_back(View):
         result = third_login.login_back(request.GET, request.user)
         if result['ret_code']:
             return HttpResponse(result['message'])
+        return HttpResponse(result['data'])
         return HttpResponseRedirect(result['url'])
 
 @sensitive_post_parameters()
