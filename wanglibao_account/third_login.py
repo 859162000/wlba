76428dcfb9d2fd046df0081c9f11423f6270bb03
zhenjing <zhenjinglee@gmail.com>
2014-10-31 11:52:32 +0800

    fix

--- wanglibao_account/third_login.py
+++ wanglibao_account/third_login.py
@@ -53,6 +53,7 @@ def login_back(args, user):
                 return {"ret_code":30034, "message":content}
             userinfo = json.loads(content)
 
+            """
             tmpuser = Binding.objects.filter(user=user).filter(btype=state).first()
             #绑定过的不再绑定,一对一关系
             if tmpuser and tmpuser.bid != userinfo['uid']:
@@ -83,8 +84,89 @@ def login_back(args, user):
             bindinfo.refresh_token = dic['refresh_token']
             bindinfo.created_at = long(time.time())
             bindinfo.save()
-            return {"ret_code":0, "message":"ok", "data":userinfo, "url":"/accounts/home/"}
+            """
+
+            rs = _bind_account(user, state, userinfo)
+            if rs:
+                return {"ret_code":0, "message":"ok", "data":userinfo, "url":"/accounts/home/"}
+            else:
+                return {"ret_code":30034, "message":"server error"}
         else:
             return {"ret_code":30033, "message":content}
     else:
         return {"ret_code":30032, "message":"state error"}
+
+#检查迅雷账号VIP
+def check_xunlei(dic):
+    state = "xunlei"
+    uri = "http://developer.open-api-auth.xunlei.com/get_user_info?"
+    params = {"client_id":partner[state]['client_id'], "scope":"get_user_info", "access_token":dic['access_token']}
+    url = uri + urllib.urlencode(params)
+    http = httplib2.Http()
+    response, content = http.request(url, 'GET')
+    if str(response['status']) != "200":
+        return {"ret_code":30061, "message":content}
+
+    userinfo = json.loads(content)
+    if userinfo['result'] != 200:
+        #刷新token
+        uri_ref = "https://open-api-auth.xunlei.com/auth2/token?"
+        params_ref = {"grant_type":"refresh_token", "refresh_token":dic['refresh_token'],
+                    "client_id":partner["xunlei"]["client_id"], "client_secret":partner["xunlei"]["secret_key"]}
+        url2 = uri_ref + urllib.urlencode(params_ref)
+        response, content = http.request(url2, 'GET')
+        if str(response['status']) != "200":
+            return {"ret_code":30062, "message":content}
+        dic = json.loads(content)
+        if dic["result"] != 200:
+            return {"ret_code":30063, "message":"token error"}
+
+        #刷新完token,重新获取用户信息
+        params['access_token'] = dic['access_token']
+        url = uri + urllib.urlencode(params)
+        response, content = http.request(url, 'GET')
+        if str(response['status']) != "200":
+            return {"ret_code":30064, "message":content}
+        userinfo = json.loads(content)
+        if userinfo['result'] != 200:
+            return {"ret_code":30065, "message":content}
+
+    rs = _bind_account(user, state, userinfo)
+    if rs:
+        return {"ret_code":0, "message":"ok", "data":userinfo, "url":"/accounts/home/"}
+    else:
+        return {"ret_code":30066, "message":"server error"}
+
+#内部方法，绑定和检查用户关系
+def _bind_account(user, state, userinfo):
+    tmpuser = Binding.objects.filter(user=user).filter(btype=state).first()
+    #绑定过的不再绑定,一对一关系
+    if tmpuser and tmpuser.bid != userinfo['uid']:
+        return True
+
+    bindinfo = Binding.objects.filter(bid=userinfo['uid']).filter(btype=state).first()
+    if bindinfo:
+        if bindinfo.user != user:
+            return True
+    else:
+        bindinfo = Binding()
+        bindinfo.user = user
+    bindinfo.btype = state
+    bindinfo.bid = userinfo['uid']
+    bindinfo.bname = userinfo['nickname']
+    if userinfo['gender'] == "f":
+        bindinfo.gender = "w"
+    elif userinfo['gender'] == "m":
+        bindinfo.gender = "m"
+    else:
+        bindinfo.gender = "n"
+    if str(userinfo['isvip']) == "0":
+        bindinfo.isvip = False
+    else:
+        bindinfo.isvip = True
+    bindinfo.extra = '{"level":%s}' % userinfo['level']
+    bindinfo.access_token = dic['access_token']
+    bindinfo.refresh_token = dic['refresh_token']
+    bindinfo.created_at = long(time.time())
+    bindinfo.save()
+    return True
