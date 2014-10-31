3ed75c348c1a0a70bc4e05407a0408ce32fab6d9
zhenjing <zhenjinglee@gmail.com>
2014-10-31 11:32:30 +0800

    fix bank list

--- wanglibao_rest/urls.py
+++ wanglibao_rest/urls.py
@@ -20,7 +20,7 @@ from wanglibao_hotlist.views import HotTrustViewSet, HotFundViewSet, MobileHotTr
 from wanglibao_p2p.views import PurchaseP2P, PurchaseP2PMobile, P2PProductViewSet, RecordView, \
     P2PProductDetailView, P2PProductListView
 from wanglibao_pay.views import (CardViewSet, LianlianAppPayView, LianlianAppPayCallbackView,
-                            BankCardAddView, BankCardListView, BankCardDelView, BankListView)
+                            BankCardAddView, BankCardListView, BankCardDelView, BankListAPIView)
 from wanglibao_portfolio.views import PortfolioViewSet, ProductTypeViewSet
 from wanglibao_preorder.views import PreOrderViewSet
 from wanglibao_profile.views import ProfileView
@@ -95,7 +95,7 @@ urlpatterns = patterns(
     url(r'^bank_card/add/$', BankCardAddView.as_view()),
     url(r'^bank_card/list/$', BankCardListView.as_view()),
     url(r'^bank_card/del/$', BankCardDelView.as_view()),
-    url(r'^bank/list/$', BankListView.as_view()),
+    url(r'^bank/list/$', BankListAPIView.as_view()),
 
     url(r'^id_validate/', IdValidate.as_view()),
     url(r'^admin_id_validate/', AdminIdValidate.as_view()),
