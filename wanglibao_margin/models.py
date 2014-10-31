5e291d4dbc91f9966f7dc08f82920b8a36d219f4
zhenjing <zhenjinglee@gmail.com>
2014-10-31 17:53:12 +0800

    test

--- wanglibao_margin/models.py
+++ wanglibao_margin/models.py
@@ -1,14 +1,15 @@
 # encoding: utf-8
 from decimal import Decimal
 from django.db import models
-from django.contrib.auth import get_user_model
+#from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 
 # Create your models here.
 class Margin(models.Model):
-    user = models.OneToOneField(get_user_model(), primary_key=True)
+    user = models.OneToOneField(User, primary_key=True)
     margin = models.DecimalField(verbose_name=u'用户余额', max_digits=20, decimal_places=2, default=Decimal('0.00'))
     freeze = models.DecimalField(verbose_name=u'冻结金额', max_digits=20, decimal_places=2, default=Decimal('0.00'))
     withdrawing = models.DecimalField(verbose_name=u'提款中金额', max_digits=20, decimal_places=2,
@@ -27,7 +28,7 @@ class Margin(models.Model):
 class MarginRecord(models.Model):
     catalog = models.CharField(verbose_name=u'流水类型', max_length=100)
     order_id = models.IntegerField(verbose_name=u'相关订单编号', null=True)
-    user = models.ForeignKey(get_user_model(), on_delete=models.SET_NULL, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     create_time = models.DateTimeField(verbose_name=u'流水时间', auto_now_add=True)
 
     amount = models.DecimalField(verbose_name=u'发生金额', max_digits=20, decimal_places=2)
