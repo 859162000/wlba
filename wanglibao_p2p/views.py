b54c94643f2eb207a06396203be4828b096071d9
rsj217 <rsj217@gmail.com>
2014-11-03 11:36:40 +0800

    add the xunlei test

--- wanglibao_p2p/views.py
+++ wanglibao_p2p/views.py
@@ -102,6 +102,77 @@ class P2PDetailView(TemplateView):
         return context
 
 
+from django.contrib.auth.decorators import permission_required
+from django.utils.decorators import method_decorator
+
+class TestP2PDetailView(TemplateView):
+    template_name = "test_p2p_detail.jade"
+
+    def get_context_data(self, id, **kwargs):
+        context = super(TestP2PDetailView, self).get_context_data(**kwargs)
+
+        try:
+            p2p = P2PProduct.objects.select_related('activity').get(pk=id, hide=False)
+            form = PurchaseForm(initial={'product': p2p})
+
+            if p2p.soldout_time:
+                end_time = p2p.soldout_time
+            else:
+                end_time = p2p.end_time
+        except P2PProduct.DoesNotExist:
+            raise Http404(u'您查找的产品不存在')
+
+        terms = get_amortization_plan(p2p.pay_method).generate(p2p.total_amount,
+                                                               p2p.expected_earning_rate/100,
+                                                               p2p.amortization_count,
+                                                               p2p.period)
+        total_earning = terms.get("total") - p2p.total_amount
+
+        total_fee_earning = 0
+
+        if p2p.activity:
+            total_fee_earning = Decimal(p2p.total_amount*p2p.activity.rule.rule_amount*(Decimal(p2p.period)/Decimal(12))).quantize(Decimal('0.01'))
+
+        user = self.request.user
+        current_equity = 0
+
+        if user.is_authenticated():
+            equity_record = p2p.equities.filter(user=user).first()
+            if equity_record is not None:
+                current_equity = equity_record.equity
+
+            xunlei_vip = Binding.objects.filter(user=user).filter(btype='xunlei').first()
+            context.update({
+                'xunlei_vip': xunlei_vip
+            })
+
+        orderable_amount = min(p2p.limit_amount_per_user - current_equity, p2p.remain)
+
+        site_data = SiteData.objects.all()[0]
+
+        context.update({
+            'p2p': p2p,
+            'form': form,
+            'end_time': end_time,
+            'orderable_amount': orderable_amount,
+            'total_earning': total_earning,
+            'current_equity': current_equity,
+            'site_data': site_data,
+            'attachments': p2p.attachment_set.all(),
+            'announcements': AnnouncementP2P,
+            'total_fee_earning': total_fee_earning
+        })
+
+        return context
+
+    @method_decorator(permission_required('wanglibao_p2p.view_p2pproduct', login_url='/'))
+    def dispatch(self, request, *args, **kwargs):
+        """
+        Only user with view p2pproduct permission can call this view
+        """
+        return super(TestP2PDetailView, self).dispatch(request, *args, **kwargs)
+
+
 class PurchaseP2P(APIView):
     permission_classes = (IsAuthenticated,)
 
