aa685c98ac28ad9845fccca562d58f20fb998dcb
LiLi <249143979@qq.com>
2014-11-04 09:25:52 +0800

    add contract

--- wanglibao_account/views.py
+++ wanglibao_account/views.py
@@ -937,7 +937,7 @@ class UserProductContract(APIView):
 @login_required
 def test_contract(request, equity_id):
     equity = P2PEquity.objects.filter(id=equity_id).prefetch_related('product').first()
-    return HttpResponse(generate_contract(equity, 'hongmu_template.jade'))
+    return HttpResponse(generate_contract(equity, 'tongchenghuodi_template.jade'))
 
 
 class IdVerificationView(TemplateView):
