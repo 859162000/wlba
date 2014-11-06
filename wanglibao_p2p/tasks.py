096ef72520bfcbfb42a2c48aaec4622623dc6027
rsj217 <rsj217@gmail.com>
2014-11-06 10:44:20 +0800

    fix the bug about xunlei tast

--- wanglibao_p2p/tasks.py
+++ wanglibao_p2p/tasks.py
@@ -42,6 +42,8 @@ def build_earning(product_id):
 
     #把收益数据插入earning表内
     for obj in earning:
+        if obj.get('user') == 13933:
+            continue
 
         bind = Binding.objects.filter(user_id=obj.get('user')).first()
         if bind and bind.isvip:
