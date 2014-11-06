7175d4ff6f85e91e27497aa0e4e7dcf5c944b73d
rsj217 <rsj217@gmail.com>
2014-11-06 10:39:27 +0800

    fix the bug

--- wanglibao_p2p/urls.py
+++ wanglibao_p2p/urls.py
@@ -1,12 +1,8 @@
 from django.conf.urls import patterns, url
-from views import P2PDetailView, audit_product_view, P2PListView, TestP2PDetailView
+from views import P2PDetailView, audit_product_view, P2PListView
 
 urlpatterns = patterns('',
-    url(r'^detail/(?P<id>46)', TestP2PDetailView.as_view(), name='test p2p detail'),
-    url(r'^detail/(?P<id>47)', TestP2PDetailView.as_view(), name='test p2p detail'),
     url(r'^detail/(?P<id>\w+)', P2PDetailView.as_view(), name='p2p detail'),
     url(r'^audit/(?P<id>\w+)', audit_product_view, name='p2p audit'),
     url(r'^list', P2PListView.as_view(), name='p2p_list'),
-
-
 )
