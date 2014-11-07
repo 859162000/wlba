133cfa5a532d83e7bbaffc50824f1408b53e1896
hetaov <v.tao.he@gmail.com>
2014-11-07 17:21:52 +0800

    wap page

--- marketing/urls.py
+++ marketing/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns(
     url(r'^invite/$', TemplateView.as_view(template_name="activity_invite.jade")),
     url(r'^xunlei/$', TemplateView.as_view(template_name="xunlei.jade")),
     url(r'^gold/$', TemplateView.as_view(template_name="gold.jade")),
+    url(r'^wap/01/$', TemplateView.as_view(template_name="xunlei_01.jade")),
 )
