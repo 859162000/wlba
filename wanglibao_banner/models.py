cccdaec71e6b95eb5d9139fc008166f80fb0a551
LiLi <249143979@qq.com>
2014-11-20 17:48:37 +0800

    update help_text in models

--- wanglibao_banner/models.py
+++ wanglibao_banner/models.py
@@ -44,8 +44,8 @@ class Partner(models.Model):
 
     type = models.CharField(max_length=15, verbose_name=u'类型', choices=TYPES, help_text=u'图片类型')
     name = models.CharField(max_length=30, verbose_name=u'名称', help_text=u'图片名称')
-    link = models.CharField(max_length=200, blank=True, verbose_name=u'链接网址', help_text=u'链接网址')
-    image = models.ImageField(upload_to='banner', blank=True, verbose_name=u'图片', help_text=u'图片')
+    link = models.CharField(max_length=200, blank=True, verbose_name=u'链接网址', default=u'http://', help_text=u'链接网址，需要以http://开头')
+    image = models.ImageField(upload_to='banner', blank=True, verbose_name=u'图片', help_text=u'图片，高度40px最佳')
     alt = models.TextField(blank=True, verbose_name=u'图片说明', help_text=u'图片说明')
     priority = models.IntegerField(verbose_name=u'优先级', help_text=u'越大越优先')
     last_updated = models.DateTimeField(auto_now=True, verbose_name=u'更新时间', help_text=u'上次更新时间')
