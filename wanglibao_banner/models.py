ae6c32a1bfba6bf360277db33371eb26e3c5a277
LiLi <249143979@qq.com>
2014-11-20 16:08:44 +0800

    add the management interface of partner

--- wanglibao_banner/models.py
+++ wanglibao_banner/models.py
@@ -30,3 +30,25 @@ class Banner(models.Model):
     priority = models.IntegerField(verbose_name=u'优先级', help_text=u'越大越优先')
     alt = models.TextField(blank=True, verbose_name=u'图片说明', help_text=u'图片说明')
     last_updated = models.DateTimeField(auto_now=True, verbose_name=u'更新时间', help_text=u'上次更新时间')
+
+
+class Partner(models.Model):
+    class Meta:
+        verbose_name_plural = u'合作伙伴'
+        ordering = ['-priority', '-last_updated']
+
+    TYPES = (
+        ('partner', u'合作伙伴'),
+        ('links', u'友情链接')
+    )
+
+    type = models.CharField(max_length=15, verbose_name=u'类型', choices=TYPES, help_text=u'图片类型')
+    name = models.CharField(max_length=30, verbose_name=u'名称', help_text=u'图片名称')
+    link = models.CharField(max_length=200, blank=True, verbose_name=u'链接网址', help_text=u'链接网址')
+    image = models.ImageField(upload_to='banner', blank=True, verbose_name=u'图片', help_text=u'图片')
+    alt = models.TextField(blank=True, verbose_name=u'图片说明', help_text=u'图片说明')
+    priority = models.IntegerField(verbose_name=u'优先级', help_text=u'越大越优先')
+    last_updated = models.DateTimeField(auto_now=True, verbose_name=u'更新时间', help_text=u'上次更新时间')
+
+    def __unicode__(self):
+        return "%s" % self.name
