ab548c10fbbd095322e64ba7e924d296cdb52e8e
leo <zdlm@mail.ustc.edu.cn>
2015-01-05 12:06:32 +0800

    add show in maintain

--- wanglibao/maintain.py
+++ wanglibao/maintain.py
@@ -20,7 +20,7 @@ def send_award(only_show=True):
     # start = timezone.datetime(2015, 1, 1, 0, 00, 00)
     # end = timezone.datetime(2015, 1, 5, 23, 59, 59)
 
-    p2p_records = P2PRecord.objects.filter(create_time__range=(start, end), catalog='申购').values("user").annotate(
+    p2p_records = P2PRecord.objects.filter(create_time__range=(start, end), catalog=u'申购').values("user").annotate(
         dsum=Sum('amount'))
 
     ban_nian_xunlei = 0
@@ -109,11 +109,97 @@ def send_award(only_show=True):
     print "*********************************"
     print "*********************************"
 
+def show_reward():
+    start = timezone.datetime(2014, 11, 1, 0, 00, 00)
+    end = timezone.datetime(2014, 12, 31, 23, 59, 59)
+
+    p2p_records = P2PRecord.objects.filter(create_time__range=(start, end), catalog=u'申购').values("user").annotate(
+        dsum=Sum('amount'))
+
+    ban_nian_xunlei = 0
+    yi_nian_xunlei = 0
+    huafei = 0
+    huafei_count = 0
+    jd_500 = 0
+    jd_1000 = 0
+    ipad_mini = 0
+    ipad_air = 0
+    iphone_6 = 0
+    iphone_6_plus = 0
+
+    for record in p2p_records:
+        if 1000 <= record["dsum"] < 50000:
+            ban_nian_xunlei += 1
+            print u"给用户%s发放半年迅雷会员第%s个,因为他投资了%s" % (record["user"],ban_nian_xunlei, record["dsum"])
+        if record["dsum"] >= 50000:
+            yi_nian_xunlei += 1
+            print u"给用户%s发放一年迅雷会员第%s个,因为他投资了%s" % (record["user"],yi_nian_xunlei, record["dsum"])
+        if 10000 <= record["dsum"] < 100000:
+            huafei_50 = int(int(record["dsum"]) / 10000) * 50
+            print u"给用户%s发放话费%s元,因为他投资了%s" % (record["user"],huafei_50, record["dsum"])
+            huafei += huafei_50
+            huafei_count += 1
+        if 100000 <= record["dsum"] < 200000:
+            jd_500 += 1
+            print u"给用户%s发放500元京东卡第%s个,因为他投资了%s" % (record["user"],jd_500, record["dsum"])
+        if 200000 <= record["dsum"] < 400000:
+            jd_1000 += 1
+            print u"给用户%s发放1000元京东卡第%s个,因为他投资了%s" % (record["user"],jd_1000, record["dsum"])
+        if 400000 <= record["dsum"] < 500000:
+            ipad_mini += 1
+            print u"给用户%s发放ipad mini 第%s个,因为他投资了%s" % (record["user"],ipad_mini, record["dsum"])
+        if 500000 <= record["dsum"] < 1000000:
+            ipad_air += 1
+            print u"给用户%s发放ipad air 第%s个,因为他投资了%s" % (record["user"],ipad_air, record["dsum"])
+        if 1000000 <= record["dsum"] < 1200000:
+            iphone_6 += 1
+            print u"给用户%s发放iphone6 第%s个,因为他投资了%s" % (record["user"],iphone_6, record["dsum"])
+        if record["dsum"] >= 1200000:
+            iphone_6_plus += 1
+            print u"给用户%s发放iphone6 plus 第%s个,因为他投资了%s" % (record["user"],iphone_6_plus, record["dsum"])
+
+    new_user = IntroducedBy.objects.filter(bought_at__range=(start, end)).exclude(
+        introduced_by__username__startswith="channel").exclude(introduced_by__wanglibaouserprofile__utype__gt=0)
+    amount_05 = 0
+    for first_user in new_user:
+        first_record = P2PRecord.objects.filter(user=first_user.user, create_time__range=(start, end),
+                                                catalog='申购').earliest("create_time")
+        print u"邀请人%s投资金额%s" %(first_user.user.wanglibaouserprofile.name,first_record.amount)
+        if first_record.amount >= 1000:
+            amount_05_one = Decimal(Decimal(first_record.amount) * Decimal(0.005) * (Decimal(first_record.product.period) / Decimal(12))).quantize(Decimal('0.01'), rounding=ROUND_DOWN)
+            print u"%s邀请了%s，活动期间首笔交易%s元，%s获得%s元奖金" % (first_user.introduced_by.wanglibaouserprofile.name, first_user.user.wanglibaouserprofile.name,first_record.amount, first_user.introduced_by.wanglibaouserprofile.name, amount_05_one)
+            amount_05 += amount_05_one
+
+    print "*********************************"
+    print "*********************************"
+    print "*************Done****************"
+    print "*********************************"
+    print "*********************************"
+    print u"发放半年迅雷会员%s个" % ban_nian_xunlei
+    print u"发放一年迅雷会员第%s个" % yi_nian_xunlei
+    print u"发放话费%s元" % huafei
+    print u"发放人数%s" % huafei_count
+    print u"发放500元京东卡%s个" % jd_500
+    print u"发放1000元京东卡%s个" % jd_1000
+    print u"发放ipad mini %s个" % ipad_mini
+    print u"发放ipad air %s个" % ipad_air
+    print u"发放iphone6 %s个" % iphone_6
+    print u"发放iphone6 plus %s个" % iphone_6_plus
+    print u"发放邀请送千5活动，送出 %s元" % amount_05
+    print "*********************************"
+    print "*********************************"
+    print "*************Done****************"
+    print "*********************************"
+    print "*********************************"
+
 
 def reward_user_ban_nian_xunlei(user_id, reward_type, amount,only_show):
     user = User.objects.get(pk=user_id)
     reward = Reward.objects.filter(is_used=False, type=reward_type).first()
-    if only_show is not True:
+    if only_show is not True and reward is None:
+        print user.wanglibaouserprofile.phone
+        print user.wanglibaouserprofile.name
+        return
         reward.is_used = True
         reward.save()
 
@@ -145,7 +231,10 @@ def reward_user_ban_nian_xunlei(user_id, reward_type, amount,only_show):
 def reward_user_yi_nian_xunlei(user_id, reward_type, amount,only_show):
     user = User.objects.get(pk=user_id)
     reward = Reward.objects.filter(is_used=False, type=reward_type).first()
-    if only_show is not True:
+    if only_show is not True and reward is None:
+        print user.wanglibaouserprofile.phone
+        print user.wanglibaouserprofile.name
+        return
         reward.is_used = True
         reward.save()
 
@@ -208,7 +297,10 @@ def reward_user_hua_fei(user_id, reward_type, amount, huafei,only_show):
 def reward_user_jd_500(user_id, reward_type, amount,only_show):
     user = User.objects.get(pk=user_id)
     reward = Reward.objects.filter(is_used=False, type=reward_type).first()
-    if only_show is not True:
+    if only_show is not True and reward is None:
+        print user.wanglibaouserprofile.phone
+        print user.wanglibaouserprofile.name
+        return
         reward.is_used = True
         reward.save()
 
@@ -240,7 +332,10 @@ def reward_user_jd_500(user_id, reward_type, amount,only_show):
 def reward_user_jd_1000(user_id, reward_type, amount,only_show):
     user = User.objects.get(pk=user_id)
     reward = Reward.objects.filter(is_used=False, type=reward_type).first()
-    if only_show is not True:
+    if only_show is not True and reward is None:
+        print user.wanglibaouserprofile.phone
+        print user.wanglibaouserprofile.name
+        return
         reward.is_used = True
         reward.save()
 
