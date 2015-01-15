fbf92cb42365a1af1fefdc2e2dcd9f6e182e5df9
rsj217 <rsj217@gmail.com>
2015-01-15 17:14:11 +0800

    xunleidenglu

--- marketing/urls.py
+++ marketing/urls.py
@@ -22,4 +22,5 @@ urlpatterns = patterns(
     url(r'^newyear/$', NewYearView.as_view(), name="new year"),
     url(r'^shengyan/$', TemplateView.as_view(template_name="shengyan.jade")),
     url(r'^app_shengyan/$', TemplateView.as_view(template_name="shengyan_h5.jade")),
+    url(r'^xunleidenglu/$', TemplateView.as_view(template_name="xunleidenglu.jade")),
 )
