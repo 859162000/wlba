33669648cf87535cb9b0632c7eb3d90b617363f1
hivencent <shadalang@gmail.com>
2015-02-05 18:46:03 +0800

    modify exchange_redpack error_code:30166 message

--- wanglibao_redpack/backends.py
+++ wanglibao_redpack/backends.py
@@ -87,7 +87,7 @@ def exchange_redpack(token, device_type, user):
     if event.give_start_at > now:
         return {"ret_code":30165, "message":"请在%s之后兑换" % local_datetime(event.give_start_at).strftime("%Y-%m-%d %H:%M:%S")}
     elif event.give_end_at < now:
-        return {"ret_code":30166, "message":"请输入有效的兑换码1"}
+        return {"ret_code":30166, "message":"兑换码已过期"}
     if event.target_channel != "":
         ch = helper.which_channel(user)
         if ch != event.target_channel:
