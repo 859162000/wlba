54931cf35d92a067011f793e72988775f0fb5b34
zhenjing <zhenjinglee@gmail.com>
2015-02-27 17:47:18 +0800

    fix bug 1255

--- wanglibao_p2p/views.py
+++ wanglibao_p2p/views.py
@@ -50,7 +50,8 @@ class P2PDetailView(TemplateView):
         context = super(P2PDetailView, self).get_context_data(**kwargs)
 
         try:
-            p2p = P2PProduct.objects.select_related('activity').exclude(status=u'流标').get(pk=id, hide=False)
+            #p2p = P2PProduct.objects.select_related('activity').get(pk=id, hide=False).exclude(status=u'流标')
+            p2p = P2PProduct.objects.select_related('activity').exclude(status=u'流标').exclude(status=u'录标').exclude(status=u'待审核').get(pk=id, hide=False)
             form = PurchaseForm(initial={'product': p2p})
 
             if p2p.soldout_time:
