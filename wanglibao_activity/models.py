06c3778606f7f8edcc780835aaa5d4b18a771620
lili <249143979@qq.com>
2015-03-17 11:16:05 +0800

    update models

--- wanglibao_activity/models.py
+++ wanglibao_activity/models.py
@@ -17,7 +17,7 @@ ACTIVITY_CATEGORY = (
 )
 PRODUCT_CATEGORY = (
     ('all', u'所有产品'),
-    ('p2p', u'P2P产品'),
+    ('p2p', u'P2P'),
     ('bill', u'票据'),
     ('ids', u'指定ID产品')
 )
@@ -33,7 +33,8 @@ TRIGGER_NODE = (
     ('recharge', u'充值'),
     ('invest', u'投资'),
     ('first_recharge', u'首次充值'),
-    ('first_invest', u'首次投资')
+    ('first_invest', u'首次投资'),
+    ('', u'满标审核')
 )
 GIFT_TYPE = (
     ('reward', u'奖品'),
@@ -45,7 +46,7 @@ GIFT_TYPE = (
 
 class Activity(models.Model):
     name = models.CharField(u'活动名称*', max_length=128)
-    code = models.CharField(u'活动代码（字母/数字组合）*', max_length=16, unique=True)
+    code = models.CharField(u'活动代码*', max_length=16, unique=True, help_text=u'字母或数字的组合')
     category = models.CharField(u'活动类型*', max_length=20, choices=ACTIVITY_CATEGORY, default=u'站内活动')
     platform = models.CharField(u'发布平台*', max_length=20, choices=PLATFORM, default=u'全平台')
     product_cats = models.CharField(u'指定产品范围', max_length=20, default=u'P2P产品', choices=PRODUCT_CATEGORY)
@@ -53,12 +54,13 @@ class Activity(models.Model):
     description = models.TextField(u'描述', null=True, blank=True)
     start_at = models.DateTimeField(default=timezone.now, null=False, verbose_name=u"活动开始时间*")
     end_at = models.DateTimeField(default=timezone.now, null=False, verbose_name=u"活动结束时间*")
-    stop_at = models.DateTimeField(null=True, verbose_name=u"手动停止时间", blank=True)
-    banner = models.ImageField(u'活动图片', null=True, upload_to='activity', blank=True)
     status = models.CharField(u'活动状态', max_length=20, choices=STATUS, default=u'未开始')
+    stop_at = models.DateTimeField(null=True, verbose_name=u"手动停止时间", blank=True)
     created_at = models.DateTimeField(u'添加时间', auto_now_add=True)
-    priority = models.IntegerField(u'优先级*', help_text=u'越大越优先', default=0, blank=False)
+    banner = models.ImageField(u'活动图片', null=True, upload_to='activity', blank=True)
     template = models.TextField(u'活动模板（pyjade编译过的模板）', null=True, blank=True)
+    url = models.URLField(u'活动URL地址', null=True, blank=True)
+    priority = models.IntegerField(u'优先级*', help_text=u'越大越优先', default=0, blank=False)
 
     def __unicode__(self):
         return self.name
@@ -72,11 +74,11 @@ class ActivityRule(models.Model):
     activity = models.ForeignKey(Activity, verbose_name=u'活动名称')
     rule_name = models.CharField(u'规则名称', max_length=128)
     rule_description = models.TextField(u'规则描述', null=True, blank=True)
-    trigger_node = models.CharField(u'触发节点', max_length=20, choices=TRIGGER_NODE)
     gift_type = models.CharField(u'赠送类型', max_length=20, choices=GIFT_TYPE)
+    trigger_node = models.CharField(u'触发节点', max_length=20, choices=TRIGGER_NODE)
     redpack = models.ForeignKey(RedPackEvent, verbose_name=u'红包类型', blank=True)
     reward = models.CharField(u'奖品类型名称', max_length=60, blank=True, help_text=u'类型名称一定要和奖品中的类型保持一致')
-    income = models.FloatField(u'收益或收益率（送收益时填写）', default=10, help_text=u'固定金额时填写大于1的数字，收益率时填写0-1之间的小数')
+    income = models.FloatField(u'收益或收益率（送收益时填写）', default=0, blank=True, help_text=u'固定金额时填写大于1的数字，收益率时填写0-1之间的小数')
     min_amount = models.IntegerField(u'最小金额（投资或充值）', default=0)
     max_amount = models.IntegerField(u'最大金额（投资或充值）', default=0)
     msg_template = models.TextField(u'站内信模板（不填则不发）', blank=True, help_text=u'站内信模板不填写则触发该规则时不发站内信，如果有动态变量，用“%s”代替')
