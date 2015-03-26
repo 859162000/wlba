cd615ad7ba940b76f661dae565066119df319bf7
color_bold <wangxiaoqing@wanglibank.com>
2015-03-26 19:18:45 +0800

    weixin

--- wanglibao_mobile/urls.py
+++ wanglibao_mobile/urls.py
@@ -2,9 +2,14 @@
 # encoding:utf-8
 
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView, RedirectView
 from views import HomeView
 
 urlpatterns = patterns('',
     #url(r'^detail/(?P<id>\w+)', P2PDetailView.as_view(), name='p2p detail'),
     url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^mobile_index/$', TemplateView.as_view(template_name="mobile_index.jade")),
+    url(r'^mobile_assets/$', TemplateView.as_view(template_name="mobile_assets.jade")),
+    url(r'^mobile_more/$', TemplateView.as_view(template_name="mobile_more.jade")),
+    url(r'^mobile_detail/$', TemplateView.as_view(template_name="mobile_detail.jade")),
 )
