a47496904f36e6ec82a165e6bd3c0470aa9eb3a8
ssicke <ssicke@163.com>
2015-03-26 18:26:05 +0800

    modify activity templates

--- wanglibao_activity/admin.py
+++ wanglibao_activity/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django import forms
 from django.forms import formsets
 from django.utils import timezone
-from models import Activity, ActivityRule, ActivityRecord, ActivityTemplates, TemplatesImages
+from models import Activity, ActivityRule, ActivityRecord, ActivityTemplates, ActivityImages
 
 class ActivityAdmin(admin.ModelAdmin):
     list_display = ('name', 'code', 'category', 'platform', 'product_cats', 'product_ids',\
@@ -24,48 +24,31 @@ class ActivityRecordAdmin(admin.ModelAdmin):
         return False
 
 
-class TemplatesImageInline(admin.TabularInline):
-    model = TemplatesImages
-    # fields = ('name', 'img', 'desc_one', 'desc_two', 'priority')
-    model.img_type = 'reward'
-    extra = 3
-    max_num = 6
-    verbose_name = u'赠品礼包'
-    verbose_name_plural = u'赠品礼包'
-
-    def get_queryset(self, request):
-        return super(TemplatesImageInline, self).get_queryset(request).filter(img_type='reward').order_by('-priority')
-
-
-class TemplatesRegisterInline(admin.TabularInline):
-    model = TemplatesImages
-    # fields = ('name', 'img', 'desc_one', 'desc_two', 'priority')
-    model.img_type = 'register'
-    extra = 0
-    max_num = 5
-    verbose_name = u'新手注册流程'
-    verbose_name_plural = u'新手注册流程'
-
-    def get_queryset(self, request):
-        return super(TemplatesRegisterInline, self).get_queryset(request).filter(img_type='register').order_by('-priority')
+class ActivityImagesAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('name', 'img_type', 'img', 'desc_one', 'desc_two', 'priority')}),
+    )
+    # ordering = ('-img_type', '-priority')
 
 
 class ActivityTemplatesAdmin(admin.ModelAdmin):
 
     list_display = ('id', )
-    inlines = [TemplatesImageInline, TemplatesRegisterInline]
 
     fieldsets = (
-        ('logo', {'fields': ['logo', 'logo_other', 'location']}),
-        ('banner', {'fields': ['banner']}),
-        (u'活动描述', {'fields': ['desc', 'desc_time']}),
-        (u'奖品图片和描述', {'fields': ['reward_img', 'reward_desc'], 'classes': ['collapse']}),
-        (u'邀请好友', {'fields': ['introduce_img', 'introduce_desc'], 'classes': ['collapse']}),
-        (u'规则描述', {'fields': ['rule_use', 'rule_activity', 'rule_reward'], 'classes': ['collapse']}),
-
+        (None, {'fields': ('name', )}),
+        ('logo', {'fields': ('logo', 'logo_other', 'location')}),
+        ('banner', {'fields': ('banner',)}),
+        (u'活动时间及描述模块', {'fields': ('is_activity_desc', 'desc', 'desc_time'), 'classes': ['collapse']}),
+        (u'奖品图片和描述模块', {'fields': ('is_reward', 'reward_img', 'reward_desc'), 'classes': ['collapse']}),
+        (u'邀请好友模块', {'fields': ['is_introduce', 'introduce_img', 'introduce_desc'], 'classes': ['collapse']}),
+        (u'新手投资模块', {'fields': ('is_teacher', 'teacher_desc'), 'classes': ['collapse']}),
+        (u'活动使用规则模块', {'fields': ('is_rule_use', 'rule_use'), 'classes': ['collapse']}),
+        (u'活动规则模块', {'fields': ('is_rule_activity', 'rule_activity'), 'classes': ['collapse']}),
+        (u'奖品发放规则模块', {'fields': ('is_rule_reward', 'rule_reward'), 'classes': ['collapse']}),
     )
 
-
+admin.site.register(ActivityImages, ActivityImagesAdmin)
 admin.site.register(ActivityTemplates, ActivityTemplatesAdmin)
 
 admin.site.register(Activity, ActivityAdmin)
