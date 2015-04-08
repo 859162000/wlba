86f653851778368171be782932d6193fa583f755
qifeifei12345 <qifeifei@wanglibank.com>
2015-04-08 14:40:49 +0800

    mei

--- wanglibao_activity/urls.py
+++ wanglibao_activity/urls.py
@@ -1,10 +1,11 @@
 # -*- coding:utf-8 -*-
 
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 from wanglibao_activity.views import TemplatesFormatTemplate
 
 
 urlpatterns = patterns(
     '',
     url(r'one/(?P<id>\w+)', TemplatesFormatTemplate.as_view(template_name='template_one.jade')),
-)
+    url(r'^new_user/$', TemplateView.as_view(template_name="new_user.jade")),)
