8b45c84b13ce4d964edfc34306a9cf962af593b8
lili <249143979@qq.com>
2015-04-10 11:36:00 +0800

    update first buy time of introduced by

--- marketing/tools.py
+++ marketing/tools.py
@@ -27,17 +27,14 @@ def decide_first(user_id, amount, device_type='pc'):
     user = User.objects.filter(id=user_id).first()
     amount = long(amount)
 
+    introduced_by = IntroducedBy.objects.filter(user=user).first()
+
+    if introduced_by and introduced_by.bought_at is None:
+        introduced_by.bought_at = timezone.now()
+        introduced_by.save()
+
     #活动检测
     activity_backends.check_activity(user, 'invest', device_type, amount)
-
-    introduced_by = IntroducedBy.objects.filter(user=user).first()
-    if not introduced_by:
-        return
-    #if not introduced_by or introduced_by.bought_at is not None:
-    #    return
-
-    introduced_by.bought_at = timezone.now()
-    introduced_by.save()
 
     #channel = helper.which_channel(user, intro=introduced_by)
     channel = helper.which_channel(user)
@@ -54,33 +51,6 @@ def decide_first(user_id, amount, device_type='pc'):
         start_time = timezone.datetime(2014, 12, 18)
         if P2PRecord.objects.filter(user=user, create_time__gt=start_time).count() == 1:
             rs.reward_user(u'一个月风行会员')
-    elif channel == helper.Channel.JIUXIAN:
-        #酒仙网
-        start_time = timezone.datetime(2014, 12, 18)
-        if amount >= 500:
-            if P2PRecord.objects.filter(user=user, create_time__gt=start_time).count() > 1:
-                return
-
-            invited_phone = introduced_by.user.wanglibaouserprofile.phone
-            send_messages.apply_async(kwargs={
-                "phones": [invited_phone],
-                "messages": [messages.jiuxian_invited(money=30)]
-            })
-            
-            title, content = messages.msg_jiuxian()
-            inside_message.send_one.apply_async(kwargs={
-                "user_id": introduced_by.user.id,
-                "title": title,
-                "content": content,
-                "mtype": "activity"
-            })
-            rwd = Reward.objects.filter(type=u'30元话费').first()
-            if rwd:
-                try:
-                    RewardRecord.objects.create(user=introduced_by.user, reward=rwd,
-                                                description=content)
-                except Exception, e:
-                    print(e)
     elif channel == helper.Channel.XUNLEI:
         # 非快盘来源(需要确定到每个渠道)
         start_time = timezone.datetime(2014, 11, 12)
