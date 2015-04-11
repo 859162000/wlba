fa6a8b44c062cb931bc146871ec6abbde891899d
lili <249143979@qq.com>
2015-04-11 14:32:01 +0800

    add used amount with red pack

--- wanglibao_redpack/models.py
+++ wanglibao_redpack/models.py
@@ -110,6 +110,7 @@ class RedPackRecord(models.Model):
     apply_platform = models.CharField(max_length=20, null=False, default="", choices=PLATFORM, verbose_name=u"使用平台")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=u'创建时间')
     apply_at = models.DateTimeField(verbose_name=u'使用时间', null=True)
+    apply_amount = models.FloatField(null=True, default=0.0, verbose_name=u'使用金额')
     order_id = models.IntegerField(verbose_name=u'关联订单', null=True)
 
     class Meta:
