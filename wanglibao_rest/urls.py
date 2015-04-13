9631ff1c9845c174eec76df19c94edbae435ebb0
zhenjing <zhenjinglee@gmail.com>
2015-04-13 12:26:55 +0800

    fix bug

--- wanglibao_rest/urls.py
+++ wanglibao_rest/urls.py
@@ -147,7 +147,7 @@ urlpatterns = patterns(
 
 
     url(r'^client_update/$', ClientUpdateAPIView.as_view()),
-    #url(r'^pushtest/$', PushTestView.as_view()),
+    url(r'^pushtest/$', PushTestView.as_view()),
     url(r'^ytx/voice_back', YTXVoiceCallbackAPIView.as_view()),
     url(r'^ytx/send_voice_code/$', SendVoiceCodeAPIView.as_view()),
     url(r'^ytx/send_voice_code/2/$', SendVoiceCodeTwoAPIView.as_view()),
