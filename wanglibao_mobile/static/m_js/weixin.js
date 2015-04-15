c11f91ab641f6bc008947f366b75084d1504dbd1
qifeifei12345 <qifeifei@wanglibank.com>
2015-04-15 14:19:15 +0800

    wei

--- wanglibao_mobile/static/m_js/weixin.js
+++ wanglibao_mobile/static/m_js/weixin.js
@@ -4,18 +4,19 @@
         $('.wei_kehuduan').hide();
     });
 
+    $('input').focus(function() {
+        $(this).attr('placeholder', ' ');
+    })
     log();
-
+    wei_password()
 })();
 function log(){
     $('.judge').on('click', function () {
         if ($(".ipon").val() == "") {
             alert("手机号码不能为空！");
-            $(".ipon").focus();
             return false;
         } else if (!$(".ipon").val().match(/^1[3|4|5|8|9][0-9]\d{4,8}$/)) {
             alert("手机号码格式不正确！");
-            $(".ipon").focus();
             return false;
         } else {
              var userName = $.trim($(".ipon").val());
@@ -39,7 +40,20 @@ function log(){
         }
     });
 }
+function wei_password(){
+    $('.wei_buttonn').on('click', function () {
+        if ($(".wei_word").val() == "") {
+            alert("密码不能为空！");
+            return false;
+        } else if (!$(".wei_word").val().match(/^[0-9_a-zA-Z]{6,20}$/)) {
+            alert("手机号码格式不正确！");
+            return false;
+        }else{
 
+        }
+
+    })
+}
 
 
 
