d719e81c56eee958bc3fa5f93763d2e4dfb9fd52
qifeifei12345 <qifeifei@wanglibank.com>
2015-04-15 11:57:43 +0800

    wei

--- wanglibao_mobile/static/m_js/weixin.js
+++ wanglibao_mobile/static/m_js/weixin.js
@@ -4,12 +4,16 @@
         $('.wei_kehuduan').hide();
     });
 
+    log();
+
+})();
+function log(){
     $('.judge').on('click', function () {
         if ($(".ipon").val() == "") {
             alert("手机号码不能为空！");
             $(".ipon").focus();
             return false;
-        } else if (!$(".ipon").val().match(/^(((13[0-9]{1})|159|153)+\d{8})$/)) {
+        } else if (!$(".ipon").val().match(/^1[3|4|5|8|9][0-9]\d{4,8}$/)) {
             alert("手机号码格式不正确！");
             $(".ipon").focus();
             return false;
@@ -17,18 +21,25 @@
              var userName = $.trim($(".ipon").val());
             $.ajax({
                 type: "get",
-                url: "user_exists/userName",
-                data:{},
-                dataType: "number",
-                success: function (result) {
+                url: "/api/user_exists/" + userName,
+                data:{
 
+                },
+                dataType: "json",
+                success: function (result) {
+                    console.log(result['existing']);
+                    var number_a=result['existing'];
+                   if(number_a===true){
+                       window.location.href = "/mobile/weixin_inputt/";
+                   }else{
+                        window.location.href = "/mobile/weixin_registered/";
+                   }
                 }
             });
         }
     });
+}
 
-
-})();
 
 
 
