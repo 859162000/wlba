2dcfae8d0d38e4fcbbcbed17402a63fa1d7e3ba5
hetaov <v.tao.he@gmail.com>
2015-04-21 14:54:07 +0800

    fix bug

--- wanglibao_p2p/prepayment.py
+++ wanglibao_p2p/prepayment.py
@@ -94,17 +94,18 @@ class PrepaymentHistory(object):
 
             #如果上一期没有结算的话抛出异常
                     #and term_date < date_now
-            if index > 0 \
-                    and amortization.settled == False:
+            if index > 0 and amortization.settled == False:
                 raise PrepaymentException()
 
             if index == 0:
-                if make_loans_time < payment_date < term_date: #and make_loans_time < date_now < term_date:
+                #and make_loans_time < date_now < term_date:
+                if make_loans_time < payment_date < term_date:
                     self.days = (term_date - make_loans_time).days
                     amortization_current = amortization
             else:
                 last_term_date = timezone.localtime(amortizations[index-1].term_date).date()
-                if last_term_date < payment_date < term_date: #and last_term_date < date_now < term_date:
+                #and last_term_date < date_now < term_date:
+                if last_term_date < payment_date < term_date:
                     self.days = (term_date - make_loans_time).days
                     amortization_current = amortization
 
