a145433cd01f1da69871fb3d545228debffb290d
jeff <200006506@qq.com>
2015-05-11 18:59:06 +0800

    Weixin

--- weixin/admin_urls.py
+++ weixin/admin_urls.py
@@ -1,13 +1,20 @@
 # encoding:utf-8
 from __future__ import unicode_literals
 from django.conf.urls import patterns, url
+from django.conf import settings
 import admin_views as views
 
 urlpatterns = patterns(
     '',
+    # view
     url(r'^weixin/account/manage/(?P<id>\w+)/$', views.WeixinView.as_view(), name='weixin_manage'),
     url(r'^weixin/account/manage/(?P<id>\w+)/mass/$', views.WeixinMassView.as_view(), name='weixin_mass'),
     url(r'^weixin/account/manage/(?P<id>\w+)/reply/$', views.WeixinReplyView.as_view(), name='weixin_reply'),
     url(r'^weixin/account/manage/(?P<id>\w+)/menu/$', views.WeixinMenuView.as_view(), name='weixin_menu'),
     url(r'^weixin/account/manage/(?P<id>\w+)/material/$', views.WeixinMaterialView.as_view(), name='weixin_material'),
+    url(r'^weixin/material/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}, name='weixin_material_file'),
+    url(r'^weixin/account/manage/(?P<id>\w+)/material/image/(?P<media_id>[\w-]+)/$', views.WeixinMaterialImageView.as_view(), name='weixin_material_image'),
+
+    # ajax json api
+    url(r'^weixin/account/manage/(?P<id>\w+)/material_list.json$', views.WeixinMaterialListJsonApi.as_view(), name='weixin_material_list_json'),
 )
--- weixin/urls.py
+++ weixin/urls.py
@@ -1,8 +1,13 @@
 # encoding:utf-8
 from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
 import views
 
 urlpatterns = patterns(
     '',
-    url(r'^connect/(?P<id>\w+)/', views.ConnectView.as_view()),
+    url(r'^connect/(?P<id>\w+)/$', views.ConnectView.as_view(), name='weixin_connect'),
+    url(r'^login/$', views.WeixinLogin.as_view(), name='weixin_login'),
+    url(r'^oauth/login/(?P<id>\w+)/', views.WeixinOauthLoginRedirect.as_view(), name='weixin_oauth_login_redirect'),
+    # js api
+    url(r'^(?P<id>\w+)/jsapi_config.json$', views.WeixinJsapiConfig.as_view(), name='weixin_jsapi_config')
 )
