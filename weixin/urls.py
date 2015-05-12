5f0ad0ed33c09efda2be04ed31fe6df27cb72440
qifeifei12345 <qifeifei@wanglibank.com>
2015-05-12 09:55:20 +0800

    account

--- weixin/urls.py
+++ weixin/urls.py
@@ -7,5 +7,6 @@ urlpatterns = patterns(
     '',
     url(r'^connect/(?P<id>\w+)/', views.ConnectView.as_view()),
     url(r'^list/', views.P2PListView.as_view(), name='weixin_p2p_list'),
+    url(r'^account/', TemplateView.as_view(template_name="weixin_account.jade")),
     url(r'^detail/(?P<id>\w+)', views.P2PDetailView.as_view(), name='weixin_p2p_detail'),
 )
