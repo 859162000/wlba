a45eb71becc6fede47a739aa05cbd8a7cc2c6eae
lili <249143979@qq.com>
2015-05-12 19:38:19 +0800

    add login

--- weixin/urls.py
+++ weixin/urls.py
@@ -1,13 +1,14 @@
 # encoding:utf-8
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView, RedirectView
+from django.contrib.auth.decorators import login_required
 import views
 
 urlpatterns = patterns(
     '',
     url(r'^connect/(?P<id>\w+)/$', views.ConnectView.as_view(), name='weixin_connect'),
     url(r'^list/', views.P2PListView.as_view(), name='weixin_p2p_list'),
-    url(r'^account/', views.WeixinAccountHome.as_view()),
+    url(r'^account/', login_required(views.WeixinAccountHome.as_view(), login_url='/weixin/login/'), name='weixin_account'),
     url(r'^detail/(?P<id>\w+)', views.P2PDetailView.as_view(), name='weixin_p2p_detail'),
     url(r'^login/$', views.WeixinLogin.as_view(), name='weixin_login'),
     url(r'^oauth/login/(?P<id>\w+)/', views.WeixinOauthLoginRedirect.as_view(), name='weixin_oauth_login_redirect'),
