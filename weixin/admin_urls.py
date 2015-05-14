9aff947e7ef7113af30cd17ffcd28cef2f9e6116
jeff <200006506@qq.com>
2015-05-14 18:44:00 +0800

    Weixin 公众号自定义菜单功能

--- weixin/admin_urls.py
+++ weixin/admin_urls.py
@@ -20,5 +20,6 @@ urlpatterns = patterns(
     # ajax json api
     url(r'^weixin/account/manage/(?P<id>\w+)/material_list.json$', views.WeixinMaterialListJsonApi.as_view(), name='admin_weixin_material_list_json'),
     url(r'^weixin/account/manage/(?P<id>\w+)/customer_service/create.api$', views.WeixinCustomerServiceCreateApi.as_view(), name='admin_weixin_customer_service_create_api'),
-
+    url(r'^weixin/menu/create.api$', views.WeixinMenuCreateApi.as_view(), name='admin_weixin_menu_create_api'),
+    url(r'^weixin/menu/delete.api$', views.WeixinMenuDeleteApi.as_view(), name='admin_weixin_menu_delete_api'),
 )
