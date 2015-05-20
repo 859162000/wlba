7e1ddd8bcd675d781135dbb38f7ac7957b7457cb
color_bold <wangxiaoqing@wanglibank.com>
2015-05-20 15:47:16 +0800

    check js

--- static/src/mobile/mobile.js
+++ static/src/mobile/mobile.js
@@ -650,18 +650,15 @@ org.calculator=(function(org){
         _addEvenList:function(){
             var $calculatorBuy = $('.calculator-buy'),
                 $countInput = $('.count-input'),
-                productId,
-                amount_profit,
-                amount;
+                productId, amount_profit, amount;
             $calculatorBuy.on('click',function(){
                 productId = $(this).attr('data-productid');
                 amount  = $countInput.val();
                 amount_profit = $("#expected_income").text();
-
-                if(amount){
-                    window.location.href = '/weixin/view/buy/' + productId + '/?amount='+ amount + ';amount_profit=' + amount_profit;
+                if(amount % 100 !== 0 && amount){
+                    return alert("请输入100的整数倍")
                 }else{
-                     window.location.href = '/weixin/view/buy/' + productId + '/';
+                    window.location.href = '/weixin/view/buy/' + productId + '/?amount='+ amount + '&amount_profit=' + amount_profit;
                 }
             })
         }
