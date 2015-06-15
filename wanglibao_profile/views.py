cac859e111277db5ccf79ecdb4729a07721c247f
huomeimei <huomeimei@wanglibank.com>
2015-06-15 10:32:50 +0800

    api/profile

--- wanglibao_profile/views.py
+++ wanglibao_profile/views.py
@@ -12,6 +12,8 @@ from wanglibao_account.utils import verify_id
 from wanglibao_pay.models import Card
 from wanglibao_account.utils import str_add_md5
 from django.db.models import F
+from wanglibao_redpack.backends import stamp
+
 
 
 class ProfileView(APIView):
@@ -34,6 +36,7 @@ class ProfileView(APIView):
             "name":profile.name,
             "id_number":profile.id_number,
             "id_is_valid":profile.id_is_valid,
+            "id_valid_time":stamp(profile.id_valid_time),
             "shumi_request_token":profile.shumi_request_token,
             "shumi_request_token_secret":profile.shumi_request_token_secret,
             "shumi_access_token":profile.shumi_access_token,
