ea8da5db3c5a53bdca5f843f5965ee29a0e4d72a
lili <249143979@qq.com>
2015-06-23 12:30:48 +0800

    remove top in index , detail page of p2p

--- wanglibao/views.py
+++ wanglibao/views.py
@@ -47,10 +47,10 @@ class IndexView(TemplateView):
 
         #排行榜
 
-        top = Top()
-        day_tops = top.day_tops(datetime.now())
-        week_tops = top.week_tops(datetime.now())
-        all_tops = top.all_tops()
+        #top = Top()
+        # day_tops = top.day_tops(datetime.now())
+        #week_tops = top.week_tops(datetime.now())
+        #all_tops = top.all_tops()
 
         return {
             "p2p_products": p2p_products,
@@ -62,10 +62,10 @@ class IndexView(TemplateView):
             'announcements': AnnouncementHomepage,
             'announcements_p2p': AnnouncementP2PNew,
             'partners': partners,
-            'day_tops': day_tops,
-            'week_tops': week_tops,
-            'all_tops': all_tops,
-            'is_valid': top.is_valid()
+            'day_tops': [],
+            'week_tops': [],
+            'all_tops': [],
+            'is_valid': False
         }
 
     def get(self, request, *args, **kwargs):
