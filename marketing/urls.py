eba99f95ef98f8be182c9f4b52f332e3224afed0
lili <249143979@qq.com>
2015-06-23 12:31:16 +0800

    remove #

--- marketing/urls.py
+++ marketing/urls.py
@@ -68,7 +68,7 @@ urlpatterns += patterns(
     '',
     url(r'^introduced_by/$', IntroducedAwardTemplate.as_view(template_name="introduced_by.jade")),
     url(r'^investment_reward/$', InvestmentRewardView.as_view(template_name="investment_reward.jade")),
-    #url(r'^investment/$', Investment.as_view(), name='day'),
+    url(r'^investment/$', Investment.as_view(), name='day'),
     # url(r'^list_day/$', Investment.as_view(), name='list_day'),
 )
 
