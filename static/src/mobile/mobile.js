27d1b794616e3a4a623db43223ceef7dd28ebfa3
wxqGitHub <color_bold@163.com>
2015-07-07 11:31:59 +0800

    fix alert

--- static/src/mobile/mobile.js
+++ static/src/mobile/mobile.js
@@ -177,15 +177,16 @@ org.ui = (function(){
                 alertFram.id="alert-cont";
                 alertFram.style.cssText="position:absolute; top:35%;left:50%; width:14rem; margin:-2.75rem 0 0 -7rem; background:#fafafa; border-radius:.3rem;z-index:1000001;";
                 strHtml = "<div id='alertTxt' class='popub-txt' style='color:#333;font-size: .9rem!important;padding: 1.25rem .75rem;'>"+txt+"</div>";
-                strHtml +=" <div class=\"popub-footer\" style=\"width: 100%;padding: .5rem 0;font-size: .9rem;text-align: center;color: #4391da;border-top: 1px solid #d8d8d8;border-bottom-left-radius: .25rem;border-bottom-right-radius: .25rem;\" onclick=\"doOk()\">确认</div>";
+                strHtml += " <div class='popub-footer' style='width: 100%;padding: .5rem 0;font-size: .9rem;text-align: center;color: #4391da;border-top: 1px solid #d8d8d8;border-bottom-left-radius: .25rem;border-bottom-right-radius: .25rem;'>确认</div>";
                 alertFram.innerHTML = strHtml;
                 document.body.appendChild(alertFram);
                 document.body.appendChild(shield);
-                this.doOk = function(){
+
+                $('.popub-footer').on('touchend click',function(){
                     alertFram.style.display = "none";
                     shield.style.display = "none";
                     callback && callback();
-                };
+                })
             }
             document.body.onselectstart = function(){return false;};
         },
