923c5de172419dd3b4c4b69db1145cd2995a8b8b
wxqGitHub <color_bold@163.com>
2015-07-24 15:37:57 +0800

    fix session

--- weixin/views.py
+++ weixin/views.py
@@ -156,7 +156,12 @@ class WeixinRegister(TemplateView):
 
     def get_context_data(self, **kwargs):
         token = self.request.GET.get(settings.PROMO_TOKEN_QUERY_STRING, '')
-        if not token:
+        token_session = self.request.session.get(settings.PROMO_TOKEN_QUERY_STRING, '')
+        if token:
+            token = token
+        elif token_session:
+            token = token_session
+        else:
             token = 'weixin'
 
         if token:
