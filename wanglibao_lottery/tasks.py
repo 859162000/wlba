f112b726b57183e32523e44d1c8252d5ac317ef2
Guoya <guoya@wanglibank.com>
2015-08-10 15:03:00 +0800

    彩票：使用aware time

--- wanglibao_lottery/tasks.py
+++ wanglibao_lottery/tasks.py
@@ -1,6 +1,6 @@
 # encoding=utf-8
 from datetime import datetime, timedelta
-
+from django.utils.timezone import get_default_timezone
 from wanglibao.celery import app
 from wanglibao_lottery.models import Lottery
 
@@ -11,10 +11,12 @@ def lottery_set_status():
     每天早上八点重置未中奖用户的状态
     :return:
     """
-    utc_yesterday = (datetime.today() - timedelta(days=1))
-    lotteries = Lottery.objects.filter(open_time__gte=utc_yesterday).fitler(status='已出票').all()
+    d = datetime.today()
+    yesterday = get_default_timezone().localize(datetime(d.year, d.month, d.day))
+    lotteries = Lottery.objects.filter(open_time__gte=yesterday).fitler(status='已出票').all()
     for lottery in lotteries:
         lottery.status = '未中奖'
         lottery.save()
+
 
 
