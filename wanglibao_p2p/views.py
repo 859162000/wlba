da6e70bf52b0ca27724916da2fb2fd3b2ef7dfb2
hbiami <hbiami@gmail.com>
2015-08-14 13:48:05 +0800

    modify purchse to purchase - guoya

--- wanglibao_p2p/views.py
+++ wanglibao_p2p/views.py
@@ -21,7 +21,9 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from marketing.models import SiteData
 from wanglibao.permissions import IsAdminUserOrReadOnly
+from wanglibao.signals import signal_product_first_bought
 from wanglibao_account.cooperation import CoopRegister
+from wanglibao_lottery.tasks import send_lottery
 from wanglibao_p2p.amortization_plan import get_amortization_plan
 from wanglibao_p2p.prepayment import PrepaymentHistory
 from wanglibao_p2p.forms import PurchaseForm, BillForm
@@ -181,7 +183,7 @@ class PurchaseP2P(APIView):
                 product_info, margin_info, equity_info = trader.purchase(amount, redpack)
 
                 #处理第三方渠道回调
-                CoopRegister(request).process_for_purchse(request.user)
+                CoopRegister(request).process_for_purchase(request.user)
 
                 return Response({
                     'data': product_info.amount,
