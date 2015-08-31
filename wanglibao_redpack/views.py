8b668823d94335cc2541aea563f88c27475dc353
lili <249143979@qq.com>
2015-08-31 18:33:27 +0800

    update ret_code

--- wanglibao_redpack/views.py
+++ wanglibao_redpack/views.py
@@ -63,6 +63,6 @@ class RedPacketSelectAPIView(APIView):
         user = request.user
         product_id = request.DATA.get("product_id", "").strip()
         if not product_id:
-            return Response({"ret_code": 30190, "message": u"产品ID错误"})
+            return Response({"ret_code": 3001, "message": u"产品ID错误"})
         result = backends.get_interest_coupon(user, product_id)
         return Response(request)
