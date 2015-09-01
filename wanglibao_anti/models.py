6905472c93491a8f0be19e1a3359a400d26caac5
Yihen <liuyihen@gmail.com>
2015-09-01 11:00:29 +0800

    针对anti增加后台管理界面

--- wanglibao_anti/models.py
+++ wanglibao_anti/models.py
@@ -4,14 +4,25 @@
 
 from django.db import models
 
+STATUS = (
+    ("0", "监控中"),
+    ("1", "正常用户"),
+    ("2", "作弊用户"),
+    ("3", "误击用户"),
+)
+
 class AntiDelayCallback(models.Model):
     uid = models.IntegerField()
     createtime = models.IntegerField()
     updatetime = models.IntegerField()
-    channel = models.CharField(max_length=32)
-    status = models.IntegerField()
+    channel = models.CharField(max_length=32, verbose_name='渠道标号')
+    status = models.IntegerField(verbose_name='状态', choices=STATUS)
     device = models.CharField(max_length=1024)
-    ip = models.CharField(max_length=32)
+    ip = models.CharField(max_length=32, verbose_name='IP地址')
+
+    class Meta:
+        verbose_name = u"渠道反作弊表"
+        verbose_name_plural = u'渠道反作弊表'
 
     def __unicode__(self):
         return u"反作弊"
