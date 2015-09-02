245ede5d9ef306c84c95b9602f212f18012253f3
qijinjin <qijinjin@wanglibank.com>
2015-09-02 18:52:11 +0800

    pc

--- static/js/anniversary.js
+++ static/js/anniversary.js
@@ -147,15 +147,47 @@
       }
 
     }
-    var height = $('#banner').height();
-    var width = 0;
-    if(height > 497){
-        width = 76
-    }else{
-      width = 76 * 0.6
-    }
+    var width = $('body').width();
     $('.wanglibao img').css({
-        'width': width + '%'
+        'width': width*0.035 +'%'
+    })
+
+     //固定回到顶部
+     function backtop(){
+       var k=document.body.clientWidth,
+         a = k - 100
+       return a;
+     }
+    var left2;
+    left2=backtop();
+    //浏览器大小改变触发的事件
+    window.onresize = function(){
+      left2 = backtop();
+    };
+    //赋值
+    $('.xl-backtop').css({'left':left2});
+
+    //显示微信二维码
+   $('#xl-weixin').on('mouseover',function(){
+     $('.erweima').show();
+   });
+
+    $('#xl-weixin').on('mouseout',function(){
+     $('.erweima').hide();
+   })
+
+    //返回顶部
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 0) {
+            $(".xl-backtop").fadeIn();
+        }else{
+            $('.xl-backtop').stop().fadeOut();
+        }
+    });
+
+    $('.backtop').on('click',function(){
+      $('body,html').animate({scrollTop: 0}, 600);
+      return false
     })
   });
 }).call(this);
