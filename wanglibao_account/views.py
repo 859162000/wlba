823174cf008531ff595a5ff4ef85cdeb01796064
hbiami <hbiami@gmail.com>
2015-09-06 10:13:18 +0800

    modify for xicai by zhoud on 2015-09-06

--- wanglibao_account/views.py
+++ wanglibao_account/views.py
@@ -118,7 +118,7 @@ class RegisterView(RegistrationView):
             'next': self.request.GET.get('next', '/accounts/login/')
         })
 
-        if sign and promo_token == 'csai' or 'xicai':
+        if sign and (promo_token == 'csai' or promo_token == 'xicai'):
 
             try:
                 from wanglibao_account.cooperation import get_xicai_user_info
