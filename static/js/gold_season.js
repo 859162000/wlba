99056c1a754609acc959631f814fcdf069b73133
cheche <100401176@qq.com>
2015-09-08 17:50:54 +0800

    添加csrfrf到js

--- static/js/gold_season.js
+++ static/js/gold_season.js
@@ -6,7 +6,42 @@
   });
 
   require(['jquery'], function($) {
-    //初始化数据
+    var  csrfSafeMethod, getCookie,sameOrigin,
+    getCookie = function(name) {
+        var cookie, cookieValue, cookies, i;
+        cookieValue = null;
+        if (document.cookie && document.cookie !== "") {
+            cookies = document.cookie.split(";");
+            i = 0;
+            while (i < cookies.length) {
+              cookie = $.trim(cookies[i]);
+              if (cookie.substring(0, name.length + 1) === (name + "=")) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+              }
+              i++;
+            }
+        }
+        return cookieValue;
+    };
+    csrfSafeMethod = function(method) {
+        return /^(GET|HEAD|OPTIONS|TRACE)$/.test(method);
+    };
+    sameOrigin = function(url) {
+        var host, origin, protocol, sr_origin;
+        host = document.location.host;
+        protocol = document.location.protocol;
+        sr_origin = "//" + host;
+        origin = protocol + sr_origin;
+        return (url === origin || url.slice(0, origin.length + 1) === origin + "/") || (url === sr_origin || url.slice(0, sr_origin.length + 1) === sr_origin + "/") || !(/^(\/\/|http:|https:).*/.test(url));
+    };
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+              xhr.setRequestHeader("X-CSRFToken", getCookie("csrftoken"));
+            }
+        }
+    });
     $.ajax({
         url: '/api/gettopofearings/',
         type: "POST"
