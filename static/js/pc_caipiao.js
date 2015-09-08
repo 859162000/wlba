005f5cacf09880b26d75c22bd3b6c707d2672ab0
wangruyue <rsj217@gmail.com>
2015-09-08 16:43:27 +0800

    caipiao

--- static/js/pc_caipiao.js
+++ static/js/pc_caipiao.js
@@ -10,10 +10,11 @@
 
   require(['jquery'], function($) {
     //立即参与
-    var H=window.innerHeight/2-$('.erweima').height()
+    var H=($(window).height()-$('.erweima').height())/2
+    alert(H)
     $('.btn').on('click',function(){
       var re_top=$('.erweima').offset().top;
-      $('body,html').animate({scrollTop:re_top+H}, 600);
+      $('body,html').animate({scrollTop:re_top-H}, 600);
       return false
     })
   });
