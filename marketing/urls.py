8fd56a26d21cf6912c7aecc40964543a6a967de5
wangruyue <rsj217@gmail.com>
2015-09-08 10:04:33 +0800

    caipiao

--- marketing/urls.py
+++ marketing/urls.py
@@ -33,6 +33,7 @@ urlpatterns = patterns(
     url(r'^app_anniversary/$', TemplateView.as_view(template_name="app_anniversary.jade")),
     url(r'^eight_gift_two/$', TemplateView.as_view(template_name="eight_gift_two.jade")),
     url(r'^xingmei/$', TemplateView.as_view(template_name="xingmei.jade")),
+    url(r'^pc_caipiao/$', TemplateView.as_view(template_name="pc_caipiao.jade")),
 
 
 )
