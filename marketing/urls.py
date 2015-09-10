af024ae033ee72b3799eb00b18e4e88f44656035
zzljack <15601017102@163.com>
2015-09-10 16:44:57 +0800

    刮奖页面

--- marketing/urls.py
+++ marketing/urls.py
@@ -58,6 +58,7 @@ urlpatterns += patterns(
     url(r'^app_gold_day/$', TemplateView.as_view(template_name="app_gold_day.jade")),
     url(r'^app_pc_download/$', TemplateView.as_view(template_name="app_pc_download.jade")),
     url(r'^app_lottery/$', TemplateView.as_view(template_name="app_lottery.jade")),
+    url(r'^app_scratch/$', TemplateView.as_view(template_name="app_scratch.jade")),
 
 )
 
