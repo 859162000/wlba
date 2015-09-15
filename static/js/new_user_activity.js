24790c81b2ac41c07a512e6c7133a1bf4508305e
cyj7 <cyj19900104@126.com>
2015-09-15 18:35:16 +0800

    new user

--- static/js/new_user_activity.js
+++ static/js/new_user_activity.js
@@ -13,6 +13,19 @@
         }
     });
     require(['jquery', 'jqueryRotate', "tools"], function ($, jqueryRotate, tool) {
+        var activityId = 65;
+        function ajaxFun(url,data,fn){
+            $.ajax({
+                type: "post",
+                url: url,
+                dataType: "json",
+                data: data,
+                async: false,
+                success: function(data){
+                    fn(data);
+                }
+            });
+        }
         //banner
         $("#banner").height($(window).height());
         //关闭弹层
@@ -31,9 +44,17 @@
             obj.show();
             $("#alert-page").show();
         }
+        var funs = {
+            "register": function(data){
+                var ret_code = data.ret_code;
+                if(ret_code == 00001){
+                   showAlert($(".no-new-user"));
+                }
+            }
+        }
         //注册领红包
         $(".receive-red").on('click',function(){
-            showAlert($(".no-new-user"));
+            ajaxFun(url,{"activity_id":activityId,"trigger_node":"register","action": "is_activity"},funs.register);
         });
         //充值
         $(".recharge").on("click",function(){
