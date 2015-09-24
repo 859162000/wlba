4d42c1ecc951a90286d8a6f3eb7d9ff2e9a59d6e
Yihen <liuyihen@gmail.com>
2015-09-24 17:32:53 +0800

    fix bug

--- wanglibao_reward/urls.py
+++ wanglibao_reward/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import url, patterns
 import views
 urlpatterns = patterns(
     '',
-    url(r'^share/(?P<phone_num>\w+)/(?P<openid>[\w,"-","_"]+)/(?P<order_id>[\w,"_","-"]+)/(?P<activity>[\w,"_"]+)/$', views.WeixinShareDetailView.as_view(), name='weixin_share_order_detail'),
+    url(r'^share/(?P<phone_num>\w+)/(?P<openid>[\w,"\-","_"]+)/(?P<order_id>[\w,"_","\-"]+)/(?P<activity>[\w,"_","\-"]+)/$', views.WeixinShareDetailView.as_view(), name='weixin_share_order_detail'),
     url(r'^share/start/$', views.WeixinShareStartView.as_view(), name='weixin_share_order_gift'),
     url(r'^share/end/$', views.WeixinShareEndView.as_view(), name='weixin_share_end'),
 )
