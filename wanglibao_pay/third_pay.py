7ef868f777b8337668e3713840b1471daaac8eb8
hbiami <hbiami@gmail.com>
2015-09-24 18:18:54 +0800

    modify a little by hb

--- wanglibao_pay/third_pay.py
+++ wanglibao_pay/third_pay.py
@@ -186,7 +186,7 @@ def withdraw(request):
     if len(str(amount)) > 20:
         return {"ret_code":20068, 'message':'金额格式错误，大于100元且为100倍数'}
     # Modify by hb on 2015-09-23 for 50000 => 100000
-    if not 0 <= amount <= 50000:
+    if not 0 <= amount <= 100000:
         return {"ret_code":20064, 'message':u'提款金额在0～100000之间'}
 
     margin = user.margin.margin
