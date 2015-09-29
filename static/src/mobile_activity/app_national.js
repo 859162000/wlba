8bf11e32a9e1fdb442420a200bc09bf80f83dcfd
zzljack <15601017102@163.com>
2015-09-29 18:54:55 +0800

    fix bug

--- static/src/mobile_activity/app_national.js
+++ static/src/mobile_activity/app_national.js
@@ -8,12 +8,25 @@ function toggles(ele){
          ele.removeAttr("data");
     }
 }
+function Down(ele){
+    var curHeight = ele.height();
+    var autoHeight = ele.css('height', 'auto').height();
+    if (!ele.hasClass('down')){
+      ele.height(curHeight).animate({height: autoHeight},600,function(){
+        ele.addClass('down')
+      });
+    }else{
+      ele.height(curHeight).animate({height: 0},600,function(){
+        ele.removeClass('down')
+      });
+    }
+  }
 $("#rule").on("click", function () {
-    toggles($("#rulebox"));
+    Down($("#rulebox"));
 });
 
 $("#btns").on("click", function () {
-    toggles($("#rulebox3"));
+    Down($("#rulebox3"));
 });
 
 
