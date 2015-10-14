1fdcc2df1d4678efa2442fc1c12a76842d07d8e4
Yihen <liuyihen@gmail.com>
2015-10-14 16:04:14 +0800

    .

--- weixin/views.py
+++ weixin/views.py
@@ -708,7 +708,7 @@ class AuthorizeUser(APIView):
                     left_counts = WanglibaoActivityGift.objects.filter(gift_id=url_id, valid=True).count()
                     logger.debug("奖品有 %s 个还没有被用户领走了" %(left_counts, ))
                     if True or (left_counts == 0 and counts > 0):
-                        if user_gift:
+                        if True or user_gift:
                             logger.debug(u"用户已经令完奖品，而且所有的奖品已经发放完毕")
                             return redirect("/weixin_activity/share/%s/%s/%s/share/" %(phone, openid, url_id))
                         else:
