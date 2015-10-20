196517f4ba3dbac76088e339ca561b2b6908a4ea
zzljack <15601017102@163.com>
2015-10-20 10:22:59 +0800

    new page

--- static/js/newgive.js
+++ static/js/newgive.js
@@ -17,6 +17,35 @@
         isNOShow : '0',
         buttonFont: '立即注册'
     });
+    var Rbtn= $("#btn_right"), Lbtn= $("#btn_left"),
+        fabox= $("#fal_box").children(),
+        len=fabox.length-1, index= 0, Millisecond=3000;
+    fabox.first().css('opacity',1);
+    Rbtn.on("click",function(){
+      index++;
+      if(index>len) index = 0;
+      Focusmap();
+    });
+    Lbtn.on('click',function(){
+      index--;
+      if(index<0) index = len;
+      Focusmap();
+    });
+    fabox.on("mouseover",function(){
+      clearInterval(time)
+    }).on("mouseout",function(){
+      clearInterval(time)
+      time = setInterval(timersFn, Millisecond);
+    })
+    timersFn = function(){
+      index++;
+      if(index>len) index = 0;
+      fabox.eq(index).fadeIn().siblings().fadeOut();
+    };
+    Focusmap = function(){
+      fabox.eq(index).fadeIn().siblings().fadeOut();
+    };
+    var time = setInterval(timersFn, Millisecond);
 
   });
 }).call(this);
