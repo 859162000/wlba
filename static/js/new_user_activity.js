982ab965c33f0b5ac89d9d11bd8d743068c6be4c
cyj7 <cyj19900104@126.com>
2015-10-23 11:43:16 +0800

    还原新手注册活动id

--- static/js/new_user_activity.js
+++ static/js/new_user_activity.js
@@ -13,8 +13,8 @@
         }
     });
     require(['jquery', 'jqueryRotate', "tools"], function ($, jqueryRotate, tool) {
-        var activityId = 37; //activity id l:37 s:65&69 w:72
-        var payId = 37;
+        var activityId = 72; //activity id l:37 s:65&69 w:72
+        var payId = 72;
         function ajaxFun(url,data,fn){
             $.ajax({
                 type: "get",
