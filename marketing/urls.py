9edd5df0e00b04b30df479efe538dc958fc8fab1
hbiami <hbiami@gmail.com>
2015-10-28 19:33:05 +0800

    wechat_reward by wangxq

--- marketing/urls.py
+++ marketing/urls.py
@@ -117,6 +117,9 @@ urlpatterns += patterns(
     url(r'^maimai_index/$', TemplateView.as_view(template_name="app_maimaiIndex.jade")),
     url(r'^maimai_rules/$', TemplateView.as_view(template_name="app_maimaiRule.jade")),
     url(r'^maimai_success/$', TemplateView.as_view(template_name="app_maimaiSuccess.jade")),
+    url(r'^wechat_reward/$', TemplateView.as_view(template_name="app_wechatReward.jade")),
+    url(r'^wechat_result/$', TemplateView.as_view(template_name="app_wechatReward_result.jade")),
+    url(r'^wechat_rule/$', TemplateView.as_view(template_name="app_wechatReward_rule.jade")),
 
 )
 
