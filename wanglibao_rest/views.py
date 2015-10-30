3d0fe7b934d4f67736f4314d478df39d81c57ee7
Yihen <liuyihen@gmail.com>
2015-10-30 12:11:28 +0800

    fix bug

--- wanglibao_rest/views.py
+++ wanglibao_rest/views.py
@@ -1245,7 +1245,7 @@ class DistributeRedpackView(APIView):
 
                     try:
                         logger.debug("用户：%s 使用的加息券id:%s" %(phone_number, redpack_id))
-                        redpack_event = RedPackEvent.objects.filter(id=redpack_id).first()
+                        redpack_event = RedPackEvent.objects.filter(id=747).first()
                     except Exception, reason:
                         logger.debug("从RedPackEvent中获得配置红包报错, reason:%s" % (reason, ))
 
