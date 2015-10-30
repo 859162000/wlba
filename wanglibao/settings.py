c9f9c00966b09b5699806c914575be69e25aa4e2
d0nezer0 <vincentchou@126.com>
2015-10-30 18:00:57 +0800

    优惠券到期提醒撤销，保存

--- wanglibao/settings.py
+++ wanglibao/settings.py
@@ -612,10 +612,10 @@ CELERYBEAT_SCHEDULE = {
     },
 
     # by Zhoudong 定期检查没有投资的新用户, 提醒投资
-    'invested_status_task_check': {
-        'task': 'marketing.tools.check_invested_status',
-        'schedule': crontab(minute=0, hour=10),
-    },
+    # 'invested_status_task_check': {
+    #     'task': 'marketing.tools.check_invested_status',
+    #     'schedule': crontab(minute=0, hour=10),
+    # },
     # by Zhoudong 定期检查用户优惠券没使用,发送提醒
     'redpack_status_task_check': {
         'task': 'marketing.tools.check_redpack_status',
