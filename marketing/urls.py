67774d5239b19f47b7ba534c5b6feaed71b9acf8
hbiami <hbiami@gmail.com>
2015-11-03 16:27:07 +0800

    by hb for xunlei-ten's url & meimei's weixin

--- marketing/urls.py
+++ marketing/urls.py
@@ -33,8 +33,8 @@ urlpatterns = patterns(
     url(r'^advance/$', TemplateView.as_view(template_name="advance.jade")),
     url(r'^gold/$', TemplateView.as_view(template_name="gold.jade"), name='marketing_gold'),
     url(r'^qixi/$', TemplateView.as_view(template_name="qixi.jade")),
-    #url(r'^xunlei_setp/$', TemplateView.as_view(template_name="xunlei_setp.jade"), name='marketing_xunlei_setp'),
-    url(r'^xunlei_setp/$', ThunderTenAcvitityTemplate.as_view(template_name="xunlei_ten.jade")),
+    url(r'^xunlei_setp/$', TemplateView.as_view(template_name="xunlei_setp.jade"), name='marketing_xunlei_setp'),
+    #url(r'^xunlei_setp/$', ThunderTenAcvitityTemplate.as_view(template_name="xunlei_ten.jade")),
     url(r'^mid_autumn/$', TemplateView.as_view(template_name="mid-autumn.jade")),
     # url(r'^xunlei_ten/$', TemplateView.as_view(template_name="xunlei_ten.jade")),
 
