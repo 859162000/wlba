d9bf027177c59924879d0efa1e820cc7059f746c
249143979 <249143979@qq.com>
2015-11-05 13:59:36 +0800

    提现相关处理，新增字段，app接口，后端80%代码更新

--- wanglibao_pay/models.py
+++ wanglibao_pay/models.py
@@ -18,13 +18,15 @@ class Bank(models.Model):
     logo = models.ImageField(upload_to='bank_logo', null=True, blank=True, help_text=u'银行图标')
     sort_order = models.IntegerField(default=0, verbose_name=u'排序权值 从大到小')
     kuai_code = models.CharField(max_length=16, verbose_name=u'快钱侧银行代码', null=True, blank=True)
-    #银行限额信息格式如下,"|"分隔第一次和第二次
-    #单笔=5000,单日=5000|单笔=50000,单日=10000000
+    # 银行限额信息格式如下,"|"分隔第一次和第二次
+    # 单笔=5000,单日=5000|单笔=50000,单日=10000000
     kuai_limit = models.CharField(max_length=500, blank=True, verbose_name=u'快钱侧银行限额信息')
     huifu_bind_limit = models.CharField(max_length=500, blank=True, verbose_name=u"汇付快捷限额信息", default="")
     huifu_bind_code = models.CharField(max_length=20, verbose_name=u'汇付侧银行代码', blank=True, default="")
     yee_bind_limit = models.CharField(max_length=500, blank=True, verbose_name=u"易宝快捷限额信息", default="")
     yee_bind_code = models.CharField(max_length=20, verbose_name=u'易宝侧银行代码', blank=True, default="")
+    # 提现限额:min_amount=50,max_amount=50000
+    withdraw_limit = models.CharField(max_length=500, blank=True, verbose_name=u"银行提现限额", default="")
 
     #last_update = models.DateTimeField(u'更新时间', auto_now=True, null=True)
 
@@ -95,7 +97,8 @@ class PayInfo(models.Model):
     uuid = models.CharField(u'唯一标示', max_length=32, unique=True, db_index=True, default=get_a_uuid)
     amount = models.DecimalField(u'实扣金额', max_digits=20, decimal_places=2)
     fee = models.DecimalField(u'手续费', max_digits=20, decimal_places=2, default=0)
-    management_fee = models.DecimalField(u'资金管理费', max_digits=20, decimal_places=2, default=0)
+    management_fee = models.DecimalField(u'资金管理费用', max_digits=20, decimal_places=2, default=0)
+    management_amount = models.DecimalField(u'资金管理金额', max_digits=20, decimal_places=2, default=0)
     total_amount = models.DecimalField(u'总金额', max_digits=20, decimal_places=2, default=0)
     create_time = models.DateTimeField(u'创建时间', auto_now_add=True)
     update_time = models.DateTimeField(u'更新时间', auto_now=True)
@@ -165,11 +168,14 @@ class WithdrawCardRecord(models.Model):
     type = models.CharField(u'类型', help_text=u'充值：D 取款：W', max_length=5)
     uuid = models.CharField(u'唯一标示', max_length=32, unique=True, db_index=True, default=get_a_uuid)
     amount = models.DecimalField(u'金额', max_digits=20, decimal_places=2)
+    fee = models.DecimalField(u'手续费', max_digits=20, decimal_places=2, default=0)
+    management_fee = models.DecimalField(u'资金管理费用', max_digits=20, decimal_places=2, default=0)
+    management_amount = models.DecimalField(u'资金管理金额', max_digits=20, decimal_places=2, default=0)
     withdrawcard = models.ForeignKey(WithdrawCard, on_delete=models.PROTECT)
     payinfo = models.ForeignKey(PayInfo, on_delete=models.PROTECT)
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     order = models.ForeignKey(Order, blank=True, null=True)
-    create_time = models.DateTimeField(u'创建时间', auto_now_add=True)
+    create_time = models.DateTimeField(u'创建时间', auto_now_add=True, db_index=True)
     update_time = models.DateTimeField(u'更新时间', auto_now=True)
     confirm_time = models.DateTimeField(u'审核时间', blank=True, null=True)
     status = models.CharField(u'状态', max_length=15)
