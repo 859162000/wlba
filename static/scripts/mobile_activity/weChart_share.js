9cd02f0500d1951a322dac5b8125d4863b81cfef
wxqGitHub <color_bold@163.com>
2015-11-06 14:13:06 +0800

    s

--- static/scripts/mobile_activity/weChart_share.js
+++ static/scripts/mobile_activity/weChart_share.js
@@ -306,11 +306,7 @@ org.weChatStart = (function(org){
 org.weChatDetail = (function(org){
     var lib = {
         init:function(){
-             window.onload = function(){
-                if($('#amount').attr('data-hasgift') == 'true'){
-                   alert('您已经领取过礼物了！')
-                }
-             }
+
         },
     }
     return {
