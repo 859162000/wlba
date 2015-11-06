35a69e82f2fdd36a8331fbde95e72a9a322685d3
wxqGitHub <color_bold@163.com>
2015-11-06 14:18:21 +0800

    add

--- static/src/mobile_activity/weChart_share.js
+++ static/src/mobile_activity/weChart_share.js
@@ -47,6 +47,40 @@ var weChatShare = (function(org){
         })
 })(org);
 
+org.ui = (function(){
+    var lib = {
+        _alert: function(txt, callback){
+            if(document.getElementById("alert-cont")){
+                document.getElementById("alertTxt").innerHTML = txt;
+                document.getElementById("popubMask").style.display = "block";
+                document.getElementById("alert-cont").style.display = "block";
+            }else{
+                var shield = document.createElement("DIV");
+                shield.id = "popubMask";
+                shield.style.cssText="position:absolute;bottom:0;top:0;width:100%; background:rgba(0,0,0,0.5); z-index:1000000;";
+                var alertFram = document.createElement("DIV");
+                alertFram.id="alert-cont";
+                alertFram.style.cssText="position:absolute; top:35%;left:50%; width:14rem; margin:-2.75rem 0 0 -7rem; background:#fafafa; border-radius:.3rem;z-index:1000001;";
+                strHtml = "<div id='alertTxt' class='popub-txt' style='color:#333;font-size: .9rem!important;padding: 1.25rem .75rem;'>"+txt+"</div>";
+                strHtml += " <div class='popub-footer' style='width: 100%;padding: .5rem 0;font-size: .9rem;text-align: center;color: #4391da;border-top: 1px solid #d8d8d8;border-bottom-left-radius: .25rem;border-bottom-right-radius: .25rem;'>确认</div>";
+                alertFram.innerHTML = strHtml;
+                document.body.appendChild(alertFram);
+                document.body.appendChild(shield);
+
+                $('.popub-footer').on('click',function(){
+                    alertFram.style.display = "none";
+                    shield.style.display = "none";
+                    callback && callback();
+                })
+            }
+            document.body.onselectstart = function(){return false;};
+        }
+    }
+
+    return {
+        alert : lib._alert,
+    }
+})();
 org.weChatStart = (function(org){
     var lib = {
         init:function(){
@@ -98,7 +132,11 @@ org.weChatDetail = (function(org){
 org.weChatEnd = (function(org){
     var lib = {
         init:function(){
-            console.log('end')
+            window.onload = function(){
+              if($('#amount').attr('data-hasgift') == 'true'){
+                 org.ui.alert('您已经领取过礼物了！');
+              }
+            }
         },
     }
     return {
