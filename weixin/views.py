8e0983f6140d19ef556569d08e5ec19870102a64
huomeimei <huomeimei@wanglibank.com>
2015-11-17 18:14:14 +0800

    微信３秒钟跳转对中文解码进行decode

--- weixin/views.py
+++ weixin/views.py
@@ -446,7 +446,7 @@ class JumpPageTemplate(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(JumpPageTemplate, self).get_context_data(**kwargs)
         message = self.request.GET.get('message', 'ERROR')
-        message = urllib.unquote(message)
+        message = urllib.unquote(message).decode('utf-8')
         logger.debug(message)
         context['message'] = message
         return {
