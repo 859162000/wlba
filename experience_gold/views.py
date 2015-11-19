152ef44545db0d114d8d05856ffe8908ba268022
249143979 <249143979@qq.com>
2015-11-19 10:21:16 +0800

    fix error

--- experience_gold/views.py
+++ experience_gold/views.py
@@ -3,6 +3,7 @@
 from django.views.generic import TemplateView
 from django.utils import timezone
 from django.db.models import Sum
+from decimal import Decimal
 
 from experience_gold.models import ExperienceEventRecord, ExperienceProduct, ExperienceAmortization
 
@@ -22,7 +23,7 @@ class ExperienceGoldView(TemplateView):
         experience_product = ExperienceProduct.objects.filter(isvalid=True).first()
 
         if user.is_authenticated():
-            experience_record = ExperienceEventRecord.objects.filter(user=user, event__invalid=False)\
+            experience_record = ExperienceEventRecord.objects.filter(user=user, apply=False, event__invalid=False)\
                 .filter(event__available_at__lt=now, event__unavailable_at__gt=now).aggregate(Sum('event__amount'))
             if experience_record.get('event__amount__sum'):
                 experience_amount = experience_record.get('event__amount__sum')
@@ -30,11 +31,11 @@ class ExperienceGoldView(TemplateView):
             experience_amortization = ExperienceAmortization.objects.filter(user=user).select_related('product')
             if experience_amortization:
                 paid_interest = reduce(lambda x, y: x + y,
-                                       [e.interest for e in experience_amortization if e.settled is True])
+                                       [e.interest for e in experience_amortization if e.settled is True], 0)
                 unpaid_interest = reduce(lambda x, y: x + y,
-                                         [e.interest for e in experience_amortization if e.settled is False])
+                                         [e.interest for e in experience_amortization if e.settled is False], 0)
 
-        total_experience_amount = experience_amount + paid_interest + unpaid_interest
+        total_experience_amount = float(experience_amount) + float(paid_interest) + float(unpaid_interest)
         return {
             'experience_amount': experience_amount,
             'product': experience_product,
