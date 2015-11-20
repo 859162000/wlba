838d5fc748436ef681a164556116c1496e5487e3
546133753 <546133753@qq.com>
2015-11-20 19:12:00 +0800

    activity_admin modify wanglibao/settings.py, wanglibao_announcement/admin.py, wanglibao_announcement/models.py, wanglibao_announcement/migrations/0003_auto__add_appmemorabilia.py

--- wanglibao_announcement/admin.py
+++ wanglibao_announcement/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from wanglibao_announcement.models import Announcement
+from wanglibao_announcement.models import Announcement, AppMemorabilia
 
 
 class AnnouncementAdmin(admin.ModelAdmin):
@@ -7,4 +7,11 @@ class AnnouncementAdmin(admin.ModelAdmin):
     list_editable = ('type', 'priority', 'status')
     list_filter = ('device', 'type', 'status')
 
+
+class AppMemorabiliaAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'detail_link', 'done_date', 'priority')
+    # list_editable = ('type', 'priority', 'status')
+    # list_filter = ('device', 'type', 'status')
+
 admin.site.register(Announcement, AnnouncementAdmin)
+admin.site.register(AppMemorabilia, AppMemorabiliaAdmin)
