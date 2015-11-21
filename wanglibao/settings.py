e3dd67628533cec068892369c65a6101e6acfa80
249143979 <249143979@qq.com>
2015-11-21 17:51:58 +0800

    体验金还款计划任务

--- wanglibao/settings.py
+++ wanglibao/settings.py
@@ -664,6 +664,11 @@ CELERYBEAT_SCHEDULE = {
         'task': 'marketing.tools.check_invested_status',
         'schedule': crontab(minute=0, hour=10),
     },
+    # 每天下午4点半开始处理体验金的还款
+    'experience_repayment_plan': {
+        'task': 'experience_gold.backends.experience_repayment_plan',
+        'schedule': crontab(minute=40, hour=16),
+    },
     # # by Zhoudong 定期检查用户优惠券没使用,发送提醒
     # 'redpack_status_task_check': {
     #     'task': 'marketing.tools.check_redpack_status',
