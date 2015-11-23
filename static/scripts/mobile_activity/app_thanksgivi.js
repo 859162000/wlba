5a32fa09be70959baf0cc7c97c2f8176edc60cca
qifeifei12345 <qifeifei@wanglibank.com>
2015-11-23 15:34:57 +0800

    thank

--- static/scripts/mobile_activity/app_thanksgivi.js
+++ static/scripts/mobile_activity/app_thanksgivi.js
@@ -328,24 +328,24 @@ var click = false;
 
 window.onload = function () {
     lottery.init('lottery');
-    $("#lottery .appjiang-button2").click(function () {
-        if (change['left'] == 0) {
-            return;
-        }
+    $("#lottery .appjiang-button2").click(function (fa) {
+        //if (change['left'] == 0) {
+        //    return;
+        //}
         redpack({
             'action': "POINT_AT",
             'activity': "thanks_given",
             'level': "5000+"
-        }, function () {
-            if (change['left'] == 0) {
+        }, function (data) {
+            if (data['left'] == 0) {
                 $('.appjiang-button').removeClass("appjiang-button2");
                 $('.appjiang-button').addClass("appjiang-button1");
                 $('.appprize-mingdan .appjiang-ri p').html('您没有抽奖机会');
-                if (change['reward'] == null) {
+                if (data['reward'] == null) {
                     return;
                 }
             } else {
-                $('.appprize-mingdan .appjiang-ri p span').text(change['left']);
+                $('.appprize-mingdan .appjiang-ri p span').text(data['left']);
             }
             if (click) {
                 return false;
@@ -361,25 +361,23 @@ window.onload = function () {
     });
     //抽奖2
     $('.app-thanksbu2').on('click', function () {
-        if (change['left'] == 0) {
-            return;
-        }
+        alert(11);
         redpack({
             'action': "POINT_AT",
             'activity': "thanks_given",
             'level': "5000-"
-        }, function () {
-            if (change['left'] == 0) {
+        }, function (data) {
+            if (data['left'] == 0) {
                 $('.app-thanksbu').removeClass("app-thanksbu2");
                 $('.app-thanksbu').addClass("app-thanksbu1");
                 $('.yellow1-main .appjiang-ri p').html('您没有抽奖机会');
                 $('.apphongxi').hide();
             } else {
-                $('.yellow1-main .appjiang-ri p span').text(change['left']);
+                $('.yellow1-main .appjiang-ri p span').text(data['left']);
             }
             $('.apphongxi').show();
-            $('#thankgi-thanks2 ').text(change['reward']);
-
+            $('#thankgi-thanks2 ').text(data['reward']);
+            alert(data['reward']);
 
             return false;
         })
@@ -459,7 +457,7 @@ function redpack(data, callback) {
         url: '/api/activity/reward/',
         type: "POST",
         data: data,
-        async: false,
+        //async: false,
         success: function (data) {
             change = data;
             callback && callback(data);
