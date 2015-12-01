2aee5ad84815d33b56bb67dda587b77cd4346812
hbiami <hbiami@gmail.com>
2015-12-01 12:20:09 +0800

    by yihen fix marketing/middlewares.py

--- marketing/middlewares.py
+++ marketing/middlewares.py
@@ -60,17 +60,22 @@ class StatsKeyWordMiddleWare(object):
         }
 
     def process_request(self, request):
-        try:
-            referer = request.META.get("HTTP_REFERER", "")
-            if referer:
+        referer = request.META.get("HTTP_REFERER", "")
+        if referer:
+            try:
                 res = urlparse.urlparse(referer)
                 website = filter(lambda item: item in res.netloc, self.statics.keys())
                 if website:
                     website = website[0]
                     qs = urlparse.parse_qs(res.query)
                     if self.statics[website]['keyword'] in qs:
-                        request.session["promo_source_keyword"] = "|".join(qs[self.statics[website]['keyword']])
+                        res = "|".join(qs[self.statics[website]['keyword']])
+                        try:
+                            gb2312 = res.decode('gb2312')
+                        except:
+                            gb2312 = res
+                        request.session["promo_source_keyword"] = gb2312
                         request.session["promo_source_site_name"] = self.statics[website]['name']
                         request.session['promo_source_website'] = self.statics[website]['website']
-        except Exception, reason:
-            logger.debug(u"SEM关键词统计，中间件报异常, reason:%s" % reason)
+            except Exception, reason:
+                logger.debug(u"SEM关键词统计，中间件报异常, reason:%s" % reason)
