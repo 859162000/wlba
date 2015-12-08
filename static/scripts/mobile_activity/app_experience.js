cd6f2887fadec3f1aa3e5a742ed02844e18d164f
qijinjin <qijinjin@wanglibank.com>
2015-12-08 15:42:22 +0800

    体验金

--- static/scripts/mobile_activity/app_experience.js
+++ static/scripts/mobile_activity/app_experience.js
@@ -273,7 +273,7 @@ wlb.ready({
             if (data.ph == '') {
                 login = false;
                 $('#nologin').on('click',function(){
-                    mixins.registerApp({refresh:0, url:'https://staging.wanglibao.com/activity/experience/redirect/'});
+                    mixins.registerApp({refresh:1, url:'https://staging.wanglibao.com/activity/experience/redirect/'});
                 })
             } else {
                 login = true;
