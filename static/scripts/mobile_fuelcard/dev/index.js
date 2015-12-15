28d9240572807fc7b6f16350ab16f2ad744a3071
wxqGitHub <color_bold@163.com>
2015-12-15 14:05:50 +0800

    save

--- static/scripts/mobile_fuelcard/dev/index.js
+++ static/scripts/mobile_fuelcard/dev/index.js
@@ -4,23 +4,11 @@ webpackJsonp([0],[
 
 	/* WEBPACK VAR INJECTION */(function($) {'use strict';
 
-	var _ajax = __webpack_require__(2);
+	var _ajax = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"./mixins/ajax\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
 
 	$('.test').html(_ajax.pi);
 	$('.test').html('12312aa3');
 	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(1)))
-
-/***/ },
-/* 1 */,
-/* 2 */
-/***/ function(module, exports) {
-
-	"use strict";
-
-	Object.defineProperty(exports, "__esModule", {
-	  value: true
-	});
-	var pi = exports.pi = 3.12222211;
 
 /***/ }
 ]);
