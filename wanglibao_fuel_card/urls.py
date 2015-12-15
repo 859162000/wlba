d93b7aad64920401922305dd62a28d4ed7238e8a
wxqGitHub <color_bold@163.com>
2015-12-15 13:41:01 +0800

    add link

--- wanglibao_fuel_card/urls.py
+++ wanglibao_fuel_card/urls.py
@@ -18,6 +18,7 @@ urlpatterns = patterns(
     url(r'^authentication/$', TemplateView.as_view(template_name="fuel_authentication.jade")),
     url(r'^about/$', TemplateView.as_view(template_name="fuel_about.jade")),
     url(r'^bank/$', TemplateView.as_view(template_name="fuel_bank.jade")),
+    url(r'^set/bank/$', TemplateView.as_view(template_name="fuel_set_bank.jade")),
 
     url(r'^records/product/$', TemplateView.as_view(template_name="fuel_records_audit.jade")),
     url(r'^records/exchange/$', TemplateView.as_view(template_name="fuel_records_exchange.jade")),
