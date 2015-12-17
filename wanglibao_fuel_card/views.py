d1d782a902c2e57037083a8fc107a86386a304d9
546133753 <546133753@qq.com>
2015-12-17 10:16:32 +0800

    jiayouka modify wanglibao_fuel_card/views.py

--- wanglibao_fuel_card/views.py
+++ wanglibao_fuel_card/views.py
@@ -60,45 +60,124 @@ class FuelCardBuyApi(APIView):
 
 
 class FuelCardBugRecordView(TemplateView):
-    """加油卡购买记录视图"""
+    """
+    加油卡购买记录视图
+    :param
+    :return
+    :request_method GET
+    :user.is_authenticated True
+    """
 
     template_name = 'fuel_records_audit.jade'
 
+    def get_sorts_for_created_time(self, queryset):
+        """根据 created_time 对请求集排序（降序）"""
+
+        data_list = sorted(queryset, key=lambda asd: asd.created_time, reverse=True)
+
+        return data_list
+
+    def get_user_amortizations(self, settled):
+        """获取用户还款计划"""
+
+        user_amotization = UserAmortization.objects.filter(user=self.request.user, settled=settled
+                                                           ).select_related(depth=2)
+        user_amotization = user_amotization.order_by('product_amortization__product_id', 'term')
+
+        ua_list = []
+        ua_tmp = user_amotization.first()
+        ua_list.append(ua_tmp)
+        for ua in user_amotization:
+            if ua.product_amortization.product != ua_tmp.product_amortization.product:
+                ua_list.append(ua_tmp)
+                ua_tmp = ua
+
+        return self.get_sorts_for_created_time(ua_list)
+
     def get_context_data(self, **kwargs):
-        status = self.request.GET.get('status', '').strip()
-        if not status:
+        _status = self.request.GET.get('status', '').strip()
+        if not _status:
             return HttpResponseForbidden(u'status参数不存在')
 
-        if status == 'auditing':
-            user_amotization = UserAmortization.objects.filter(user=self.request.user, settled=False
-                                                               ).select_related(depth=2)
-            user_amotization = user_amotization.order_by('product_amortization__product', 'term')
+        status_list = ['auditing', 'done']
+        if _status in status_list:
+            settled = False if _status == 'auditing' else True
+            data = self.get_user_amortizations(settled)
 
-            ua_list = []
-            ua_tmp = user_amotization.first()
-            ua_list.append(ua_tmp)
-            for ua in user_amotization:
-                if ua.product_amortization.product != ua_tmp.product_amortization.product:
-                    ua_list.append(ua_tmp)
-                    ua_tmp = ua
-            user_amotization = user_amotization.order_by('cre',
-                                                         'term')
-        elif status == 'tack_effect':
-            pass
-        elif status == 'done':
-            pass
+            return {
+                'data': data,
+                'status': _status,
+            }
         else:
             return HttpResponseForbidden(u'无效参数status')
 
 
-# class FuelCardBuyView(TemplateView):
-#     """加油卡购买页面视图"""
-#
-#     template_name = 'fuel_buy.jade'
-#
-#     def get_context_data(self, **kwargs):
-#         phone = get_phone_for_coop(self.request.user.id)
-#         fuel_card_amount = ''
+class FuelCardExchangeRecordView(TemplateView):
+    """
+    加油卡兑换记录视图
+    :param
+    :return
+    :request_method GET
+    """
+
+    template_name = 'fuel_records_exchange.jade'
+
+    def get_sorts_for_created_time(self, queryset):
+        """根据 created_time 对请求集排序（降序）"""
+
+        data_list = sorted(queryset, key=lambda asd: asd.created_time, reverse=True)
+
+        return data_list
+
+    def get_user_amortizations(self, settled):
+        """获取用户还款计划"""
+
+        user_amotization = UserAmortization.objects.filter(user=self.request.user, settled=settled
+                                                           ).select_related(depth=2)
+        user_amotization = user_amotization.order_by('product_amortization__product_id', 'term')
+
+        ua_list = []
+        ua_tmp = user_amotization.first()
+        ua_list.append(ua_tmp)
+        for ua in user_amotization:
+            if ua.product_amortization.product != ua_tmp.product_amortization.product:
+                ua_list.append(ua_tmp)
+                ua_tmp = ua
+
+        return self.get_sorts_for_created_time(ua_list)
+
+    def get_context_data(self, **kwargs):
+        _status = self.request.GET.get('status', '').strip()
+        if not _status:
+            return HttpResponseForbidden(u'status参数不存在')
+
+        status_list = ['wait_receive', 'receive']
+        if _status in status_list:
+            settled = False if _status == 'wait_receive' else True
+            data = self.get_user_amortizations(settled)
+
+
+class FuelCardBuyView(TemplateView):
+    """
+    加油卡购买页面视图
+    :param
+    :return
+    :request_method GET
+    """
+
+    template_name = 'fuel_buy.jade'
+
+    def get_context_data(self, p_id, **kwargs):
+        phone = get_phone_for_coop(self.request.user.id)
+        try:
+            p2p_product = P2PProduct.objects.get(pk=p_id)
+        except P2PProduct.DoesNotExist:
+            return HttpResponseForbidden(u'无效产品ID')
+
+        return {
+            'p2p_product': p2p_product,
+            'phone': phone,
+        }
 
 
 class FuelCardListViewForApp(TemplateView):
