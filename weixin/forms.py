768692e2e22f2e1cf6d44398e6b445da86f120ab
cyj7 <cyj19900104@126.com>
2015-12-21 10:53:22 +0800

    添加曹玉娇的配置

--- weixin/forms.py
+++ weixin/forms.py
@@ -9,6 +9,7 @@ class OpenidAuthenticationForm(forms.Form):
 
     error_messages = {
         'param_is_null': '1',
+        'user_not_exist': '2',
     }
 
     def __init__(self, openid, *args, **kwargs):
@@ -24,6 +25,7 @@ class OpenidAuthenticationForm(forms.Form):
         self._errors = None
 
     def clean(self):
+        print 'self.openid==%s'%self.openid
         self.user_cache = authenticate(openid=self.openid)
         if self.user_cache is None:
             raise forms.ValidationError(
