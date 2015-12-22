24ef91d72789a15179ef05edeb2e1285c84aed52
cyj7 <cyj19900104@126.com>
2015-12-22 15:50:01 +0800

    修改微站相关文件

--- wanglibao_rest/utils.py
+++ wanglibao_rest/utils.py
@@ -61,7 +61,7 @@ def split_ua(request):
 def get_client_ip(request):
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        return x_forwarded_for
+        return x_forwarded_for[0:13]
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
