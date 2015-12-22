696b2812250410f3f593e9a4be0d24927777021a
546133753 <546133753@qq.com>
2015-12-22 17:29:07 +0800

    jiayouka modify wanglibao_fuel_card/views.py

--- wanglibao_fuel_card/views.py
+++ wanglibao_fuel_card/views.py
@@ -17,6 +17,9 @@ from .trade import P2PTrader
 from .utils import get_sorts_for_created_time, get_p2p_reward_using_range
 
 
+def get_user_revenue_count():
+
+
 class FuelCardBuyApi(APIView):
     """
     加油卡购买接口
@@ -201,23 +204,25 @@ class FuelCardListViewForApp(TemplateView):
 
     template_name = 'fuel_index.jade'
 
+    def get_classes_period(self):
+
     def get_context_data(self, **kwargs):
-        # 优先级越低排越前面
         p2p_products = P2PProduct.objects.filter(hide=False, publish_time__lte=timezone.now(), category=u'加油卡',
-                                                 status__in=[u'已完成', u'满标待打款', u'满标已打款', u'满标待审核',
-                                                             u'满标已审核', u'还款中', u'正在招标'
-                                                             ]).order_by('priority', '-publish_time')
+                                                 status=u'正在招标').order_by('period', '-priority')
 
         data = []
+        product_1 = product_2 = product_3 = None
         if p2p_products:
             # 根据优先级排序，并获取每种优先级的第一条记录
             data.append(p2p_products[0])
-            unique_priority = p2p_products[0].priority
+            unique_period = p2p_products[0].period
             for p2p_product in p2p_products[1:]:
-                if p2p_product.priority != unique_priority:
+                if p2p_product.period != unique_period:
                     data.append(p2p_product)
-                    unique_priority = p2p_product.priority
+                    unique_period = p2p_product.period
 
+            for i in data:
+                if
         return {
             'html_data': data,
         }
