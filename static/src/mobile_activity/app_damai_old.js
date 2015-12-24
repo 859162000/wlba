3b033d7f3f12328a65bbee3c43b2eaf516f60299
qifeifei12345 <qifeifei@wanglibank.com>
2015-12-24 16:09:48 +0800

    damaiold

--- static/src/mobile_activity/app_damai_old.js
+++ static/src/mobile_activity/app_damai_old.js
@@ -15,11 +15,11 @@ function redpack() {
             } else if (damai['ret_code'] == 1006) {
                 $('.tishiyu').text('您已领过网利宝摇滚之夜门票');
             } else if (damai['ret_code'] == 1005) {
-                $('.tishiyu').text('很抱歉，奖品发没了');
+                $('.tishiyu').text('很抱歉，门票已经发完了');
             } else if (damai['ret_code'] == 1004) {
                 $('.tishiyu').text('很抱歉，没有在预定的时间内购标');
             } else if (damai['ret_code'] == 1003) {
-                $('.tishiyu').text('很抱歉，票已经发完了');
+                $('.tishiyu').text('很抱歉，门票已经发完了');
             } else if (damai['ret_code'] == 1001) {
                 $('.tishiyu').text('很抱歉，您投资没有满1000元');
             }
