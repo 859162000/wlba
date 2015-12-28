ecf98363ceef86cfc4fa2023198402fdb908c99f
qijinjin <qijinjin@wanglibank.com>
2015-12-28 13:36:42 +0800

    同卡进出

--- static/js/bank_list.js
+++ static/js/bank_list.js
@@ -93,5 +93,45 @@
                 });
             }
         });
+
+        /*绑定银行卡 */
+        $('#bank-List').delegate('.binding-card','click',function() {
+          var card, par, str;
+          $('#bindingOldCard').modal();
+          $('#bindingOldCard').find('.ok-btn').attr({
+            'data-card': $(this).attr('data-card')
+          });
+          $('#bindingOldCard').find('.close-modal').hide();
+          $('.modal').css({
+            'width': '560px'
+          });
+          par = $(this).parent();
+          card = par.find('.bank-card--info-value').text();
+          str = par.find('.bank-card--bank-name').find('label').text() + '尾号' + card.substr(card.length - 4);
+          return $('.bankInfo').html(str);
+        });
+
+        /*确认绑定 */
+        $('#bindingOldCard').delegate('.ok-bt','click',function() {
+          return $.ajax({
+            url: '/api/pay/the_one_card/',
+            data: {
+              card_id: $(this).attr('data-card')
+            },
+            type: 'put'
+          }).done(function() {
+            return location.reload();
+          }).fail(function(xhr) {
+            return tool.modalAlert({
+              title: '温馨提示',
+              msg: xhr.message
+            });
+          });
+        });
+
+        /*取消绑定 */
+        $('#bindingOldCard').delegate('.no-btn','click',function() {
+          return $.modal.close();
+        });
     })
 })()
