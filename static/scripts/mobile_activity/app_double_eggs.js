62cd88d283cd0bc65e9b1b8f12f802b47e47c3e3
hbiami <hbiami@gmail.com>
2015-12-28 12:59:11 +0800

    20151228a

--- static/scripts/mobile_activity/app_double_eggs.js
+++ static/scripts/mobile_activity/app_double_eggs.js
@@ -231,7 +231,7 @@ var Zepto=function(){function L(t){return null==t?String(t):j[S.call(t)]||"objec
     wlb.ready({
         app: function(mixins) {
 			//mixins.shareData({title: '双旦来了，翻倍狂欢', content: '红包、加息券、体验金全部翻倍送、实物大奖同台登场，年底倾囊N重回馈。'});
-			
+
             mixins.sendUserInfo(function(data) {
 				$('.link_licai').click(function(){
 					mixins.jumpToManageMoney();
@@ -243,8 +243,9 @@ var Zepto=function(){function L(t){return null==t?String(t):j[S.call(t)]||"objec
                 if (data.ph == '') {
                     login = false;
 					$('.recharge_button').click(function() {
-                        mixins.loginApp();
+                        mixins.loginApp({refresh:1, url:'https://www.wanglibao.com/activity/app_double_eggs'});
                     })
+					
                 } else {
                     login = true;
                     $('.recharge_button').click(function() {
