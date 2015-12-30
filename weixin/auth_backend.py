14f2c0f25c65530b015aa80cb73936657854203e
huomeimei <huomeimei@wanglibank.com>
2015-12-30 18:43:17 +0800

    公众号自动登录ｆｉｘ

--- weixin/auth_backend.py
+++ weixin/auth_backend.py
@@ -7,9 +7,12 @@ User = get_user_model()
 class OpenidAuthBackend(object):
     def authenticate(self, **kwargs):
         openid = kwargs.get('openid')
-        w_user = WeixinUser.objects.get(openid=openid)
-        user = w_user.user
-        active_user = user if user else None
+        active_user = None
+        if openid:
+            w_user = WeixinUser.objects.filer(openid=openid).first()
+            if w_user:
+                user = w_user.user
+                active_user = user if user else None
         return active_user
 
     def get_user(self, user_id):
