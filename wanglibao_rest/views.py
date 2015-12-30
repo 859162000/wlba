45c1072fb957cfd46e389aade895a9cb18f68957
249143979 <249143979@qq.com>
2015-12-30 16:19:23 +0800

    修改电视的统计数据

--- wanglibao_rest/views.py
+++ wanglibao_rest/views.py
@@ -1000,27 +1000,27 @@ class Statistics(APIView):
     def post(self, request, *args, **kwargs):
         today = datetime.now()
         # tomorrow = today + timedelta(1)
-        yesterday = today - timedelta(days=1)
+        # yesterday = today - timedelta(days=1)
         today_start = local_to_utc(datetime(today.year, today.month, today.day), 'min')
-        yesterday_start = local_to_utc(datetime(yesterday.year, yesterday.month, yesterday.day), 'min')
+        # yesterday_start = local_to_utc(datetime(yesterday.year, yesterday.month, yesterday.day), 'min')
         # today_end = datetime.combine(tomorrow, time())
 
         today_user = User.objects.filter(date_joined__gte=today_start).aggregate(Count('id'))
         today_amount = P2PRecord.objects.filter(create_time__gte=today_start, catalog='申购').aggregate(Sum('amount'))
-        yesterday_amount = P2PRecord.objects.filter(create_time__gte=yesterday_start, create_time__lt=today_start)\
-            .filter(catalog='申购').aggregate(Sum('amount'))
+        # yesterday_amount = P2PRecord.objects.filter(create_time__gte=yesterday_start, create_time__lt=today_start)\
+        #     .filter(catalog='申购').aggregate(Sum('amount'))
         today_num = P2PRecord.objects.filter(create_time__gte=today_start, catalog='申购').values('id').count()
 
-        today_repayment = ProductAmortization.objects.filter(settled=True)\
-            .filter(settlement_time__gte=yesterday_start, settlement_time__lt=today_start)\
-            .aggregate(Sum('principal'), Sum('interest'))
+        # today_repayment = ProductAmortization.objects.filter(settled=True)\
+        #     .filter(settlement_time__gte=yesterday_start, settlement_time__lt=today_start)\
+        #     .aggregate(Sum('principal'), Sum('interest'))
 
-        amount_sum_yesterday = yesterday_amount['amount__sum'] if yesterday_amount['amount__sum'] else Decimal('0')
-        principal_sum = today_repayment['principal__sum'] if today_repayment['principal__sum'] else Decimal('0')
-        interest_sum = today_repayment['interest__sum'] if today_repayment['interest__sum'] else Decimal('0')
+        # amount_sum_yesterday = yesterday_amount['amount__sum'] if yesterday_amount['amount__sum'] else Decimal('0')
+        # principal_sum = today_repayment['principal__sum'] if today_repayment['principal__sum'] else Decimal('0')
+        # interest_sum = today_repayment['interest__sum'] if today_repayment['interest__sum'] else Decimal('0')
 
         # 昨日资金净流入
-        today_inflow = amount_sum_yesterday - principal_sum - interest_sum
+        # today_inflow = amount_sum_yesterday - principal_sum - interest_sum
 
         all_user = User.objects.all().aggregate(Count('id'))
         all_amount = P2PRecord.objects.filter(catalog='申购').aggregate(Sum('amount'))
@@ -1035,7 +1035,7 @@ class Statistics(APIView):
             'all_user': all_user['id__count'],
             'all_amount': all_amount['amount__sum'],
 
-            'today_inflow': today_inflow
+            # 'today_inflow': today_inflow
         }
 
         return Response(data, status=status.HTTP_200_OK)
