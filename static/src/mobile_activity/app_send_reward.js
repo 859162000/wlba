8c1b63462f04531b103114df8785eea2c98a7d2a
车思雪 <100401176@qq.com>
2016-01-05 15:35:32 +0800

    年终奖奖品提交

---
+++ static/src/mobile_activity/app_send_reward.js
@@ -0,0 +1,48 @@
+(function(org) {
+
+    var jsApiList = ['scanQRCode', 'onMenuShareAppMessage','onMenuShareTimeline','onMenuShareQQ'];
+	org.ajax({
+		type : 'GET',
+		url : '/weixin/api/jsapi_config/',
+		dataType : 'json',
+		success : function(data) {
+			//请求成功，通过config注入配置信息,
+			wx.config({
+				debug: false,
+				appId: data.appId,
+				timestamp: data.timestamp,
+				nonceStr: data.nonceStr,
+				signature: data.signature,
+				jsApiList: jsApiList
+			});
+		}
+	});
+	wx.ready(function(){
+		var host = 'https://www.wanglibao.com/',
+			shareName = '网利宝用户专享福利',
+			shareImg = host + '/static/imgs/mobile_activity/app_noviceDecember_h5/300x300.jpg',
+			shareLink = host + '/activity/app_noviceDecember_h5/',
+			shareMainTit = '网利宝用户专享福利',
+			shareBody = '网利宝送你新手福利大红包，快来领哦！';
+		//分享给微信好友
+		org.onMenuShareAppMessage({
+			title: shareMainTit,
+			desc: shareBody,
+			link: shareLink,
+			imgUrl: shareImg
+		});
+		//分享给微信朋友圈
+		org.onMenuShareTimeline({
+			title: '网利宝用户专享福利',
+			link : shareLink,
+			imgUrl: shareImg
+		})
+		//分享给QQ
+		org.onMenuShareQQ({
+			title: shareMainTit,
+			desc: shareBody,
+			link : shareLink,
+			imgUrl: shareImg
+		})
+	})
+})(org);
