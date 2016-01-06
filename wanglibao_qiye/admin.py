51eae61d4618058d12711a60dbb9fc7605e48190
546133753 <546133753@qq.com>
2016-01-06 18:12:36 +0800

    qylc modify wanglibao_qiye/admin.py, wanglibao_qiye/models.py, wanglibao_qiye/urls.py, wanglibao_qiye/migrations/0001_initial.py

--- wanglibao_qiye/admin.py
+++ wanglibao_qiye/admin.py
@@ -6,14 +6,20 @@ from wanglibao_qiye.models import EnterpriseUserProfile
 
 class EnterpriseProfileAdmin(admin.ModelAdmin):
     pass
-    # actions = None
-    # search_fields = ('user_id', 'user__wanglibaouserprofile__phone')
-    #
-    # def has_delete_permission(self, request, obj=None):
-    #     return False
-    #
-    # def has_add_permission(self, request):
-    #     return False
+    actions = None
+    list_display = ('user', 'company_name', 'status', 'description', 'modify_time', 'created_time')
+    search_fields = ('user_id', 'user__wanglibaouserprofile__phone')
+    raw_id_fields = ('user', 'bank')
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        has_change_fields = ('status', 'description')
+        return has_change_fields
 
 
 admin.site.register(EnterpriseUserProfile, EnterpriseProfileAdmin)
