5a558320d404cbf94728406baaa4b28076685d39
huomeimei <huomeimei@wanglibank.com>
2016-01-06 11:58:37 +0800

    微站调整获取登录路径方法

--- weixin/util.py
+++ weixin/util.py
@@ -40,6 +40,7 @@ def get_fwh_login_url(next=None):
             FWH_LOGIN_URL = BASE_WEIXIN_URL.format(appid=account.app_id, redirect_uri=fwh_login_url, state=original_id)
             global FWH_UNBIND_URL
             FWH_UNBIND_URL = BASE_WEIXIN_URL.format(appid=account.app_id, redirect_uri=fwh_unbind_url, state=original_id)
+            return FWH_LOGIN_URL
 
 
 if not FWH_LOGIN_URL:
