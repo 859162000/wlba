ad6e5adc77e935353f1fd0fa0c91112e5ddae536
qijinjin <qijinjin@wanglibank.com>
2016-01-11 11:18:39 +0800

    优化代码

--- static/src/pc/footer.js
+++ static/src/pc/footer.js
@@ -6,6 +6,38 @@ require(['jquery'], function( $ ) {
              }
              return result[1];
     }
+    checkUserStatus = function(){
+        $.ajax({
+          url: '/qiye/profile/exists/',
+          type: "GET",
+          data: {
+          }
+        }).done(function(data) {
+            if(data.ret_code == 10000){
+                $.ajax({
+                  url: '/qiye/profile/get/',
+                  type: "GET",
+                  data: {
+                  }
+                }).done(function(data) {
+                    if(data.data.status == '审核通过'){
+                         $('#checkUserType').attr('href','/accounts/home/')
+                    }else{
+                        $('#checkUserType').attr('href','/qiye/profile/edit/')
+                        $('#minNavs').hide()
+                    }
+                })
+            }
+        }).fail(function(data){
+            var result = JSON.parse(data.responseText);
+            if(result.ret_code == 20001){
+                $('#checkUserType').attr('href','/accounts/home')
+            }else{
+               $('#checkUserType').attr('href','/qiye/info/')
+               $('#minNavs').hide()
+            }
+        })
+    }
     $('#investment').on('click',function(){
         if($('.checkLoginStatus').length > 0){
             $.ajax({
