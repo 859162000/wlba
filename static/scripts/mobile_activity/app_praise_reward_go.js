522d5aa0de287a086bd91abe6a0bb2986811db51
车思雪 <100401176@qq.com>
2016-01-12 15:45:18 +0800

    1月年终奖go页面跳转修改

--- static/scripts/mobile_activity/app_praise_reward_go.js
+++ static/scripts/mobile_activity/app_praise_reward_go.js
@@ -246,22 +246,23 @@ var Zepto=function(){function L(t){return null==t?String(t):j[S.call(t)]||"objec
 	var is_myself;
 	var phone_num;
 	/*申请领取*/
-	$('#go_receive').click(function(){
+	$('#go_receive').click(function() {
 
 		phone_num = $('#get_phone').val();
 
 		$.ajax({
-			url: '/weixin_activity/weixin/bonus/?act=apply&phone='+phone_num+'&wxid='+wxid,
+			url: '/weixin_activity/weixin/bonus/?act=apply&phone=' + phone_num + '&wxid=' + wxid,
 			type: "GET",
-		}).done(function (xhr) {
-			if(xhr.err_code==0){
-				window.location.href = '/weixin_activity/weixin/bonus/?wxid='+wxid
-			}else{
-				$('.friend_top span').text(xhr.err_messege);
-				$('.friend_top').show();
+			success: function (xhr) {
+				if (xhr.err_code == 0) {
+					window.location.href = '/weixin_activity/weixin/bonus/?wxid=' + wxid
+				} else {
+					$('.friend_top span').text(xhr.err_messege);
+					$('.friend_top').show();
+				}
 			}
 		});
-	});
+	})
 	/*申请领取*/
 
 	$('.friend_top .close').click(function(){
