14f219c3993ba9ad7d86b19785e2f07157a5f345
车思雪 <100401176@qq.com>
2016-01-12 15:48:19 +0800

    1月年终奖go页面跳转修改

--- static/scripts/mobile_activity/app_praise_reward_go.js
+++ static/scripts/mobile_activity/app_praise_reward_go.js
@@ -246,12 +246,12 @@ var Zepto=function(){function L(t){return null==t?String(t):j[S.call(t)]||"objec
 	var is_myself;
 	var phone_num;
 	/*申请领取*/
-	$('#go_receive').click(function() {
+	$('#go_receive').click(function(){
 
 		phone_num = $('#get_phone').val();
 
 		$.ajax({
-			url: '/weixin_activity/weixin/bonus/?act=apply&phone=' + phone_num + '&wxid=' + wxid,
+			url: '/weixin_activity/weixin/bonus/?act=apply&phone='+phone_num+'&wxid='+wxid,
 			type: "GET",
 			success: function (xhr) {
 				if (xhr.err_code == 0) {
