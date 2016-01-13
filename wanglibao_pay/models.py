8bf969365495b32a9b3625e7087b392f62f4b3d7
Guoya <guoya@wanglibank.com>
2016-01-13 13:54:50 +0800

    company_invest_model for weibing

--- wanglibao_pay/models.py
+++ wanglibao_pay/models.py
@@ -27,6 +27,7 @@ class Bank(models.Model):
     yee_bind_code = models.CharField(max_length=20, verbose_name=u'易宝侧银行代码', blank=True, default="")
     # 提现限额:min_amount=50,max_amount=50000
     withdraw_limit = models.CharField(max_length=500, blank=True, verbose_name=u"银行提现限额", default="")
+    have_company_channel = models.BooleanField(u"是否对公", default=False)
 
     #last_update = models.DateTimeField(u'更新时间', auto_now=True, null=True)
 
