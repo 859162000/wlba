81699e201571e962bb3a46c14876d046077cc0a9
车思雪 <100401176@qq.com>
2016-01-14 11:11:53 +0800

    解决年终奖点赞go页面跳转问题

--- static/scripts/mobile_activity/app_praise_reward_go.js
+++ static/scripts/mobile_activity/app_praise_reward_go.js
@@ -246,16 +246,16 @@ var Zepto=function(){function L(t){return null==t?String(t):j[S.call(t)]||"objec
 	var is_myself;
 	var phone_num;
 	/*申请领取*/
-	$('#go_receive').click(function() {
+	$('#go_receive').click(function(){
 
 		phone_num = $('#get_phone').val();
 
 		$.ajax({
-			url: '/weixin_activity/weixin/bonus/?act=apply&phone=' + phone_num + '&wxid=' + wxid,
+			url: '/weixin_activity/weixin/bonus/?act=apply&phone='+phone_num+'&wxid='+wxid,
 			type: "GET",
 			success: function (xhr) {
 				if (xhr.err_code == 0) {
-					window.location.href = '/weixin_activity/weixin/bonus/?wxid=' + wxid
+					window.location.href ='/weixin_activity/weixin/bonus/?wxid='+wxid
 				} else {
 					$('.friend_top span').text(xhr.err_messege);
 					$('.friend_top').show();
--- static/src/mobile_activity/app_praise_reward_go.js
+++ static/src/mobile_activity/app_praise_reward_go.js
@@ -27,16 +27,16 @@
 	var is_myself;
 	var phone_num;
 	/*申请领取*/
-	$('#go_receive').click(function() {
+	$('#go_receive').click(function(){
 
 		phone_num = $('#get_phone').val();
 
 		$.ajax({
-			url: '/weixin_activity/weixin/bonus/?act=apply&phone=' + phone_num + '&wxid=' + wxid,
+			url: '/weixin_activity/weixin/bonus/?act=apply&phone='+phone_num+'&wxid='+wxid,
 			type: "GET",
 			success: function (xhr) {
 				if (xhr.err_code == 0) {
-					window.location.href = '/weixin_activity/weixin/bonus/?wxid=' + wxid
+					window.location.href ='/weixin_activity/weixin/bonus/?wxid='+wxid
 				} else {
 					$('.friend_top span').text(xhr.err_messege);
 					$('.friend_top').show();
