2e911c9ea9ad08e5cd74f236c7ca99db4dbdaa2f
qijinjin <qijinjin@wanglibank.com>
2016-01-20 10:30:52 +0800

    体验金账户页加是否登录验证

--- static/scripts/mobile_activity/account_experience.js
+++ static/scripts/mobile_activity/account_experience.js
@@ -258,40 +258,59 @@ var Zepto=function(){function L(t){return null==t?String(t):j[S.call(t)]||"objec
         alert : lib._alert
     }
 })();
-org.experience = (function (org) {
-    var lib = {
-        init: function () {
-            lib._goInvest()
-        },
-        _goInvest: function () {
-            /*投资*/
-            $('.accountInvestBtn').on('click', function () {
-                org.ajax({
-                    url: '/api/experience/buy/',
-                    type: 'POST',
-                    data: {},
-                    success: function (data) {
-                        if (data.ret_code > 0) {
+var login = false;
+wlb.ready({
+    app: function (mixins) {
+        mixins.sendUserInfo(function (data) {
+            if (data.ph == '') {
+                login = false;
+                mixins.registerApp({refresh:1, url:'https://staging.wanglibao.com/activity/experience/account/'});
+            } else {
+                login = true;
+                initFun();
+            }
+        })
+    },
+    other: function(){
+        initFun();
+    }
+})
+function initFun() {
+    org.experience = (function (org) {
+        var lib = {
+            init: function () {
+                lib._goInvest()
+            },
+            _goInvest: function () {
+                /*投资*/
+                $('.accountInvestBtn').on('click', function () {
+                    org.ajax({
+                        url: '/api/experience/buy/',
+                        type: 'POST',
+                        data: {},
+                        success: function (data) {
+                            if (data.ret_code > 0) {
+                                org.ui.alert(data.message, '', '4')
+                            } else {
+                                org.ui.alert('', '', '2')
+                                setTimeout(function () {
+                                    $('#alert-cont,#popubMask').hide();
+                                    location.reload();
+                                }, 2000)
+                            }
+                        },
+                        error: function (data) {
                             org.ui.alert(data.message, '', '4')
-                        } else {
-                            org.ui.alert('', '', '2')
-                            setTimeout(function () {
-                                $('#alert-cont,#popubMask').hide();
-                                location.reload();
-                            }, 2000)
                         }
-                    },
-                    error: function (data) {
-                        org.ui.alert(data.message, '', '4')
-                    }
-                });
-            })
+                    });
+                })
+            }
         }
-    }
-    return {
-        init: lib.init
-    }
-})(org);
+        return {
+            init: lib.init
+        }
+    })(org);
+}
 $.each($('script'), function(){
     var src = $(this).attr('src');
     if(src){
