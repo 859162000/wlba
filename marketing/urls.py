01e3953cb12448953996004a1e690615ca1c268a
hbiami <hbiami@gmail.com>
2016-01-20 15:46:31 +0800

    by qijj for 替换首页小狮子

--- marketing/urls.py
+++ marketing/urls.py
@@ -79,6 +79,7 @@ urlpatterns = patterns(
     url(r'^send_reward/$', TemplateView.as_view(template_name="send_reward.jade")),
     url(r'^january_reward/$', TemplateView.as_view(template_name="january_reward.jade")),
     url(r'^damai-video/$', TemplateView.as_view(template_name="damai-video.jade")),
+    url(r'^new_year/$', TemplateView.as_view(template_name="new_year.jade")),
 )
 
 # app URL
