05a095cb41d50120ce5f3e32ce0602bdf39009fd
cyj7 <cyj19900104@126.com>
2016-01-20 16:12:47 +0800

    修改接财神分享js

--- static/src/mobile_activity/h5_cammon.js
+++ static/src/mobile_activity/h5_cammon.js
@@ -63,7 +63,7 @@
             }
         });
         wx.ready(function () {
-            var host = 'https://www.wanglibao.com',
+            var host = 'https://staging.wanglibao.com',
                 shareImg = host + '/static/imgs/mobile_activity/mammon/cs_img.png',
                 shareLink = host + '/activity/weixin_mammon/',
                 shareMainTit = shareTit ? ('《财神说：'+shareTit +'》') : '《财神说：接财神、测财运、领开运红包》',
