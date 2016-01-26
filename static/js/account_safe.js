9173508e11f2be8d97bafda874b66ff236b8554a
车思雪 <100401176@qq.com>
2016-01-26 14:36:59 +0800

    手机号修改提交

--- static/js/account_safe.js
+++ static/js/account_safe.js
@@ -54,16 +54,31 @@
           }
       });
 
-      $('.phone_modify_main_wrap_1 .button').click(function(){
-          if($('.phone_change_wrap').hasClass('show')){
-              $('.phone_change_wrap').removeClass('show').hide();
-              $('.phone_modify_main_wrap_1 .button').removeClass('button_is_click');
-          }else{
-              $('.phone_change_wrap').addClass('show').show();
-              $('.phone_modify_main_wrap_1 .button').addClass('button_is_click');
+      $('.phone_modify_main_wrap_1 .phone_change_button').click(function() {
+        if ($('.phone_change_wrap').length) {
+          if ($('.phone_change_wrap').hasClass('show')) {
+            $('.phone_change_wrap').removeClass('show').hide();
+            $('.phone_modify_main_wrap_1 .phone_change_button').removeClass('button_is_click');
+          } else {
+            $('.phone_change_wrap').addClass('show').show();
+            $('.phone_modify_main_wrap_1 .phone_change_button').addClass('button_is_click');
           }
+        }else{
+          //3秒后跳转到实名认证地址
+          $('.phone_modify_popup').show();
+          var time_count = 2;
+          var timerFunction = function () {
+              if (time_count > 0) {
+                  time_count--;
+                  return
+              } else {
+                  clearInterval(timerFunction);
+                  window.location.href = '/accounts/id_verify/';
+              }
+          };
+          setInterval(timerFunction, 1000);
+        }
       })
-
 
       /*密码管理*/
 
@@ -160,98 +175,6 @@
         });
 
     /*找回交易密码 */
-
-
-      /*实名认证*/
-      $.validator.addMethod('idNumber', function(value, element) {
-          var reg;
-          reg = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/;
-          return reg.test(value);
-        }, '请输入有效身份证');
-        $('#validate_id_form').validate({
-          rules: {
-            name: {
-              required: true
-            },
-            id_number: {
-              required: true,
-              idNumber: true
-            }
-          },
-          messages: {
-            name: {
-              required: '请输入姓名'
-            },
-            id_number: {
-              required: '请输入身份证',
-              idNumber: '请输入有效身份证'
-            }
-          },
-          errorPlacement: function(error, element) {
-            return error.appendTo($(element).closest('.form-row').find('.form-row-error'));
-          },
-          submitHandler: function(form) {
-            var id_number, name;
-            name = $('#id_name').val();
-            id_number = $('#id_id_number').val();
-            if ($("#validate_id_button").hasClass("disabled")) {
-              return;
-            }
-            $("#validate_id_button").addClass('disabled');
-            return $.ajax({
-              url: '/api/id_validate/',
-              data: {
-                name: name,
-                id_number: id_number
-              },
-              type: 'post'
-            }).done(function() {
-              return tool.modalAlert({
-                title: '温馨提示',
-                msg: '实名认证成功',
-                callback_ok: function() {
-                  return location.reload();
-                }
-              });
-            }).fail(function(xhr) {
-              var result;
-              result = JSON.parse(xhr.responseText);
-              if (result.error_number === 8) {
-                tool.modalAlert({
-                  title: '温馨提示',
-                  msg: result.message
-                });
-                return;
-              } else if (result.error_number === 9) {
-                $("#validate_id_button").removeClass("disabled");
-                tool.modalAlert({
-                  title: '温馨提示',
-                  msg: result.message
-                });
-              }
-              $("#validate_id_button").removeClass("disabled");
-              tool.modalAlert({
-                title: '温馨提示',
-                msg: result.message
-              });
-              return $('.captcha-refresh', '#validate_id_form').trigger('click');
-            });
-          }
-        });
-        return (function() {
-          var url;
-          url = location.protocol + "//" + window.location.hostname + ":" + location.port + "/captcha/refresh/?v=" + (+new Date());
-          $.getJSON(url, {}, function(json) {
-            $('input[name="captcha_0"]').val(json.key);
-            return $('img.captcha').attr('src', json.image_url);
-          });
-        })();
-      /*实名认证结束*/
-
-
-
-
-
 
     })
 
