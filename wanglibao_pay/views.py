aaced295b3cdd6089793ae5d34c01fefa69e8cdf
Guoya <guoya@wanglibank.com>
2016-01-27 18:27:25 +0800

    feature: set pc_bank_num in admin

--- wanglibao_pay/views.py
+++ wanglibao_pay/views.py
@@ -79,7 +79,10 @@ class BankListView(TemplateView):
         default_bank = Bank.get_deposit_banks().filter(
             name=self.request.user.wanglibaouserprofile.deposit_default_bank_name).first()
 
-        bank_num = int(Misc.objects.get(key='pc_bank_num').value)
+        try:
+            bank_num = int(Misc.objects.get(key='pc_bank_num').value)
+        except:
+            bank_num = 14
         context.update({
             'default_bank': default_bank,
             'banks': Bank.get_deposit_banks()[:bank_num],
