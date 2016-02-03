907b80100842ade3bff5c09e8df2ae8dfa2808f4
hbiami <hbiami@gmail.com>
2016-02-03 15:47:03 +0800

    fix by hb for 网利宝农行卡用户专享福利

--- wanglibao_announcement/urls.py
+++ wanglibao_announcement/urls.py
@@ -20,4 +20,5 @@ urlpatterns = patterns(
     url(r'^rock/$', TemplateView.as_view(template_name="client_announcement_rock.jade")),
     url(r'^trading-A/$', TemplateView.as_view(template_name="client_announcement_trading_A.jade")),
     url(r'^trading-B/$', TemplateView.as_view(template_name="client_announcement_trading_B.jade")),
+    url(r'^abc-pay/$', TemplateView.as_view(template_name="client_announcement_abc_pay.jade")),
 )
