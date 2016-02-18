a79e34dbed655324c76a84c22042a71b8b6c2db6
cyj7 <cyj19900104@126.com>
2016-02-18 17:22:51 +0800

    添加ajax提交错误提示

--- static/js/festival_two.js
+++ static/js/festival_two.js
@@ -510,7 +510,7 @@ $(function(){
                     error: function(e){
                         myApp.ajaxSwitch01 = true;
                         myApp.ajaxSwitch011 = true;
-                        //alert($.parseJSON(e));
+                        alert($.parseJSON(e));
                         alert('服务器连接超时，请查看您的网络！');
                     }
                 });
