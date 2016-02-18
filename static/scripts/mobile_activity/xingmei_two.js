7389acdc9c86f9380520c4503f7716ed35a40bbb
wangruyue <rsj217@gmail.com>
2016-02-18 17:55:55 +0800

    xingmei js

--- static/scripts/mobile_activity/xingmei_two.js
+++ static/scripts/mobile_activity/xingmei_two.js
@@ -226,13 +226,15 @@ var Zepto=function(){function L(t){return null==t?String(t):j[S.call(t)]||"objec
   //  $('.xm-error').show()
   //
   //})
-  $('.no-reg').on('click',function(){
+  $('.reg-btn').on('click',function(){
     org.ajax({
       type: 'POST',
       data: {activity:'xm2'},
       url: '/api/activity/reward/',
       success: function(data){
-        if(data.ret_code==1003){
+        if(data.ret_code==1000){
+          window.location.href='/weixin/regist/?next=/activity/app_xingmei_two/?promo_token=xm2'
+        }else if(data.ret_code==1003){
           $('.xm-error').text('对不起,您不符合领取规则')
         }else if(data.ret_code==0){
           $('.xm-error').text('恭喜您,您已获得奖励,请到个人用户查看')
