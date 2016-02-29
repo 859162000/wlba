40cc0e89bbbdb3298856e7f4522a4d54bdcebecd
zhouqiyun <zhouqiyun@yunzongnet.com>
2016-02-29 15:21:15 +0800

    fix

--- wanglibao_pay/kuai_pay.py
+++ wanglibao_pay/kuai_pay.py
@@ -1134,11 +1134,13 @@ class KuaiShortPay:
             if not bank or not bank.kuai_code.strip():
                 return {"ret_code":201151, "message":"不支持该银行"}
         #fix bink new bank card warning message
-        #if len(card_no) == 10:
-            #card = Card.objects.filter(user=user, no__startswith=card_no[:6], no__endswith=card_no[-4:]).first()
-        #else:
+        if len(card_no) == 10:
+            card = Card.objects.filter(user=user, no__startswith=card_no[:6], no__endswith=card_no[-4:]).first()
+        else:
+            card = Card.objects.filter(no=card_no, user=user, bank=bank).first()
             #card = Card.objects.filter(no=card_no, user=user).first()
-            #if bank and card and bank != card.bank:
+            #pay_record = PayInfo.objects.filter(card_no=card_no, user=user, bank=bank, status='成功').count()
+            #if bank and card and bank != card.bank and pay_record==0:
                 #return {"ret_code":201153, "message":"银行卡与银行不匹配"}
 
         if not card:
