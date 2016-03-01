0243a09445db31b241443458b88911a9d18e1d02
wxqGitHub <color_bold@163.com>
2016-03-01 13:16:45 +0800

    update

--- static/src/pc/generalize.js
+++ static/src/pc/generalize.js
@@ -12,27 +12,33 @@ require(['jquery', 'jquery.waypoints'], function ($, waypoints) {
     var waypoints = $('.model-animate').waypoint(function(direction) {
         var ele = $(this.element);
         var ele_id = parseInt($(this.element).attr('id').substring(19,20));
-        console.log(ele_id)
         var model_class = $(this.element).attr('class');
         if(model_class.indexOf("active") < 0){
-            ele.addClass('active');
+            ele.addClass('active').siblings('.model-animate').removeClass('active');
         }
         $('.generalize-explain-title-'+ ele_id).fadeIn().siblings('.generalize-explain-title-pub').hide()
         $('.generalize-explain-content-'+ ele_id).fadeIn().siblings('.generalize-explain-content-pub').hide()
     }, {
-      offset: -90
+      offset: -100
     })
-
-
-    var ele_right =$(window).width()- $('.generalize-dec').offset().left- $('.generalize-dec').width()
+    var ele_right =$(window).width()- $('.generalize-dec-pub').offset().left- $('.generalize-dec-pub').width()
     var ele_top = 965;
     $(window).scroll(function () {
+        console.log($(window).scrollTop())
         if($(window).scrollTop() > ele_top){
-            $(".generalize-dec").addClass('generalize-dec-fixed').css('right', ele_right).animate({top: 100}, 300)
-        }else if($(window).scrollTop() < ele_top){
-            $(".generalize-dec").stop(!0, !0).removeAttr('style').removeClass('generalize-dec-fixed');
-        }
+                if($(window).scrollTop() > 3768){
+                    $(".generalize-dec-pub").hide()
+                    return $(".generalize-dec-other").show()
+                }else{
+                    $(".generalize-dec-pub").show()
+                     $(".generalize-dec-other").hide()
+                }
+                $(".generalize-dec-pub").addClass('generalize-dec-fixed').css('right', ele_right).animate({top: 150}, 300)
 
+        }else if($(window).scrollTop() < ele_top){
+            $(".generalize-dec-pub").stop(!0, !0).removeAttr('style').removeClass('generalize-dec-fixed');
+        }
     })
+
 
 });
