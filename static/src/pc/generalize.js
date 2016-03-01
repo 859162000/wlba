6f0924540e74a08d2003d9dcc7b3c79a7fad6906
wxqGitHub <color_bold@163.com>
2016-03-01 13:25:46 +0800

    update js

--- static/src/pc/generalize.js
+++ static/src/pc/generalize.js
@@ -8,37 +8,37 @@ require.config({
 });
 
 require(['jquery', 'jquery.waypoints'], function ($, waypoints) {
-
     var waypoints = $('.model-animate').waypoint(function(direction) {
         var ele = $(this.element);
         var ele_id = parseInt($(this.element).attr('id').substring(19,20));
-        var model_class = $(this.element).attr('class');
-        if(model_class.indexOf("active") < 0){
-            ele.addClass('active').siblings('.model-animate').removeClass('active');
-        }
-        $('.generalize-explain-title-'+ ele_id).fadeIn().siblings('.generalize-explain-title-pub').hide()
+        ele.addClass('active').siblings('.model-animate').removeClass('active');
+        $('.generalize-explain-title-'+ ele_id).fadeIn().siblings('.generalize-explain-title-pub').hide();
         $('.generalize-explain-content-'+ ele_id).fadeIn().siblings('.generalize-explain-content-pub').hide()
     }, {
       offset: -100
     })
-    var ele_right =$(window).width()- $('.generalize-dec-pub').offset().left- $('.generalize-dec-pub').width()
-    var ele_top = 965;
-    $(window).scroll(function () {
-        console.log($(window).scrollTop())
-        if($(window).scrollTop() > ele_top){
-                if($(window).scrollTop() > 3768){
-                    $(".generalize-dec-pub").hide()
-                    return $(".generalize-dec-other").show()
-                }else{
-                    $(".generalize-dec-pub").show()
-                     $(".generalize-dec-other").hide()
-                }
-                $(".generalize-dec-pub").addClass('generalize-dec-fixed').css('right', ele_right).animate({top: 150}, 300)
 
-        }else if($(window).scrollTop() < ele_top){
-            $(".generalize-dec-pub").stop(!0, !0).removeAttr('style').removeClass('generalize-dec-fixed');
+    var
+        $download_layout = $('.generalize-dec-pub'),
+        $download_layout_copy = $('.generalize-dec-other'),
+        $download_layout_RIGHT =$(window).width()- $download_layout.offset().left- $download_layout.width(),
+        $download_layout_TOP = 965;
+
+    $(window).scroll(function () {
+        if($(window).scrollTop() > $download_layout_TOP){
+            if($(window).scrollTop() > 3768){
+                $download_layout.hide();
+                return $download_layout_copy.show()
+            }else{
+                $download_layout.show();
+                $download_layout_copy.hide()
+            }
+
+            $download_layout.addClass('generalize-dec-fixed').css('right', $download_layout_RIGHT).animate({top: 150}, 300)
+
+        }else if($(window).scrollTop() < $download_layout_TOP){
+            $download_layout.stop(!0, !0).removeAttr('style').removeClass('generalize-dec-fixed');
         }
     })
-
 
 });
