266a92cabc8278caf9b7935c3e76e357122bc49f
车思雪 <100401176@qq.com>
2016-03-03 13:46:58 +0800

    提交春日活动

--- static/src/mobile_activity/app_spring_mobilization.js
+++ static/src/mobile_activity/app_spring_mobilization.js
@@ -152,8 +152,8 @@
                 type: 'post',
                 success: function (data1) {
 
-                    $('.card_box[data-card="'+card_no+'"] .num').text(data1.redpack.amount);
-                    $(this).find('.card').addClass('card_box_open');
+                    $('.card_box[data-card="'+card_no+'"] .num').text(data1.redpack.amount+'元');
+                    $('.card_box[data-card="'+card_no+'"]').find('.card').addClass('card_box_open');
 
                 },error: function(data1){
                     $('.popup_box .text').text(data1.message);
