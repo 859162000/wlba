abd754cbaf3d2b95411f1f1813067edbe32333e7
yihen <liuyihen@gmail.com>
2016-03-03 11:20:29 +0800

    增加提示语参数，remark

--- wanglibao_p2p/trade.py
+++ wanglibao_p2p/trade.py
@@ -189,6 +189,7 @@ class P2PTrader(object):
                                                 "keyword2": "%s 元"%str(amount),
                                                 "keyword3": now,
                                                 "url":settings.CALLBACK_HOST + '/weixin/activity_ggl/?order_id=%s' % (self.order_id),
+                                                "remark": u'\n恭喜您获得3次刮奖机会，速来戳"详情"拼运气!'
                                                     })},
                                                 queue='celery02')
 
