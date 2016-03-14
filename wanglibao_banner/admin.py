62dea8517eee4cfd9f1f780f679d201833751770
546133753 <546133753@qq.com>
2016-03-14 12:03:04 +0800

    静态页优化

--- wanglibao_banner/admin.py
+++ wanglibao_banner/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from wanglibao_banner.models import Banner, Partner, Hiring, Aboutus, AppActivate
+from wanglibao_banner.models import Banner, Partner, Hiring, Aboutus, AppActivate, AboutDynamic
 
 
 class BannerAdmin(admin.ModelAdmin):
@@ -13,7 +13,7 @@ class PartnerAdmin(admin.ModelAdmin):
 
 
 class HiringAdmin(admin.ModelAdmin):
-    list_display = ('name', 'is_urgent', 'is_hide', 'priority')
+    list_display = ('name', 'position_types', 'is_urgent', 'is_hide', 'priority')
     list_editable = ('priority',)
 
 
@@ -25,8 +25,13 @@ class AppActivateAdmin(admin.ModelAdmin):
     list_display = ('name', 'device', 'img_one', 'img_two', 'img_three', 'img_four', 'last_updated', 'is_long_used', 'start_at', 'end_at', 'is_used', )
 
 
+class AboutDynamicAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'start_time', 'end_time', 'priority', 'hide_in_list', 'updated_time')
+
+
 admin.site.register(Banner, BannerAdmin)
 admin.site.register(Partner, PartnerAdmin)
 admin.site.register(Hiring, HiringAdmin)
 admin.site.register(Aboutus, AboutusAdmin)
 admin.site.register(AppActivate, AppActivateAdmin)
+admin.site.register(AboutDynamic, AboutDynamicAdmin)
