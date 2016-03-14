260619823693ad26a8f5d85a32e464f0fc44a287
249143979 <249143979@qq.com>
2016-03-14 14:55:29 +0800

    投资成功后从投资确认中拆分红包金额

--- wanglibao_margin/marginkeeper.py
+++ wanglibao_margin/marginkeeper.py
@@ -100,6 +100,20 @@ class MarginKeeper(KeeperBaseMixin):
             record = self.__tracer(catalog, amount, margin.margin, description)
             return record
 
+    def settle_redpack(self, amount, description=u'', savepoint=True):
+        amount = Decimal(amount)
+        check_amount(amount)
+        with transaction.atomic(savepoint=savepoint):
+            margin = Margin.objects.select_for_update().filter(user=self.user).first()
+            if amount > margin.freeze:
+                logger.debug('user id: {}, amount:{}, freeze red pack:{} ========'.format(self.user.id, amount, margin.freeze))
+                raise MarginLack(u'202')
+            margin.freeze -= amount
+            margin.save()
+            catalog = u'红包交易成功扣款'
+            record = self.__tracer(catalog, amount, margin.margin, description)
+            return record
+
     def amortize(self, principal, interest, penal_interest, coupon_interest, redpack_amount=0, description=u'', savepoint=True):
         check_amount(principal)
         check_amount(interest)
