27c003c378e9d7ae9360efd1e13bf9a447d71c64
546133753 <546133753@qq.com>
2016-03-17 11:39:14 +0800

    渠道中心修改

--- wanglibao/settings.py
+++ wanglibao/settings.py
@@ -245,7 +245,7 @@ LOGGING = {
     },
     'handlers': {
         'console': {
-            'level': 'DEBUG',
+            'level': 'INFO',
             'class': 'logging.StreamHandler',
             'formatter': 'simple'
         },
