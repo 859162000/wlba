66e476ca26dbfd49b3cb5b5e309e4da90cfea440
车思雪 <100401176@qq.com>
2016-03-21 17:04:12 +0800

    提交关于网利宝

--- static/src/pc/str.js
+++ static/src/pc/str.js
@@ -1,5 +1,4 @@
 require(['jquery'], function( $ ) {
-    var str1= '[图片]',str2 = '[视频]';
     $.each($('.description'),function(i,o){
         labelToStr($(o),95);
     })
@@ -7,22 +6,9 @@ require(['jquery'], function( $ ) {
         labelToStr($(o),130)
     })
     function labelToStr(obj,counts){
-        var str = obj.html(),
-            img = obj.find('img'),
-            embed = obj.find('embed');
-        if(img.length > 0){
-            img.before(('<span>'+str1+'</span>'));
-            img.remove();
-        }
-        if(embed.length > 0){
-            embed.before(('<span>'+str2+'</span>'));
-            embed.remove();
-        }
+        var str = obj.html();
         if(str.length > counts){
-            //if(obj.find('p').first().text().length > counts || obj.find('p').length > 2){
-            //    obj.parent().find('.link').addClass('link-p');
-            //}
-            obj.html($.trim(obj.html()).substring(0,counts)+'...')
+            obj.text($.trim(obj.text()).substring(0,counts)+'...')
         }
     }
 })
