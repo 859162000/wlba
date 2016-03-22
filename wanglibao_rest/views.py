4f8dd5ea2f394aa387345b358ace6ce7ace1d88a
546133753 <546133753@qq.com>
2016-03-22 13:21:26 +0800

    渠道中心修改

--- wanglibao_rest/views.py
+++ wanglibao_rest/views.py
@@ -395,14 +395,16 @@ class CoopDataDispatchApi(APIView):
 
                 product_instance = P2PProduct.objects.filter(pk=product['id']).first()
                 if product_instance:
+                    product_instance_status = product_instance.status
+                    product_instance_soldout_time = product_instance.soldout_time
                     product_form = P2PProductForm(product, instance=product_instance)
                 else:
                     product_form = P2PProductForm(product)
                 if product_form.is_valid():
                     if product_instance:
-                        if product_instance.status != product['status']:
+                        if product_instance_status != product['status']:
                             # 如果产品已经售完，则更新投资记录的售完时间
-                            if not product_instance.soldout_time and product['soldout_time']:
+                            if not product_instance_soldout_time and product['soldout_time']:
                                 P2PRecord.objects.filter(product=product_instance).update(invest_end_time=product['soldout_time'])
                             product_form.save()
                     else:
