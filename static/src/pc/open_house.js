80ef9bf09ba7eef386ff51f232c5042d1fd66274
qijinjin <qijinjin@wanglibank.com>
2016-03-25 16:34:53 +0800

    开放日

--- static/src/pc/open_house.js
+++ static/src/pc/open_house.js
@@ -11,7 +11,7 @@
         }
     });
     require(['jquery',"tools"], function($,tool) {
-        var reg = /^1[3|5|8]\d{9}$/;
+        var reg = /^1\d{10}$/;
         var isphone = function(date){
             if(date.ret_code == 10000){
                 $(".mesg,.dialog").show();
