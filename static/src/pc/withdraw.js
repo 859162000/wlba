8286ba4d6acd2b5a0e6b28aabbc735165b928606
wxqGitHub <color_bold@163.com>
2016-03-25 13:24:17 +0800

    save

--- static/src/pc/withdraw.js
+++ static/src/pc/withdraw.js
@@ -1,6 +1,95 @@
 (function () {
 
-    require(['jquery', 'tools', 'jquery.validate', 'csrf'], function ($, tool) {
+    require(['jquery', 'tools', 'jquery.validate', 'csrf'], function ($, tool, validate) {
+
+
+        var $amount = $('input[name=amount]');
+        var $fee = $('input[name=fee]');
+
+        $('.fees-alert').on('click', function () {
+            $(this).modal()
+        })
+
+        var margin = $('.withdraw-margin').data('margin');
+        $('.cash-amount-all').on('click', function () {
+            $amount.trigger('focus').val(margin)
+        })
+
+        $.validator.addMethod("balance", function (value, element) {
+            var balance = $(element).attr('data-balance')
+            return (value - balance).toFixed(2) <= 0
+        });
+        $.validator.addMethod("money", function (value, element) {
+            var re = /^\d+(\.\d{0,2})?$/
+            return re.test
+            value
+        });
+        $.validator.addMethod("huge", function (value, element) {
+            return value <= max_amount;
+        });
+        $.validator.addMethod("small", function (value, element) {
+            var balance;
+            balance = $(element).attr('data-balance');
+            if (value <= 0) {
+                return false;
+            }
+            if (balance - value === 0) {
+                return true;
+            } else if (value >= min_amount) {
+                return true;
+            }
+            return false;
+        });
+
+        var max_amount = parseInt($fee.attr('data-max_amount')),
+            min_amount = parseInt($fee.attr('data-min_amount'));
+
+        $('#withdraw-form').validate({
+            rules: {
+                amount: {
+                    required: true,
+                    money: true,
+                    balance: false,
+                    huge: false,
+                    small: false
+                },
+                validate_code: {
+                    required: true
+                },
+                captcha_1: {
+                    required: true,
+                    minlength: 1
+                },
+                trade_pwd: {
+                    required: true
+                }
+            },
+            messages: {
+                amount: {
+                    required: '请输入金额',
+                    money: '请输入正确的金额格式',
+                    balance: '余额不足',
+                    huge: '单笔提现金额不能超过' + max_amount + '万元',
+                    small: '最低提现金额 ' + min_amount + ' 元起。如果余额低于 ' + min_amount + ' 元，请一次性取完。'
+                },
+                validate_code: {
+                    required: '请输入验证码'
+                },
+                captcha_1: {
+                    required: '不能为空',
+                    minlength: $.format("验证码至少输入1位")
+                },
+                trade_pwd: {
+                    required: '请输入交易密码'
+                }
+            },
+            errorPlacement: function (error, element) {
+                return error.appendTo($(element).parents('td').children('.form-row-error'));
+            },
+            submitHandler: function (form) {
+
+            }
+        })
 
     });
 
