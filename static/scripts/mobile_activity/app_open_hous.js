a41f460a3895ea5278a4893bd3a624c718703fe3
zzljack <15601017102@163.com>
2016-03-28 13:55:10 +0800

    开放日

--- static/scripts/mobile_activity/app_open_hous.js
+++ static/scripts/mobile_activity/app_open_hous.js
@@ -223,6 +223,7 @@ var Zepto=function(){function L(t){return null==t?String(t):j[S.call(t)]||"objec
             if(date.ret_code == 10000){
                 $(".mesg,.dialog").show();
             }else{
+
                 if(date.message.name){
                     $(".lg_name").text(date.message.name[0]);
                 }
@@ -242,6 +243,7 @@ var Zepto=function(){function L(t){return null==t?String(t):j[S.call(t)]||"objec
                data : option,
                async : false,
                success : function(date){
+                   console.log(date)
                    isphone(date);
                },
                error : function(){
@@ -256,10 +258,27 @@ var Zepto=function(){function L(t){return null==t?String(t):j[S.call(t)]||"objec
             var opt = {"phone": phone, "name": name, "address": address};
             if(!reg.test(phone)){
                 $(".lg_phone").text("*手机号输入错误");
+            }else if(strlen(name)>20){
+                $(".lg_name").text("*姓名输入错误");
+            }else if(strlen(address)>20){
+                $(".lg_address").text("*地址输入错误");
             }else{
                 ajaxFn("/api/activity_user_info/upload/",opt);
             }
         };
+        function strlen(str){
+            var len = 0;
+            for(var i=0; i<str.length; i++){
+                var c = str.charCodeAt(i);
+                if ((c >= 0x0001 && c <= 0x007e) || (0xff60<=c && c<=0xff9f)) {
+                   len++;
+                 }
+                 else {
+                  len+=2;
+                 }
+            }
+            return len;
+        }
 
         $("#login_btn").on("click",Event);
         $("#lg_uls").on("keyup","input",function(){
