d7d0405f5113b2e0887ef60fc43e38d8e2bc9dcc
cyj7 <cyj19900104@126.com>
2016-04-05 15:25:52 +0800

    还原发送验证码js

--- static/scripts/mobile/dev/regist.js
+++ static/scripts/mobile/dev/regist.js
@@ -244,11 +244,8 @@ webpackJsonp([12],[
 	Object.defineProperty(exports, "__esModule", {
 	    value: true
 	});
-	exports.Automatic = undefined;
 
 	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-	var _images_validation = __webpack_require__(12);
 
 	function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
@@ -345,10 +342,6 @@ webpackJsonp([12],[
 	                if (icon != '') target.siblings('.' + icon).addClass('active');
 	                if (othericon != '') {
 	                    $('.' + othericon).removeAttr('disabled');
-	                    if (_images_validation.timeIntervalId) {
-	                        clearInterval(_images_validation.timeIntervalId);
-	                        $('.' + othericon).text('获取验证码');
-	                    }
 	                }
 	                if (operation != '') target.siblings('.' + operation).show();
 	            }
@@ -543,7 +536,7 @@ webpackJsonp([12],[
 	Object.defineProperty(exports, "__esModule", {
 	    value: true
 	});
-	exports.validation = exports.timeIntervalId = undefined;
+	exports.validation = undefined;
 
 	var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
@@ -553,9 +546,9 @@ webpackJsonp([12],[
 
 	var _from_validation = __webpack_require__(6);
 
-	var timeIntervalId = exports.timeIntervalId = null;
 	var validation = exports.validation = function validation($phone, $captcha_0, $captcha_1, $captcha) {
 
+	    var timeIntervalId = null;
 	    var $validate_operation = $('button[name=validate_operation]');
 
 	    //获取图像验证码
@@ -633,7 +626,7 @@ webpackJsonp([12],[
 	                }
 	            };
 	            timerFunction();
-	            return exports.timeIntervalId = timeIntervalId = setInterval(timerFunction, 1000);
+	            return timeIntervalId = setInterval(timerFunction, 1000);
 	        });
 	    }
 
