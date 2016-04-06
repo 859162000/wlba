f9c65493471b70a98408ebe5c3f6cc6f5e2170eb
cyj7 <cyj19900104@126.com>
2016-04-06 18:07:30 +0800

    红包雨

--- wanglibao_invite/urls.py
+++ wanglibao_invite/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns(
     '',
-    url(r'^sub_checkIn_share/$', TemplateView.as_view(template_name="service_checkIn_share.jade")),
+    url(r'^ser_redpack_index/$', TemplateView.as_view(template_name="service_redpack_index.jade")),
 
 )
 
