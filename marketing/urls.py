28053ee9886f3dd5d19fb2a5598e963fc238951d
Guoya <guoya@wanglibank.com>
2016-04-07 15:46:03 +0800

    feaure1: change textfor feifan; feature2: wangli_vip for chesixue

--- marketing/urls.py
+++ marketing/urls.py
@@ -87,6 +87,7 @@ urlpatterns = patterns(
     url(r'^damai-back/$', TemplateView.as_view(template_name="damai-back.jade")),
     url(r'^open_house/$', TemplateView.as_view(template_name="open_house.jade")),
     # url(r'^spring_mobilization/$', TemplateView.as_view(template_name="spring_mobilization.jade")),
+    url(r'^wangli_vip/$', TemplateView.as_view(template_name="wangli_vip.jade")),
 
     url(r'^one_lifestyle/$', TemplateView.as_view(template_name="lifestyle.jade")),
     url(r'^xunlei_three/$', ThunderTenAcvitityTemplate.as_view(wx_classify='fwh', wx_code='')),
@@ -189,7 +190,7 @@ urlpatterns += patterns(
     url(r'^app_send_reward/$', TemplateView.as_view(template_name="app_send_reward.jade")),
     url(r'^app_january_reward/$', TemplateView.as_view(template_name="app_january_reward.jade")),
     # url(r'^app_thanksgivin/$', login_required(TemplateView.as_view(template_name="app_thanksgivin.jade"), login_url='/accounts/token_login/')),
-    # url(r'^app_spring_mobilization/$', TemplateView.as_view(template_name="app_spring_mobilization.jade")),
+    url(r'^app_wangli_vip/$', TemplateView.as_view(template_name="app_wangli_vip.jade")),
 
     url(r'^weixin_mammon/$', TemplateView.as_view(template_name="h5_mammon.jade")),
     url(r'^app_two_novice/$', TemplateView.as_view(template_name="app_two_novice.jade")),
---
+++ static/js/wangli_vip.js
@@ -0,0 +1,68 @@
+(function() {
+    require.config({
+        paths: {
+            jquery: 'lib/jquery.min'
+        },
+        shim: {
+            'jquery.modal': ['jquery']
+        }
+    });
+    require(['jquery'],
+    function($, re) {
+
+        var csrfSafeMethod, getCookie, sameOrigin,
+        getCookie = function (name) {
+              var cookie, cookieValue, cookies, i;
+              cookieValue = null;
+              if (document.cookie && document.cookie !== "") {
+                  cookies = document.cookie.split(";");
+                  i = 0;
+                  while (i < cookies.length) {
+                      cookie = $.trim(cookies[i]);
+                      if (cookie.substring(0, name.length + 1) === (name + "=")) {
+                          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                          break;
+                      }
+                      i++;
+                  }
+              }
+              return cookieValue;
+          };
+      csrfSafeMethod = function (method) {
+          return /^(GET|HEAD|OPTIONS|TRACE)$/.test(method);
+      };
+      sameOrigin = function (url) {
+          var host, origin, protocol, sr_origin;
+          host = document.location.host;
+          protocol = document.location.protocol;
+          sr_origin = "//" + host;
+          origin = protocol + sr_origin;
+          return (url === origin || url.slice(0, origin.length + 1) === origin + "/") || (url === sr_origin || url.slice(0, sr_origin.length + 1) === sr_origin + "/") || !(/^(\/\/|http:|https:).*/.test(url));
+      };
+      $.ajaxSetup({
+          beforeSend: function (xhr, settings) {
+              if (!csrfSafeMethod(settings.type) && sameOrigin(settings.url)) {
+                  xhr.setRequestHeader("X-CSRFToken", getCookie("csrftoken"));
+              }
+          }
+      });
+
+      var tab_title;
+      $('.privilege_box .tab').click(function(){
+          $(this).addClass('tab_choose').siblings().removeClass('tab_choose');
+          tab_title = $(this).attr('data-title');
+          $(''+tab_title+'').show().siblings().hide();
+      })
+
+
+      $('#body_wrap #banenr .banner_main .view').click(function(){
+          $(this).css({'color':'#fff','border-bottom':'1px #fff solid'});
+          $('.slide_bottom').slideDown('fast');
+      })
+      $('#body_wrap .slide_bottom .main .bottom_arrow').click(function(){
+          $('.slide_bottom').slideUp('fast');
+          $('#body_wrap #banenr .banner_main .view').css({'color':'#ffea00','border-bottom':'1px #ffea00 solid'});
+      })
+    })
+
+}).call(this);
