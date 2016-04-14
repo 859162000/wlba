0321e5fa847ef6bd4a1416bce9781f5ea1c64d59
qijinjin <qijinjin@wanglibank.com>
2016-04-14 20:03:04 +0800

    app_april_mobilization

--- static/src/mobile_activity/april_mobilization.js
+++ static/src/mobile_activity/april_mobilization.js
@@ -6,21 +6,42 @@
         },
         success: function (data) {
             var result = data.weekranks;
-            var str = '';
+            var str = '',sty = '',icon = '';
             $.each(result,function(i,o){
-                str+='<tr><td>'+o.phone.substring(0,3)+'****' +o.phone.substr(o.phone.length-4) +'</td><td>'+ fmoney(o.amount__sum) +' 元</td><td>'+ o.aa +'</td></tr>'
+                if(i <= 2){
+                    sty ='red';
+                    icon = 'icon'+ (i+1)
+                }else{
+                    sty ='';
+                }
+                str+='<tr class='+ sty +'><td><span class='+ icon +'>'+ (i+1) +'</span>'+o.phone.substring(0,3)+'****' +o.phone.substr(o.phone.length-4) +'</td><td class="tl">'+ fmoney(o.amount__sum) +' 元</td><td>'+ o.coupon +'</td></tr>'
             })
             $('#list').append(str);
+            $('.transaction-counts').html(fmoney1(data.week_sum_amount,2))
         }
     })
     function fmoney(s, n) {
         n = n > 0 && n <= 20 ? n : 2;
         s = parseFloat((s + "").replace(/[^\d\.-]/g, "")).toFixed(n) + "";
-        var l = s.split(".")[0].split("").reverse();
+        var l = s.split(".")[0].split("").reverse(), r = s.split(".")[1];
         t = "";
         for (i = 0; i < l.length; i++) {
             t += l[i] + ((i + 1) % 3 == 0 && (i + 1) != l.length ? "," : "");
         }
-        return t.split("").reverse().join("");
+        if(n == 0){
+            return t.split("").reverse().join("");
+        }else{
+            return t.split("").reverse().join("") + "," + r;
+        }
+    }
+    function fmoney1(s, n) {
+        n = n > 0 && n <= 20 ? n : 2;
+        s = parseFloat((s + "").replace(/[^\d\.-]/g, "")).toFixed(n) + "";
+        var l = s.split(".")[0].split("").reverse(), r = s.split(".")[1];
+        t = "";
+        for (i = 0; i < l.length; i++) {
+            t += l[i] + ((i + 1) % 3 == 0 && (i + 1) != l.length ? "," : "");
+        }
+        return t.split("").reverse().join("") + "," + r;
     }
 })(org);
