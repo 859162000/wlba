354e5de71a567118f8e9a97aac5cffcd59dadebd
车思雪 <100401176@qq.com>
2016-04-19 15:13:39 +0800

    提交404条转问题

--- static/js/app_airport_operation.js
+++ static/js/app_airport_operation.js
@@ -17,7 +17,53 @@ $(function(){
               $(this).removeClass("audio_off");
               ss = 1;
           }
-     },false)
+     },false);
+
+    var jsApiList = ['scanQRCode', 'onMenuShareAppMessage','onMenuShareTimeline','onMenuShareQQ'];
+	org.ajax({
+		type : 'GET',
+		url : '/weixin/api/jsapi_config/',
+		dataType : 'json',
+		success : function(data) {
+			//请求成功，通过config注入配置信息,
+			wx.config({
+				debug: false,
+				appId: data.appId,
+				timestamp: data.timestamp,
+				nonceStr: data.nonceStr,
+				signature: data.signature,
+				jsApiList: jsApiList
+			});
+		}
+	});
+	wx.ready(function(){
+		var host = 'https://staging.wanglibao.com/',
+			shareName = '网利宝影像投资节送福利喽',
+			shareImg = host + '/static/imgs/mobile_activity/app_august_phone/300x300.jpg',
+			shareLink = host + 'weixin_activity/app_august_phone/',
+			shareMainTit = '网利宝影像投资节送福利喽',
+			shareBody = '全民福利 火速领取';
+		//分享给微信好友
+		org.onMenuShareAppMessage({
+			title: shareMainTit,
+			desc: shareBody,
+			link: shareLink,
+			imgUrl: shareImg
+		});
+		//分享给微信朋友圈
+		org.onMenuShareTimeline({
+			title: '网利宝影像投资节送福利喽',
+			link : shareLink,
+			imgUrl: shareImg
+		})
+		//分享给QQ
+		org.onMenuShareQQ({
+			title: shareMainTit,
+			desc: shareBody,
+			link : shareLink,
+			imgUrl: shareImg
+		})
+	})
 });
 
 
