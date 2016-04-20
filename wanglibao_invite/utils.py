0cb7df280ede02f9d5dd517975e9ed04b22c1345
huomeimei <huomeimei@wanglibank.com>
2016-04-20 10:55:15 +0800

    fix 领取红包雨接口

--- wanglibao_invite/utils.py
+++ wanglibao_invite/utils.py
@@ -48,10 +48,10 @@ def getWechatDailyReward(openid):
         }
     )
     if daily_reward.status:
-        return -1, "", 0
+        return -1, "已经领取过", 0
     user = w_user.user
     if not user:
-        return -1, "", 0
+        return 0, "", 0
     return sendDailyReward(user, daily_reward.id, save_point=True)
 
 
@@ -60,7 +60,7 @@ def sendDailyReward(user, daily_reward_id, save_point=False, new_registed=False)
         daily_reward = WechatUserDailyReward.objects.select_for_update().get(id=daily_reward_id)
         amount = 0
         if daily_reward.status:
-            return -1, "", 0
+            return -1, "已经领取过", 0
         if daily_reward.reward_type == "redpack":
             share_invite_config = getMiscValue("redpack_rain_award_config")
             # {"reward_types":["redpack", "experience_gold"], "daily_rewards":{'371': [0,20],'372': [0,30], '373': [0,40], '374': [0,10]},
@@ -84,7 +84,7 @@ def sendDailyReward(user, daily_reward_id, save_point=False, new_registed=False)
         if daily_reward.reward_type == "experience_gold":
             experience_record_id, experience_event = SendExperienceGold(user).send(pk=daily_reward.experience_gold_id)
             if not experience_record_id:
-                return -1, "", amount
+                return -1, "体验金活动不存在", amount
             daily_reward.experience_gold_record_id = experience_record_id
             amount = experience_event.amount
         daily_reward.user = user
@@ -92,59 +92,3 @@ def sendDailyReward(user, daily_reward_id, save_point=False, new_registed=False)
         daily_reward.save()
         return 0, "ok", amount
 
-# def sendInviteReward(user):
-#     # {"reward_types":["redpack", "experience_gold"], "daily_rewards":{'1': [0,20],'2': [0,30], '3': [0,40], '4': [0,10]}
-#     # "first_invest":1000, "base_experience_amount":200000, "first_invest_reward":1, "first_invest_reward_type":0, "invite_experience_id":1}
-#     share_invite_config = getMiscValue("share_invite_config")
-#     base_experience_amount = share_invite_config.get('base_experience_amount', 0)
-#     # InviteRewardRecord
-#     inv_relation = InviteRelation.objects.filter(user=user).first()
-#     if inv_relation:
-#         inviter = inv_relation.inviter
-#         if inviter:
-#             inviter_extra = UserExtraInfo.objects.filter(user=inviter).first()
-#             if inviter_extra.invite_experience_amount < base_experience_amount:
-#                 #todo
-#                 invite_experience_id = share_invite_config['invite_experience_id']
-#                 SendExperienceGold(user).send(pk=invite_experience_id)
-#
-# def sendInvestReward(user):
-#     # {"reward_types":["redpack", "experience_gold"], "daily_rewards":{'1': [0,20],'2': [0,30], '3': [0,40], '4': [0,10]}
-#     # "first_invest":1000, "base_experience_amount":200000, "first_invest_reward":1, "first_invest_reward_type":0, "invite_experience_id":1}
-#     share_invite_config = getMiscValue("share_invite_config")
-#     # InviteRewardRecord
-#     inv_relation = InviteRelation.objects.filter(user=user).first()
-#     if inv_relation:
-#         inviter = inv_relation.inviter
-#         if inviter:
-#             first_buy = P2PRecord.objects.filter(user=user,
-#                                                  # create_time__gt=
-#                                                  ).order_by('create_time').first()
-
-
-# [2016-04-14 15:08:56,293: INFO/MainProcess] Task weixin.tasks.bind_ok[22a2c590-55c5-437a-a647-2ba7c2dcaeeb] succeeded in 0.153715637s: None
-# [2016-04-14 15:08:56,344: ERROR/MainProcess] Task wanglibao_invite.tasks.processShareInviteDailyReward[8388bdaa-033e-4ebb-ae25-a8e607ace6a8] raised unexpected: TypeError("'int' object has no attribute '__getitem__'",)
-# Traceback (most recent call last):
-#   File "/usr/local/lib/python2.7/dist-packages/celery/app/trace.py", line 240, in trace_task
-#     R = retval = fun(*args, **kwargs)
-#   File "/usr/local/lib/python2.7/dist-packages/celery/app/trace.py", line 437, in __protected_call__
-#     return self.run(*args, **kwargs)
-#   File "/home/wanglibao-dev/wangli-backend/wanglibao-backend/wanglibao_invite/tasks.py", line 13, in processShareInviteDailyReward
-#     user = User.objects.get(user_id)
-#   File "/usr/local/lib/python2.7/dist-packages/django/db/models/manager.py", line 151, in get
-#     return self.get_queryset().get(*args, **kwargs)
-#   File "/usr/local/lib/python2.7/dist-packages/django/db/models/query.py", line 301, in get
-#     clone = self.filter(*args, **kwargs)
-#   File "/usr/local/lib/python2.7/dist-packages/django/db/models/query.py", line 593, in filter
-#     return self._filter_or_exclude(False, *args, **kwargs)
-#   File "/usr/local/lib/python2.7/dist-packages/django/db/models/query.py", line 611, in _filter_or_exclude
-#     clone.query.add_q(Q(*args, **kwargs))
-#   File "/usr/local/lib/python2.7/dist-packages/django/db/models/sql/query.py", line 1198, in add_q
-#     if not self.need_having(q_object):
-#   File "/usr/local/lib/python2.7/dist-packages/django/db/models/sql/query.py", line 1161, in need_having
-#     return any(self.need_having(c) for c in obj.children)
-#   File "/usr/local/lib/python2.7/dist-packages/django/db/models/sql/query.py", line 1161, in <genexpr>
-#     return any(self.need_having(c) for c in obj.children)
-#   File "/usr/local/lib/python2.7/dist-packages/django/db/models/sql/query.py", line 1158, in need_having
-#     return (refs_aggregate(obj[0].split(LOOKUP_SEP), self.aggregates)
-# TypeError: 'int' object has no attribute '__getitem__'
