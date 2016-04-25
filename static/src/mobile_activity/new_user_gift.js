b6aaf9a1d154dbdd12db711a3a8fcfe7cc0c26b7
cyj7 <cyj19900104@126.com>
2016-04-25 17:02:59 +0800

    尊贵新人礼-相关文件

--- static/src/mobile_activity/new_user_gift.js
+++ static/src/mobile_activity/new_user_gift.js
@@ -16,7 +16,6 @@ $(function(){
             $(".page-loading").hide();
             $("#swiper-container .swiper-wrapper,#next-box").show();
         }, 1000);
-        alert($(window).width());
     };
 
     //弹层
@@ -45,63 +44,63 @@ $(function(){
         }
     });
 
-    var weiURL = '/weixin/api/jsapi_config/';
-    var jsApiList = ['scanQRCode', 'onMenuShareAppMessage', 'onMenuShareTimeline', 'onMenuShareQQ'];
-    org.ajax({
-        type: 'GET',
-        url: weiURL,
-        dataType: 'json',
-        success: function (data) {
-            //请求成功，通过config注入配置信息,
-            wx.config({
-                debug: false,
-                appId: data.appId,
-                timestamp: data.timestamp,
-                nonceStr: data.nonceStr,
-                signature: data.signature,
-                jsApiList: jsApiList
-            });
-        }
-    });
-    wx.ready(function () {
-        var winHost = window.location.href;
-        var host = winHost.substring(0,winHost.indexOf('/activity')),
-            shareImg = host + '/static/imgs/mobile/weChat_logo.png',
-            shareLink = host + '/activity/h5_recruit/',
-            shareMainTit = '极致工作疯狂玩乐，最有“宝”的互联网金融公司',
-            shareBody = '2016我们一起High，你来不来？';
-        //分享给微信好友
-         org.onMenuShareAppMessage({
-            title: shareMainTit,
-            desc: shareBody,
-            link: shareLink,
-            imgUrl: shareImg
-        });
-        //分享给微信朋友圈
-        org.onMenuShareTimeline({
-            title: shareMainTit,
-            link : shareLink,
-            imgUrl: shareImg
-        });
-        //分享给QQ
-        org.onMenuShareQQ({
-            title: shareMainTit,
-            desc: shareBody,
-            link : shareLink,
-            imgUrl: shareImg
-        });
-    });
+
 
     wlb.ready({
         app: function(mixins){
-            //mixins.loginApp()
+            mixins.loginApp();
             document.getElementById('refresh').onclick= function(){
                 window.location.reload();
             }
             mixins.shareData({title: "网利宝,是一种生活方式", content: "2015，你的收益如何？让他们来跟你分享下，投资创造美好生活的心得吧~"});
         },
         other: function(){
-
+            var weiURL = '/weixin/api/jsapi_config/';
+            var jsApiList = ['scanQRCode', 'onMenuShareAppMessage', 'onMenuShareTimeline', 'onMenuShareQQ'];
+            org.ajax({
+                type: 'GET',
+                url: weiURL,
+                dataType: 'json',
+                success: function (data) {
+                    //请求成功，通过config注入配置信息,
+                    wx.config({
+                        debug: false,
+                        appId: data.appId,
+                        timestamp: data.timestamp,
+                        nonceStr: data.nonceStr,
+                        signature: data.signature,
+                        jsApiList: jsApiList
+                    });
+                }
+            });
+            wx.ready(function () {
+                var winHost = window.location.href;
+                var host = winHost.substring(0,winHost.indexOf('/activity')),
+                    shareImg = host + '/static/imgs/mobile/weChat_logo.png',
+                    shareLink = host + '/activity/h5_recruit/',
+                    shareMainTit = '极致工作疯狂玩乐，最有“宝”的互联网金融公司',
+                    shareBody = '2016我们一起High，你来不来？';
+                //分享给微信好友
+                 org.onMenuShareAppMessage({
+                    title: shareMainTit,
+                    desc: shareBody,
+                    link: shareLink,
+                    imgUrl: shareImg
+                });
+                //分享给微信朋友圈
+                org.onMenuShareTimeline({
+                    title: shareMainTit,
+                    link : shareLink,
+                    imgUrl: shareImg
+                });
+                //分享给QQ
+                org.onMenuShareQQ({
+                    title: shareMainTit,
+                    desc: shareBody,
+                    link : shareLink,
+                    imgUrl: shareImg
+                });
+            });
         }
     })
 });
