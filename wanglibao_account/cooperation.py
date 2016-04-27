370e1bbf92645503f43439fcba6a31a818ba5954
yihen <liuyihen@gmail.com>
2016-04-27 11:52:58 +0800

    .

--- wanglibao_account/cooperation.py
+++ wanglibao_account/cooperation.py
@@ -1147,71 +1147,6 @@ class XingMeiRegister(CoopRegister):
                 logger.debug(u"生成获奖记录报异常, reason:%s" % reason)
                 raise Exception(u"生成获奖记录异常")
 
-class KongGang1Register(CoopRegister):
-    def __init__(self, request):
-        super(KongGangRegister, self).__init__(request)
-        self.c_code = 'kgyx1'
-        self.invite_code = 'kgyx1'
-
-    def purchase_call_back(self, user, order_id):
-        p2p_record = P2PRecord.objects.filter(user_id=user.id, catalog=u'申购').order_by('create_time').first()
-
-        # 判断是否首次投资
-        if p2p_record and p2p_record.order_id == int(order_id):
-            with transaction.atomic():
-                join_record = WanglibaoRewardJoinRecord.objects.select_for_update().filter(user=user, activity_code=self.c_code).first()
-                if not join_record:
-                    join_record = join_record.objects.create(
-                            user=user,
-                            activity_code=self.c_code
-                    )
-                cip_reward = Reward.objects.filter(type='CIP专用安检通道服务', is_used=False).first()
-                wait_reward = Reward.objects.filter(type='尊贵休息室服务', is_used=False).first()
-                all_reward = Reward.objects.filter(type='贵宾全套出岗服务', is_used=False).first()
-
-                p2p_amount = int(p2p_record.amount)
-                send_reward = None
-                if  p2p_amount>=500 and p2p_amount<5000:
-                    send_reward = cip_reward
-                if  p2p_amount>=5000 and p2p_amount<10000:
-                    send_reward = wait_reward or cip_reward
-                if  p2p_amount>=10000:
-                    send_reward = all_reward or wait_reward or cip_reward
-                if send_reward:
-                    try:
-                        ActivityReward.objects.create(
-                                activity='kgyx1',
-                                order_id=order_id,
-                                user=user,
-                                p2p_amount=p2p_record.amount,
-                                reward=send_reward,
-                                has_sent=True,
-                                left_times=0,
-                                join_times=0)
-                        send_msg = u'尊敬的贵宾客户，恭喜您获得%s,' \
-                                   u' 服务地址请访问：www.trvok.com 查询，请使用时在机场贵宾服务台告知【空港易行】并出示此短信' \
-                                   u'，凭券号于现场验证后核销，券号：%s。如需咨询休息室具体位置可直接拨打空港易行客服热线:' \
-                                   u'4008131888，有效期：2016-4-15至2017-3-20；【网利科技】' % (send_reward.type, send_reward.content)
-                        send_messages.apply_async(kwargs={
-                            "phones": [user.id, ],
-                            "messages": [send_msg, ],
-                        })
-
-                        inside_message.send_one.apply_async(kwargs={
-                            "user_id": user.id,
-                            "title": u"空港易行优惠服务",
-                            "content": send_msg,
-                            "mtype": "activity"
-                        })
-                        send_reward.is_used = True
-                        send_reward.save()
-                    except Exception:
-                        logger.debug('user:%s, order_id:%s,p2p_amount:%s,空港易行发奖报错')
-                    join_record.save()
-                else: #所有奖品已经发完了
-                    join_record.save()
-                    return
-
 
 class KongGangRegister(CoopRegister):
     def __init__(self, request):
@@ -1220,6 +1155,31 @@ class KongGangRegister(CoopRegister):
         self.invite_code = 'kgyx'
 
     def purchase_call_back(self, user, order_id):
+
+        key = 'konggang'
+        activity_config = Misc.objects.filter(key=key).first()
+        if activity_config:
+            activity = json.loads(activity_config.value)
+            if type(activity) == dict:
+                try:
+                    start_time = activity['start_time']
+                    end_time = activity['end_time']
+                except KeyError, reason:
+                    logger.debug(u"misc中activities配置错误，请检查,reason:%s" % reason)
+                    raise Exception(u"misc中activities配置错误，请检查，reason:%s" % reason)
+            else:
+                raise Exception(u"misc中activities的配置参数，应是字典类型")
+        else:
+            raise Exception(u"misc中没有配置activities杂项")
+
+        now = time.strftime(u"%Y-%m-%d %H:%M:%S", time.localtime())
+        if now < start_time or now > end_time:
+            json_to_response = {
+                'ret_code': 1001,
+                'message': u'活动还未开始,请耐心等待'
+            }
+            return HttpResponse(json.dumps(json_to_response), content_type='application/json')
+
         p2p_record = P2PRecord.objects.filter(user_id=user.id, catalog=u'申购').order_by('create_time').first()
 
         # 判断是否首次投资
