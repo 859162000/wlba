a2a971f4d2bf73479fd150fe2e6ec1c968e48367
车思雪 <100401176@qq.com>
2016-04-27 13:59:27 +0800

    8月照相馆活动测试

--- static/src/mobile_activity/app_august_phone.js
+++ static/src/mobile_activity/app_august_phone.js
@@ -85,12 +85,12 @@
                         ts: data.ts
                     },
                     success: function (data) {
-                        var url = location.href;
-                        var times = url.split("?");
-                        if(times[1] != 1){
-                            url += "?1";
-                            self.location.replace(url);
-                        }
+                        //var url = location.href;
+                        //var times = url.split("?");
+                        //if(times[1] != 1){
+                        //    url += "?1";
+                        //    self.location.replace(url);
+                        //}
                     }
                 })
             }
@@ -98,10 +98,10 @@
             mixins.sendUserInfo(function(data) {
 
                 if (data.ph == '') {
-                    alert('1');
+                    alert('3');
                 } else {
                     connect(data);
-                    alert('2');
+                    alert('4');
                 }
 
                 $('#button_link').on("click",function(){
