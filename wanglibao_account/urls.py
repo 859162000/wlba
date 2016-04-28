b5e8b28dc3016e86f17a74a5acc5718fb1ef0ddd
wxqGitHub <color_bold@163.com>
2016-04-28 11:03:33 +0800

    fix yuelibao

--- wanglibao_account/urls.py
+++ wanglibao_account/urls.py
@@ -72,7 +72,7 @@ urlpatterns = patterns(
 
     url(r'^login/$', 'wanglibao_account.views.login_for_redirect',
         {
-            "template_name": "login_test.jade",
+            "template_name": "loginAndRegister.jade",
             "authentication_form": EmailOrPhoneAuthenticationForm,
         }, name="auth_login"),
     url(r'^token/login/ajax/$', 'wanglibao_account.views.ajax_token_login'),
