932a171ce012e877293c1697a3661272def62203
249143979 <249143979@qq.com>
2016-04-28 16:21:07 +0800

    修复微信task

--- weixin/tasks.py
+++ weixin/tasks.py
@@ -21,18 +21,25 @@ def bind_ok(openid, is_first_bind):
     weixin_user = WeixinUser.objects.get(openid=openid)
     now_str = datetime.datetime.now().strftime('%Y年%m月%d日')
     if is_first_bind:
-        from wanglibao_activity.backends import check_activity
-        check_activity(weixin_user.user, 'first_bind_weixin', "weixin", is_first_bind=is_first_bind)
+        from wanglibao_activity.tasks import check_activity_task
+        check_activity_task.apply_async(kwargs={
+                "user_id": weixin_user.id,
+                "trigger_node": 'first_bind_weixin',
+                "device_type": 'weixin',
+                "is_first_bind": is_first_bind,
+            }, queue='celery02')
+        # check_activity(user, 'first_bind_weixin', "weixin", is_first_bind=is_first_bind)
     else:
-        sentTemplate.apply_async(kwargs={"kwargs":json.dumps({
-                                    "openid":weixin_user.openid,
-                                    "template_id":BIND_SUCCESS_TEMPLATE_ID,
-                                    "name1":"",
-                                    "name2":weixin_user.user.wanglibaouserprofile.phone,
-                                    "time":now_str,
-                                        })},
-                                    queue='celery02'
-                                    )
+        sentTemplate.apply_async(kwargs={
+            "kwargs": json.dumps({
+                "openid": weixin_user.openid,
+                "template_id": BIND_SUCCESS_TEMPLATE_ID,
+                "name1": "",
+                "name2": weixin_user.wanglibaouserprofile.phone,
+                "time": now_str,
+            })
+        }, queue='celery02')
+
 
 @app.task
 def detect_product_biding(product_id):
@@ -46,8 +53,7 @@ def detect_product_biding(product_id):
                     "rate_desc": rate_desc,
                     "period_desc": period_desc,
                     "pay_method": pay_method,
-                },
-                                              queue='celery02')
+                }, queue='celery02')
 
     product = P2PProduct.objects.get(pk=product_id)
     utc_now = timezone.now()
@@ -88,10 +94,6 @@ def detect_product_biding(product_id):
                     _sendProductToUser(sub_record.w_user.openid, finance_service.describe, product.id, product.name, rate_desc, period_desc, product.pay_method)
 
 
-
-
-
-
 @app.task
 def sendUserProductOnLine(openid, service_desc, product_id, product_name, rate_desc, period_desc, pay_method):
     w_user = WeixinUser.objects.filter(openid=openid).first()
@@ -102,6 +104,7 @@ def sendUserProductOnLine(openid, service_desc, product_id, product_name, rate_d
             first=service_desc, keyword1=product_name, keyword2=rate_desc,
             keyword3=period_desc, keyword4=pay_method, url=url)
         sendTemplate(w_user, template)
+
 
 @app.task
 def sentTemplate(kwargs):
@@ -118,6 +121,7 @@ def sentTemplate(kwargs):
     if w_user and template and w_user.subscribe:
         sendTemplate(w_user, template)
 
+
 @app.task
 def sentCustomerMsg(txt, openid):
     fwh_info = getMiscValue("weixin_qrcode_info")
@@ -126,10 +130,9 @@ def sentCustomerMsg(txt, openid):
         weixin_account = WeixinAccounts.getByOriginalId(original_id)
         client = WeChatClient(weixin_account.app_id, weixin_account.app_secret)
         client.message._send_custom_message({
-                                    "touser":openid,
-                                    "msgtype":"text",
-                                    "text":
-                                    {
-                                        "content": txt
-                                    }
-                                }, account="007@wanglibao400")
+            "touser": openid,
+            "msgtype": "text",
+            "text": {
+                "content": txt
+            }
+        }, account="007@wanglibao400")
