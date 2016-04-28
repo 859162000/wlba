eb225721c86d2c665d4c12344207a5194f2e70c6
hbiami <hbiami@gmail.com>
2016-04-28 18:47:01 +0800

    fix by hb for zhaoxiangguan

--- wanglibao_reward/utils.py
+++ wanglibao_reward/utils.py
@@ -327,7 +327,7 @@ def processAugustAwardZhaoXiangGuan(user, product_id, order_id, amount):
         return
 
     #判断有没有奖品剩余
-    with transaction.atomic:
+    with transaction.atomic():
         reward = Reward.objects.select_for_update().filter(type='影像投资节优惠码', is_used=False).first()
         if reward == None:
             raise Exception(u"奖品已经发完了")
