ce352636af9c64d79b0c19086905bfedcab0a90d
hbiami <hbiami@gmail.com>
2016-04-29 14:35:34 +0800

    fix by chesx for share-url

--- static/src/mobile_activity/app_august_phone.js
+++ static/src/mobile_activity/app_august_phone.js
@@ -139,7 +139,7 @@
                     dataType: 'json',
                     success: function(data){
                         if(data.ret_code=='1000'){
-                            window.location.href = '/weixin/regist/?next=/activity/app_august_phone/?promo_token=sy'
+                            window.location.href = '/weixin/regist/?promo_token=sy&next=/activity/app_august_phone/?promo_token=sy'
                         }else if(data.ret_code=='1'){
                             $('.popup_box .main .textairport').text(''+data.message+'');
                             $('.popup_box').show();
