ae6aa3e944c5e67765c2717026d63079cfa62006
huomeimei <huomeimei@wanglibank.com>
2016-05-13 15:56:48 +0800

    fix 红木贷

--- weixin/urls.py
+++ weixin/urls.py
@@ -6,6 +6,7 @@ from wanglibao_activity.views import WeixinGGLTemplate
 import views, activity_views, manage_views, sub_views, base, main_views
 from experience_gold.views import ExperienceGoldView
 from common.decorators import fwh_login_required
+from marketing.views import HMDP2PListView
 
 urlpatterns = patterns(
     '',
@@ -95,7 +96,7 @@ urlpatterns = patterns(
     # url(r'^sub_account_old/$', fwh_login_required(main_views.AccountTemplate.as_view(template_name="service_account_old.jade"), login_url="/weixin/sub_login_redirect/")),
 
     url(r'^sub_recharge/$', fwh_login_required(main_views.RechargeTemplate.as_view(template_name="service_recharge.jade"), login_url="/weixin/sub_login_redirect/"), name="sub_recharge"),
-    url(r'^sub_list/$', fwh_login_required(main_views.FwhP2PlistTemplate.as_view(template_name="service_list.jade"), login_url="/weixin/sub_login_redirect/")),
+    url(r'^sub_list/$', fwh_login_required(main_views.FwhP2PlistTemplate.as_view(template_name="service_list.jade"), login_url="/weixin/sub_login_redirect/"), name='fwh_p2p_list'),
     url(r'^sub_detail/(?P<template>\w+)/(?P<id>\w+)/$', fwh_login_required(views.P2PDetailView.as_view(source='fwh'), login_url="/weixin/sub_login_redirect/")),
     url(r'^sub_experience/(?P<template>(account))/$', fwh_login_required(ExperienceGoldView.as_view(), login_url='/weixin/sub_login_redirect/')),
     url(r'^sub_transaction/(?P<status>\w+)/$', fwh_login_required(views.WeixinTransaction.as_view(template_name="service_transaction_repay.jade", source='fwh'), login_url="/weixin/sub_login_redirect/")),
@@ -151,6 +152,7 @@ urlpatterns += patterns(
 urlpatterns += patterns(
     '',
     url(r'^activity/share/$', activity_views.WechatShareTemplate.as_view(template_name="invite_redpacket_index.jade"), name='hby_weixin_share'),
+    url(r'^fwh_open_day_review/$', HMDP2PListView.as_view(template_name="app_open_day_review.jade", p2p_list_url_name="fwh_p2p_list")),
 )
 # 微信管理后台
 urlpatterns += patterns(
