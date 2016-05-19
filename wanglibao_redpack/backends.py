24bd4ae3479a3aa0cea49ae9fefd3ee2f2c9df1b
249143979 <249143979@qq.com>
2016-05-19 18:35:11 +0800

    理财券类型针对ios显示修复

--- wanglibao_redpack/backends.py
+++ wanglibao_redpack/backends.py
@@ -145,7 +145,7 @@ def list_redpack(user, status, device_type, product_id=0, rtype='redpack', app_v
                 if device_type == 'ios' or device_type == 'android':
                     if app_version <= "2.9.0":
                         period_type_event = event.period_type if event.period_type not in ('month_lte', 'day_lte') else ''
-                        if device_type == 'ios':
+                        if device_type == 'ios' and event.period_type in ('month_lte', 'day_lte'):
                             period_tmp = 0
                     else:
                         period_type_event = event.period_type
@@ -251,7 +251,7 @@ def list_redpack(user, status, device_type, product_id=0, rtype='redpack', app_v
                     if device_type == 'ios' or device_type == 'android':
                         if app_version <= "2.9.0":
                             period_type_event = event.period_type if event.period_type not in ('month_lte', 'day_lte') else ''
-                            if device_type == 'ios':
+                            if device_type == 'ios' and event.period_type in ('month_lte', 'day_lte'):
                                 period_tmp = 0
                         else:
                             period_type_event = event.period_type
@@ -310,10 +310,12 @@ def list_redpack(user, status, device_type, product_id=0, rtype='redpack', app_v
             else:
                 p2p_types_id = 0
                 p2p_types_name = ''
-
+            period_tmp = event.period
             if device_type == 'ios' or device_type == 'android':
                 if app_version <= "2.9.0":
                     period_type_event = event.period_type if event.period_type not in ('month_lte', 'day_lte') else ''
+                    if device_type == 'ios' and event.period_type in ('month_lte', 'day_lte'):
+                        period_tmp = 0
                 else:
                     period_type_event = event.period_type
             else:
@@ -324,7 +326,7 @@ def list_redpack(user, status, device_type, product_id=0, rtype='redpack', app_v
                 "available_at": stamp(start_time), "unavailable_at": stamp(end_time),
                 "id": x.id, "invest_amount": event.invest_amount, "amount": event.amount, "event_id": event.id,
                 "highest_amount": event.highest_amount,
-                "period": event.period,
+                "period": period_tmp,
                 "period_type": period_type_event,
                 "period_name": str(event.period) + period_name.get(event.period_type, ''),
                 "p2p_types_id": p2p_types_id, "p2p_types_name": p2p_types_name,
