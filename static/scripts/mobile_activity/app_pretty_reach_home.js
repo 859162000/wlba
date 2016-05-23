68b0dd3a056d9db21a6d900c5e36f83b3287a12e
车思雪 <100401176@qq.com>
2016-05-23 14:54:50 +0800

    小美到家规则修改

--- static/scripts/mobile_activity/app_pretty_reach_home.js
+++ static/scripts/mobile_activity/app_pretty_reach_home.js
@@ -295,7 +295,7 @@ var Zepto=function(){function L(t){return null==t?String(t):j[S.call(t)]||"objec
                                 type: 'post',
                                 success: function (data) {
                                     if(data.ret_code=='1000'){
-                                        mixins.loginApp({refresh:1, url:'/activity/app_pretty_reach_home/?promo_token=xmdj2'});
+                                        mixins.registerApp({refresh:1, url:'/activity/app_pretty_reach_home/?promo_token=xmdj2'});
                                     }else if(data.ret_code=='1001'||data.ret_code=='1002'){
                                         $('.popup_box .main .textairport').text(''+data.message+'');
                                         $('.popup_box').show();
@@ -318,7 +318,7 @@ var Zepto=function(){function L(t){return null==t?String(t):j[S.call(t)]||"objec
 
 
                     $('#take_prize,#take_prize_2').click(function() {
-                        mixins.loginApp({refresh:1, url:'/activity/app_pretty_reach_home/?promo_token=xmdj2'});
+                        mixins.registerApp({refresh:1, url:'/activity/app_pretty_reach_home/?promo_token=xmdj2'});
                     });
                 } else {
                     login = true;
@@ -335,7 +335,7 @@ var Zepto=function(){function L(t){return null==t?String(t):j[S.call(t)]||"objec
                     type: 'post',
                     success: function (data) {
                         if(data.ret_code=='1000'){
-                            window.location.href = '/weixin/login/?promo_token=zypwt&next=/activity/app_pretty_reach_home/?promo_token=xmdj2'
+                            window.location.href = '/weixin/regist/?promo_token=zypwt&next=/activity/app_pretty_reach_home/?promo_token=xmdj2'
                         }else if(data.ret_code=='1001'||data.ret_code=='1002'){
                             $('.popup_box .main .textairport').text(''+data.message+'');
                             $('.popup_box').show();
