933f166ad4d959857a1a41b0a02864ab22ec3497
车思雪 <100401176@qq.com>
2016-05-25 17:53:42 +0800

    提交新客服footer

--- static/src/pc/footer_kefu.js
+++ static/src/pc/footer_kefu.js
@@ -1,7 +1,7 @@
- (function(a,h,c,b,f,g){a["UdeskApiObject"]=f;a[f]=a[f]||function(){(a[f].d=a[f].d||[]).push(arguments)};g=h.createElement(c);g.async=1;g.src=b;c=h.getElementsByTagName(c)[0];c.parentNode.insertBefore(g,c)})(window,document,"script","http://18612250386.udesk.cn/im_client/js/udeskApi.js?_t=1464168984272","ud"); ud({"code":"h3g84ai","link":"http://18612250386.udesk.cn/im_client","isInvite":true,"mobile":{"mode":"blank","color":"#307AE8","pos_flag":"crb","onlineText":"联系客服，在线咨询","offlineText":"客服下班，请留言","pop":{"direction":"top","arrow":{"top":0,"left":"70%"}}},"mode":"blank","color":"#307AE8","pos_flag":"srb","onlineText":"联系客服，在线咨询","offlineText":"客服下班，请留言","pop":{"direction":"top","arrow":{"top":0,"left":"80%"}}});
+ (function(a,h,c,b,f,g){a["UdeskApiObject"]=f;a[f]=a[f]||function(){(a[f].d=a[f].d||[]).push(arguments)};g=h.createElement(c);g.async=1;g.src=b;c=h.getElementsByTagName(c)[0];c.parentNode.insertBefore(g,c)})(window,document,"script","https://18612250386.udesk.cn/im_client/js/udeskApi.js?_t=1464168984272","ud"); ud({"code":"h3g84ai","link":"https://18612250386.udesk.cn/im_client","isInvite":true,"mobile":{"mode":"blank","color":"#307AE8","pos_flag":"crb","onlineText":"联系客服，在线咨询","offlineText":"客服下班，请留言","pop":{"direction":"top","arrow":{"top":0,"left":"70%"}}},"mode":"blank","color":"#307AE8","pos_flag":"srb","onlineText":"联系客服，在线咨询","offlineText":"客服下班，请留言","pop":{"direction":"top","arrow":{"top":0,"left":"80%"}}});
 function kefu_new(){
 
-    var openUrl = "http://18612250386.udesk.cn/im_client/";//弹出窗口的url
+    var openUrl = "https://18612250386.udesk.cn/im_client/";//弹出窗口的url
     var iWidth = 780; //弹出窗口的宽度;
     var iHeight = 615; //弹出窗口的高度;
     var iTop = 225; //获得窗口的垂直位置;
