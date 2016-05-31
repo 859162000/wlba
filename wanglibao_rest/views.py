287a2631d94e3dcd6cfe4a8cb61c0fd541655000
yihen <liuyihen@gmail.com>
2016-05-31 16:24:55 +0800

    app端同一个ip最多可以错误登录5此

--- wanglibao_rest/views.py
+++ wanglibao_rest/views.py
@@ -1050,13 +1050,25 @@ class LoginAPIView(DecryptParmsAPIView):
             return Response({"token":"false", "message":u"用户名或密码不可为空"}, status=400)
 
         from wanglibao_account.models import GeetestModifiedTimes
+        client_ip = request.META['HTTP_X_FORWARDED_FOR'] if request.META.get('HTTP_X_FORWARDED_FOR', None) else request.META.get('HTTP_X_REAL_IP', None)
         geetest_record = GeetestModifiedTimes.objects.filter(identified=identifier).first()
         if not geetest_record:
             geetest_record = GeetestModifiedTimes.objects.create(
                 identified=identifier,
                 times=0)
 
+        geetest_record_ip = GeetestModifiedTimes.objects.filter(identified=client_ip).first()
+        if not geetest_record_ip:
+            geetest_record_ip = GeetestModifiedTimes.objects.create(
+                    identified=client_ip,
+                    times=0)
+
         if geetest_record.times>2:
+            res, message = verify_captcha(dic=request.POST, keep=True)
+            if not res:
+                return Response({"token":"false", 'message': message, "type": "captcha"}, status=400)
+
+        if geetest_record_ip.times>5:
             res, message = verify_captcha(dic=request.POST, keep=True)
             if not res:
                 return Response({"token":"false", 'message': message, "type": "captcha"}, status=400)
@@ -1070,8 +1082,10 @@ class LoginAPIView(DecryptParmsAPIView):
 
         if not user:
             geetest_record.times += 1
+            geetest_record_ip.times += 1
             display_pic = 'true' if geetest_record.times>2 else 'false'
             geetest_record.save()
+            geetest_record_ip.save()
             return Response({"token": "false", "message": u"用户名或密码错误", "display_pic":display_pic}, status=400)
         if not user.is_active:
             return Response({"token": "false", "message": u"用户已被关闭"}, status=400)
@@ -1108,7 +1122,9 @@ class LoginAPIView(DecryptParmsAPIView):
         #     token.delete()
         #     token, created = Token.objects.get_or_create(user=user)
         geetest_record.times = 0
+        geetest_record_ip.times = 0
         geetest_record.save()
+        geetest_record_ip.save()
         return Response({'token': token.key, "user_id":user.id}, status=status.HTTP_200_OK)
 
 
