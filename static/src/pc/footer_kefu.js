7626f0de01a7314bda0d9ee88714733f5543fb86
车思雪 <100401176@qq.com>
2016-06-02 16:46:22 +0800

    提交footer

--- static/src/pc/footer_kefu.js
+++ static/src/pc/footer_kefu.js
@@ -1,30 +1,4 @@
- //(function(a,h,c,b,f,g){a["UdeskApiObject"]=f;a[f]=a[f]||function(){(a[f].d=a[f].d||[]).push(arguments)};g=h.createElement(c);g.async=1;g.src=b;c=h.getElementsByTagName(c)[0];c.parentNode.insertBefore(g,c)})(window,document,"script","https://18612250386.udesk.cn/im_client/js/udeskApi.js?_t=1464168984272","ud"); ud({"code":"h3g84ai","link":"https://18612250386.udesk.cn/im_client","isInvite":true,"mobile":{"mode":"blank","color":"#307AE8","pos_flag":"crb","onlineText":"联系客服，在线咨询","offlineText":"客服下班，请留言","pop":{"direction":"top","arrow":{"top":0,"left":"70%"}}},"mode":"blank","color":"#307AE8","pos_flag":"srb","onlineText":"联系客服，在线咨询","offlineText":"客服下班，请留言","pop":{"direction":"top","arrow":{"top":0,"left":"80%"}}});
-//(function() {
-//function kefu_new(){
-//
-//    var openUrl = "http://wanglibao.udesk.cn/im_client/";//弹出窗口的url
-//    var iWidth = 780; //弹出窗口的宽度;
-//    var iHeight = 615; //弹出窗口的高度;
-//    var iTop = 225; //获得窗口的垂直位置;
-//    var iLeft = 400; //获得窗口的水平位置;
-//
-//
-//    $('#kefu_link').click(function(){
-//      $.ajax({
-//        url: '/api/udesk/url',
-//        type: 'post',
-//        success: function (data1) {
-//
-//
-//        },error: function(data1){
-//
-//        }
-//    })
-//})
-//}
-//kefu_new();
-//})();
-
+//(function(a,h,c,b,f,g){a["UdeskApiObject"]=f;a[f]=a[f]||function(){(a[f].d=a[f].d||[]).push(arguments)};g=h.createElement(c);g.async=1;g.src=b;c=h.getElementsByTagName(c)[0];c.parentNode.insertBefore(g,c)})(window,document,"script","https://18612250386.udesk.cn/im_client/js/udeskApi.js?_t=1464168984272","ud"); ud({"code":"h3g84ai","link":"https://18612250386.udesk.cn/im_client","isInvite":true,"mobile":{"mode":"blank","color":"#307AE8","pos_flag":"crb","onlineText":"联系客服，在线咨询","offlineText":"客服下班，请留言","pop":{"direction":"top","arrow":{"top":0,"left":"70%"}}},"mode":"blank","color":"#307AE8","pos_flag":"srb","onlineText":"联系客服，在线咨询","offlineText":"客服下班，请留言","pop":{"direction":"top","arrow":{"top":0,"left":"80%"}}});
 
  (function() {
     require.config({
@@ -75,17 +49,26 @@
             }
         });
 
+        var openUrl = "http://wanglibao.udesk.cn/im_client/";//弹出窗口的url
+        var iWidth = 780; //弹出窗口的宽度;
+        var iHeight = 615; //弹出窗口的高度;
+        var iTop = 225; //获得窗口的垂直位置;
+        var iLeft = 400; //获得窗口的水平位置;
+
         $('#kefu_link').click(function(){
           $.ajax({
-            url: '/api/udesk/url',
+            url: '/api/udesk/url/',
             type: 'post',
             data:{
 
             },
-            success: function (data1) {
+            success: function (data) {
+                openUrl = data.url;
+                //弹出窗口的url
 
+                window.open(openUrl, "_blank", "height=" + iHeight + ", width=" + iWidth + ", top=" + iTop + ", left=" + iLeft);
 
-            },error: function(data1){
+            },error: function(data){
 
             }
             })
