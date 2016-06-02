4f4e418cc25148834591821fc3513525bb5e36f0
kvenchou <qi_yun_zhou@163.com>
2016-06-02 10:57:34 +0800

    no message

--- marketing/tools.py
+++ marketing/tools.py
@@ -70,10 +70,6 @@ def decide_first(user_id, amount, device, order_id, product_id=0, is_full=False,
         "ylb_period": ylb_period
     }, queue='celery02')
 
-    # 月利宝的标不进行除发奖励外的操作.
-    if ylb_period:
-        return
-
     # fix@chenweibi, add order_id
     try:
         utils.log_clientinfo(device, "buy", user_id, order_id, amount)
@@ -120,6 +116,10 @@ def decide_first(user_id, amount, device, order_id, product_id=0, is_full=False,
                 queue='coop_celery', routing_key='coop_celery', exchange='coop_celery')
     except:
         pass
+    
+    # 月利宝的标不进行除发奖励外的操作.
+    if ylb_period:
+        return
 
 def checkUpdateHmdRanks(product_id):
     activity = Activity.objects.filter(code='hmd').first()
