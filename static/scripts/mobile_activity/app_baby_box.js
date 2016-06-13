e8f2fb4743e451aabd61631ed9c7c4ffe7984ce5
hbiami <hbiami@gmail.com>
2016-06-13 17:23:36 +0800

    fix by chesx for 宝贝格子Fix

--- static/scripts/mobile_activity/app_baby_box.js
+++ static/scripts/mobile_activity/app_baby_box.js
@@ -220,13 +220,31 @@ var Zepto=function(){function L(t){return null==t?String(t):j[S.call(t)]||"objec
 ;
 (function(org) {
 
+
+    var url_search = window.location.search,promo_token_name='';
+	var searchArray = url_search.substring(1).split("?");
+	for(var i = 0;i < searchArray.length; i++){
+		var temp = searchArray[i].split('=');
+		if(temp[0] == 'promo_token'){
+			promo_token_name = temp[1] ? temp[1] : '';
+		}
+	}
+
+	if(!promo_token_name){
+		$('.body_wrap').hide();
+		window.location.href = ''+window.location.href+'?promo_token=bg'
+	}else{
+		$('.body_wrap').show();
+	}
+
+
     $('#take_prize,#take_prize_2').click(function() {
         org.ajax({
             url: '/api/activity/baobeigezi/',
             type: 'post',
             success: function (data) {
                 if(data.ret_code=='1000'){
-                    window.location.href = '/weixin/regist/?promo_token=bg&next=/activity/app_baby_box/?promo_token=bg'
+                    window.location.href = '/weixin/regist/?promo_token=bg&next=/activity/app_baby_box/h5/?promo_token=bg'
                 }else if(data.ret_code=='1002'){
                     window.location.href = '/weixin/list/?promo_token=bg'
                 }else if(data.ret_code=='1001'||data.ret_code=='1002'||data.ret_code=='1004'){
@@ -239,6 +257,7 @@ var Zepto=function(){function L(t){return null==t?String(t):j[S.call(t)]||"objec
             }
         })
     })
+
 
 
     $('.popup_box .popup_button,.popup_box .close_popup').click(function(){
--- static/src/mobile_activity/app_baby_box_h5.js
+++ static/src/mobile_activity/app_baby_box_h5.js
@@ -21,7 +21,7 @@
 		var host = location.protocol+"//"+location.host,
 			shareName = '网利宝免费萌娃礼 只为爱升温！',
 			shareImg = host + '/static/imgs/mobile_activity/app_baby_box/300*300.jpg',
-			shareLink = host + '/activity/app_baby_box/?promo_token=bg',
+			shareLink = host + '/activity/app_baby_box/h5/?promo_token=bg',
 			shareMainTit = '网利宝免费萌娃礼 只为爱升温！',
 			shareBody = '最好的爱 只为予你';
 		//分享给微信好友
@@ -33,7 +33,7 @@
 		});
 		//分享给微信朋友圈
 		org.onMenuShareTimeline({
-			title: '网利宝免费萌娃礼 只为爱升温！”',
+			title: '网利宝免费萌娃礼 只为爱升温！',
 			link : shareLink,
 			imgUrl: shareImg
 		})
@@ -46,13 +46,30 @@
 		})
 	})
 
+	var url_search = window.location.search,promo_token_name='';
+	var searchArray = url_search.substring(1).split("?");
+	for(var i = 0;i < searchArray.length; i++){
+		var temp = searchArray[i].split('=');
+		if(temp[0] == 'promo_token'){
+			promo_token_name = temp[1] ? temp[1] : '';
+		}
+	}
+
+
+	if(!promo_token_name){
+		$('.body_wrap').hide();
+		window.location.href = ''+window.location.href+'?promo_token=bg'
+	}else{
+		$('.body_wrap').show();
+	}
+
     $('#take_prize,#take_prize_2').click(function() {
         org.ajax({
             url: '/api/activity/baobeigezi/',
             type: 'post',
             success: function (data) {
                 if(data.ret_code=='1000'){
-                    window.location.href = '/weixin/regist/?promo_token=bg&next=/activity/app_baby_box/?promo_token=bg'
+                    window.location.href = '/weixin/regist/?promo_token=bg&next=/activity/app_baby_box/h5/?promo_token=bg'
                 }else if(data.ret_code=='1002'){
                     window.location.href = '/weixin/list/?promo_token=bg'
                 }else if(data.ret_code=='1001'||data.ret_code=='1002'||data.ret_code=='1004'){
@@ -61,15 +78,19 @@
                 }else{
                     $('.popup_box .main .textairport').text('系统繁忙，请稍后再试');
                     $('.popup_box').show();
+					$('.popup_box_123').show();
+
                 }
             }
         })
     })
 
-
     $('.popup_box .popup_button,.popup_box .close_popup').click(function(){
         $('.popup_box').hide();
     });
+	$('.popup_box .popup_button,.popup_box .close_popup').click(function(){
+        $('.popup_box').hide();
+    });js
     $('.slideDown_button').on('click',function(){
         var ele = $('.slideDown_box');
         var curHeight = ele.height();
