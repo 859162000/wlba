bae4ce608b3ab2820acd3af5a716670bed25aad3
huomeimei <huomeimei@wanglibank.com>
2016-06-13 14:32:19 +0800

    fix 加入优先级

--- wanglibao_app/views.py
+++ wanglibao_app/views.py
@@ -144,12 +144,12 @@ class AppActivateImageAPIView(APIView):
             if device_type == 'android': device_type = 'act_android'
 
         size = self.SIZE_MAP[size]
-        # activate_query = AppActivate.objects.filter(Q(is_used=True), Q(device=self.DEVICE_MAP[device_type]))
-        # activate = activate_query.filter(Q(is_long_used=False) & Q(start_at__lte=timezone.now()) & Q(end_at__gte=timezone.now())).order_by('-priority').first()
-        # if not activate:
-        #     activate = activate_query.filter(Q(is_long_used=True)).order_by('-priority').first()
+        activate_query = AppActivate.objects.filter(Q(is_used=True), Q(device=self.DEVICE_MAP[device_type]))
+        activate = activate_query.filter(Q(is_long_used=False) & Q(start_at__lte=timezone.now()) & Q(end_at__gte=timezone.now())).order_by('-priority').first()
+        if not activate:
+            activate = activate_query.filter(Q(is_long_used=True)).order_by('-priority').first()
 
-        activate = AppActivate.objects.filter(Q(is_used=True), Q(device=self.DEVICE_MAP[device_type]), Q(is_long_used=True) | (Q(is_long_used=False) & Q(start_at__lte=timezone.now()) & Q(end_at__gte=timezone.now()))).first()
+        # activate = AppActivate.objects.filter(Q(is_used=True), Q(device=self.DEVICE_MAP[device_type]), Q(is_long_used=True) | (Q(is_long_used=False) & Q(start_at__lte=timezone.now()) & Q(end_at__gte=timezone.now()))).first()
         if activate:
             if size == 'img_one':
                 img_url = activate.img_one
