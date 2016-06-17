d2b6e52c459957674657899a8ef6c293d0601f6a
kvenchou <qi_yun_zhou@163.com>
2016-06-17 11:40:52 +0800

    no message

--- wanglibao/settings.py
+++ wanglibao/settings.py
@@ -1095,8 +1095,6 @@ else:
 WLB_FOR_DOUWANWANG_KEY = '1992'
 DOUWANWANG_CALL_BACK_URL = 'http://mall.366dw.com/interface/reflection'
 
-#弹66 accesskey
-TANLIULIU_ACCESSKEY = f405cd3212e7
 
 #西财
 XICAI_TOKEN_URL = 'http://api.csai.cn/oauth2/access_token2'
